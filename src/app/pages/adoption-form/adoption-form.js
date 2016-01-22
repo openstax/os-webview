0f8a9c829a5abe5c88db428b7474c7cd33f57e9e
Roy Johnson <roy.e.johnson@rice.edu>
2016-01-22 14:07:16 -0600

    Refactor to use @on events
    Factored sectionToggler into separate object
    amend: remove alert

--- src/app/pages/adoption-form/adoption-form.js
+++ src/app/pages/adoption-form/adoption-form.js
@@ -1,7 +1,37 @@
 import BaseView from '~/helpers/backbone/view';
 import BookInfoView from './book-info/book-info';
-import {props} from '~/helpers/backbone/decorators';
+import {on, props} from '~/helpers/backbone/decorators';
 import {template} from './adoption-form.hbs';
+
+function sectionToggler(options) {
+    let isOpen = false,
+        toggleControl = options.toggleControl,
+        toggleArea = options.toggleArea,
+        openText = options.openText,
+        closeText = options.closeText,
+        region = options.region,
+        updateToggleRegion = () => {
+            if (isOpen) {
+                let secondaryBookView = new BookInfoView('adoption_two');
+
+                toggleControl.innerHTML = closeText;
+                region.append(secondaryBookView);
+                toggleArea.style.display = '';
+            } else {
+                toggleControl.innerHTML = openText;
+                region.empty();
+                toggleArea.style.display = 'none';
+            }
+        };
+
+    return {
+        toggle: () => {
+            isOpen = !isOpen;
+            updateToggleRegion();
+        },
+        update: updateToggleRegion
+    };
+}
 
 @props({
     template: template,
@@ -11,47 +41,35 @@ import {template} from './adoption-form.hbs';
     }
 })
 export default class AdoptionForm extends BaseView {
+
+    @on('click .toggle-section')
+    toggleSection() {
+        event.preventDefault();
+        this.toggler.toggle();
+    }
+
+    @on('submit form')
+    failIfInvalid(event) {
+        let invalid = this.el.querySelectorAll('.invalid');
+
+        if (invalid.length > 0) {
+            event.preventDefault();
+        }
+    }
+
     onRender() {
         this.el.classList.add('text-content');
-        let bookInfoView = new BookInfoView('adoption'),
-            toggleControl = this.el.querySelector('.toggle-section'),
-            toggleArea = document.getElementById('secondary-book'),
-            toggleIsOpen = false,
-            openText = '+ Add a second book',
-            closeText = '- Remove the second book',
-            form = this.el.querySelector('form'),
-            updateToggleRegion = () => {
-                let region = this.regions.secondaryBook;
-
-                if (toggleIsOpen) {
-                    let secondaryBookView = new BookInfoView('adoption_two');
-
-                    toggleControl.innerHTML = closeText;
-                    region.append(secondaryBookView);
-                    toggleArea.style.display = '';
-                } else {
-                    toggleControl.innerHTML = openText;
-                    region.empty();
-                    toggleArea.style.display = 'none';
-                }
-            };
+        this.toggler = sectionToggler({
+            toggleControl: this.el.querySelector('.toggle-section'),
+            toggleArea: document.getElementById('secondary-book'),
+            openText: '+ Add a second book',
+            closeText: '- Remove the second book',
+            region: this.regions.secondaryBook
+        });
+        let bookInfoView = new BookInfoView('adoption');
 
         this.regions.primaryBook.append(bookInfoView);
-        updateToggleRegion();
-
-        toggleControl.addEventListener('click', (event) => {
-            event.preventDefault();
-            toggleIsOpen = !toggleIsOpen;
-            updateToggleRegion();
-        });
-
-        form.addEventListener('submit', (event) => {
-            let invalid = this.el.querySelectorAll('.invalid');
-
-            if (invalid.length > 0) {
-                event.preventDefault();
-            }
-        });
+        this.toggler.update();
     }
 
 }
--- src/app/pages/adoption-form/book-info/book-info.js
+++ src/app/pages/adoption-form/book-info/book-info.js
@@ -1,74 +1,66 @@
 import BaseView from '~/helpers/backbone/view';
-import {props} from '~/helpers/backbone/decorators';
+import {on, props} from '~/helpers/backbone/decorators';
 import {template} from './book-info.hbs';
 
-function requireOne(container) {
-    let checkboxes = container.querySelectorAll('input[type=checkbox]'),
-        checkCount = 0,
-        setClass = function () {
-            if (checkCount > 0) {
-                container.classList.remove('invalid');
-            } else {
-                container.classList.add('invalid');
-            }
-        },
-        onChange = function (e) {
-            if (e.target.checked) {
-                checkCount += 1;
-            } else {
-                checkCount -= 1;
-            }
-            setClass();
-        };
-
-    for (let i=0; i<checkboxes.length; ++i) {
-        let el = checkboxes[i];
-
-        el.addEventListener('change', onChange);
-    }
-    setClass();
-}
+const noneBoxSelector = '.require-one input[type="checkbox"][value="None"]',
+    otherBoxSelector = 'input[type="checkbox"][value="other"]';
 
 @props({
     template: template
 })
 export default class BookInfoView extends BaseView {
+
+    @on('change .require-one input[type="checkbox"]')
+    checkboxChanged(e) {
+        this.checkboxesChecked += e.target.checked ? 1 : -1;
+        let addOrRemove = this.checkboxesChecked > 0 ? 'remove' : 'add';
+
+        this.checkboxGroup.classList[addOrRemove]('invalid');
+    }
+
+    @on(`change ${noneBoxSelector}`)
+    noneBoxChanged() {
+        let boxes = this.checkboxGroup.querySelectorAll('input[type=checkbox]:not([value="None"])'),
+            noneBox = this.noneBox;
+
+        for (let i=0; i<boxes.length; ++i) {
+            let box=boxes[i];
+
+            if (noneBox.checked && box.checked) {
+                box.checked = false;
+                box.dispatchEvent(new Event('change'));
+            }
+            box.disabled = noneBox.checked;
+        }
+    }
+
+    @on(`change ${otherBoxSelector}`)
+    otherBoxChange() {
+        let otherText = this.el.querySelector(this.otherTextSelector),
+            otherBox = this.el.querySelector(otherBoxSelector);
+
+        if (!otherBox.checked) {
+            otherText.value = '';
+        }
+        otherText.disabled = !otherBox.checked;
+    }
+
     constructor(prefix) {
         super();
         this.templateHelpers = {
             prefix: prefix
         };
+        this.checkboxesChecked = 0;
+        this.prefix = prefix;
+        this.otherTextSelector = `input[type="text"][name="${prefix}[other]"]`;
     }
     onRender() {
-        let checkboxGroup = this.el.querySelector('.require-one');
+        this.checkboxGroup = this.el.querySelector('.require-one');
+        this.checkboxGroup.classList.add('invalid');
+        this.noneBox = this.el.querySelector(noneBoxSelector);
+        let otherBox = this.el.querySelector(otherBoxSelector),
+            otherText = this.el.querySelector(this.otherTextSelector);
 
-        requireOne(checkboxGroup);
-
-        let noneBox = this.el.querySelector('input[type="checkbox"][value="None"]');
-
-        noneBox.addEventListener('change', () => {
-            let boxes = checkboxGroup.querySelectorAll('input[type=checkbox]:not([value="None"])');
-
-            for (let i=0; i<boxes.length; ++i) {
-                let box=boxes[i];
-
-                if (noneBox.checked && box.checked) {
-                    box.checked = false;
-                    box.dispatchEvent(new Event('change'));
-                }
-                box.disabled = noneBox.checked;
-            }
-        });
-        let prefix = this.templateHelpers.prefix,
-            otherBox = this.el.querySelector('input[type="checkbox"][value="other"]'),
-            otherText = this.el.querySelector(`input[type="text"][name="${prefix}[other]"]`);
-
-        otherBox.addEventListener('change', () => {
-            if (!otherBox.checked) {
-                otherText.value = '';
-            }
-            otherText.disabled = !otherBox.checked;
-        });
         otherText.disabled = !otherBox.checked;
     }
 }
