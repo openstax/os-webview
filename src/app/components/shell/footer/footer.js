fe9a12bd1705655f17c5bb8bbf1e115667a5cd6f
Zach Roehr <zach.roehr@gmail.com>
2016-01-27 10:53:47 -0600

    html refactor, added css animations for micro-interactions

--- src/app/components/shell/footer/footer.js
+++ src/app/components/shell/footer/footer.js
@@ -9,45 +9,48 @@ import {template} from './footer.hbs';
 })
 export default class Footer extends BaseView {
   onRender() {
-    console.log('testing');
-    // this.regions.featured.show(new CarouselView());
-  //   var waypoint = new Waypoint({
-  //   element: document.getElementById('main-nav'),
-  //   handler: function(direction) {
-  //     console.log(this.element.id + ' triggers at ' + this.triggerPoint)
-  //   },
-  //   offset: 20
-  // })
-  // var waypoint = new Waypoint({
-  //   element: document.getElementById('main'),
-  //   handler: function(direction) {
-  //     console.log('Scrolled to waypoint!')
-  //   }
-  // })
-  // var sticky = new Waypoint.Sticky({
-  //   element: document.getElementsByClassName('nav')[0],
-  //   //wrapper: false,
-  //   handler: function(direction) {
-  //     alert('50% past the top')
-  //   },
-  //   offset: -150
-  // })
-  window.addEventListener("scroll", function(e) {
-    console.log('test');
-    var element = document.getElementById('header');
-    console.log(element.scrollTop);
 
-    if (document.body.scrollTop >= 70) {
-      element.className = "sticky";
-      //console.log(element.scrollTop);
+    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop,
+    position = document.body.scrollTop;
 
-    } else if (document.body.scrollTop <= 70) {
-      //wrap.removeClass("fix-search");
-      //console.log('bark');
-      element.className = "";
 
-}
+    function cloneElement() {
+      var element = document.getElementsByClassName('nav-menu')[0];
+      var header = document.getElementsByClassName('nav-menu')[1].parentNode;
+      console.log(header);
+      var clone = element.cloneNode(true);
+      clone.classList.add('clone');
+      header.insertBefore(clone, header.childNodes[2]);
 
-});
+    }
+
+    function scrollD() {
+    var timer;
+    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    var element = document.getElementsByClassName('page-header')[0];
+
+
+
+    if (scroll >= 250) {
+            element.classList.add('sticky');
+            setTimeout(function() {
+              element.classList.add('is-visible');
+            });
+          } else if ((scroll <= 249) && (scroll >= 150)) {
+
+            element.classList.remove('is-visible');
+          } else {
+            element.classList.remove('sticky');
+
+          }
+    }
+    window.addEventListener("scroll", function(e) {
+
+      scrollD();
+
+    });
+    document.addEventListener("DOMContentLoaded", cloneElement());
+
+
   }
 }
