641fd693aeb74e1526c7698e54172e93af32982e
Maher <me24@rice.edu>
2016-02-12 11:49:50 -0600

    add loader to shell directory

--- src/app/components/shell/shell.js
+++ src/app/components/shell/shell.js
@@ -1,5 +1,6 @@
 import BaseView from '~/helpers/backbone/view';
 import {props} from '~/helpers/backbone/decorators';
+import loader from './loader/loader';
 import header from './header/header';
 import footer from './footer/footer';
 import {template} from './shell.hbs';
@@ -9,16 +10,19 @@ import zendesk from '~/helpers/zendesk';
     el: 'body',
     template: template,
     regions: {
+	    loader: '#loader',
         main: '#main',
         header: '#header',
         footer: '#footer'
     }
 })
+
 class AppView extends BaseView {
 
     constructor() {
         super();
 
+        this.loader = loader;
         this.header = header;
         this.footer = footer;
 
@@ -32,6 +36,7 @@ class AppView extends BaseView {
         System.import(`~/pages/${pageName}/${pageName}`).then((m) => {
             let Page = m.default;
 
+            this.regions.loader.show(loader);
             this.regions.header.show(header);
             this.regions.main.show(new Page(options));
             this.regions.footer.show(footer);
