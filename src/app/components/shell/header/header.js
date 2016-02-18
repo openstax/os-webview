8586d87500ad8ad0fe18b204665ec1361aa8b4cc
Zach Roehr <zach.roehr@gmail.com>
2016-02-18 12:02:52 -0600

    fix spacing

--- src/app/components/shell/header/header.js
+++ src/app/components/shell/header/header.js
@@ -79,7 +79,7 @@ class Header extends BaseView {
     isTransparent() {
         return !!this.classList('contains', 'transparent');
     }
-    
+
     isVisible() {
         return !!this.classList('contains', 'visible');
     }
