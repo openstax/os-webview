6fb7b88a9c94dc8d477490764aeac65e74a9f0cc
Roy Johnson <roy.e.johnson@rice.edu>
2016-02-25 16:27:19 -0600

    Make color strips a component

--- src/app/pages/accessibility-statement/accessibility-statement.js
+++ src/app/pages/accessibility-statement/accessibility-statement.js
@@ -1,8 +1,10 @@
 import BaseView from '~/helpers/backbone/view';
 import {props} from '~/helpers/backbone/decorators';
 import {template} from './accessibility-statement.hbs';
+import {template as strips} from '~/components/strips/strips.hbs';
 
 @props({
-    template: template
+    template: template,
+    templateHelpers: {strips}
 })
 export default class Accessibility extends BaseView {}
