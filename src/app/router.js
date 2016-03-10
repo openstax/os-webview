dc55fa98e9abf9cc5e562d14df5b9237b8168c9b
Roy Johnson <roy.e.johnson@rice.edu>
2016-03-10 10:52:15 -0600

    Create interest confirmation page, fix download menu issues
    After handling interest form, Salesforce should redirect to
    interest-confirmation.
    No asterisk on View on Web menu item (download menu)
    Hide unavailable download options (get print copy, daisy/BRF)

--- src/app/router.js
+++ src/app/router.js
@@ -26,7 +26,8 @@ class Router extends Backbone.Router {
         ['about', 'books', 'contact', 'news', 'license', 'subjects', 'details',
         'interest', 'adoption', 'adoption-confirmation', 'comp-copy',
         'accessibility-statement', 'faculty-verification', 'k-12', 'allies',
-        'finish-profile', 'finished-verify', 'finished-no-verify', 'about-us']
+        'finish-profile', 'finished-verify', 'finished-no-verify', 'about-us',
+        'interest-confirmation']
         .forEach(this.standardRoute, this);
 
         this.route(/to[u|s]/, 'tos', () => {
