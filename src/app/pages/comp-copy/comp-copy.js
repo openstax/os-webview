5319078066b6b5a1122283ade49d23803e9aeba2
Roy Johnson <roy.e.johnson@rice.edu>
2016-03-15 09:10:23 -0500

    Add single-select proxy widget to comp copy form

--- src/app/pages/comp-copy/comp-copy.js
+++ src/app/pages/comp-copy/comp-copy.js
@@ -1,5 +1,6 @@
 import BaseView from '~/helpers/backbone/view';
 import salesforceModel from '~/models/salesforce-model';
+import SingleSelect from '~/components/single-select/single-select';
 import {on, props} from '~/helpers/backbone/decorators';
 import {template} from './comp-copy.hbs';
 import {template as strips} from '~/components/strips/strips.hbs';
@@ -30,8 +31,27 @@ export default class CompCopyForm extends BaseView {
         }
     }
 
+    failIfInvalid(event) {
+        this.el.querySelector('form').classList.add('has-been-submitted');
+        for (let widget of this.selectWidgets) {
+            widget.doValidChecks();
+        }
+        let invalid = this.el.querySelectorAll('.invalid');
+
+        if (invalid.length > 0) {
+            event.preventDefault();
+        }
+    }
+
     onRender() {
         this.el.classList.add('comp-copy-form');
         salesforceModel.prefill(this.el);
+        this.selectWidgets = [];
+        for (let ss of this.el.querySelectorAll('select:not([multiple])')) {
+            let widget = new SingleSelect();
+
+            widget.replace(ss);
+            this.selectWidgets.push(widget);
+        }
     }
 }
