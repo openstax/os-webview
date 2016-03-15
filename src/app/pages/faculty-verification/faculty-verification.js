c6477b2976ed4bfedf02c796c1671a2ee1726784
Maher <me24@rice.edu>
2016-03-15 02:07:29 -0500

    Add Instructor Verification page styles

--- src/app/pages/faculty-verification/faculty-verification.js
+++ src/app/pages/faculty-verification/faculty-verification.js
@@ -1,11 +1,13 @@
 import BaseView from '~/helpers/backbone/view';
 import {props} from '~/helpers/backbone/decorators';
 import {template} from './faculty-verification.hbs';
+import {template as strips} from '~/components/strips/strips.hbs';
 
 @props({
     template: template,
     templateHelpers: {
-        urlOrigin: window.location.origin
+        urlOrigin: window.location.origin,
+        strips
     }
 })
 export default class FacultyVerificationForm extends BaseView {
