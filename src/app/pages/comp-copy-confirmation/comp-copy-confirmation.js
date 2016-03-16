aaaebb7ac1db4ec2471462f00a310c12dc583027
Maher <me24@rice.edu>
2016-03-16 17:13:41 -0500

    Add styles for both Comp Copy Confirmation and Interest Confirmation

--- src/app/pages/comp-copy-confirmation/comp-copy-confirmation.js
+++ src/app/pages/comp-copy-confirmation/comp-copy-confirmation.js
@@ -1,14 +1,23 @@
 import BaseView from '~/helpers/backbone/view';
+import Calculator from '~/components/calculator/calculator';
 import {props} from '~/helpers/backbone/decorators';
 import {template} from './comp-copy-confirmation.hbs';
+import {template as strips} from '~/components/strips/strips.hbs';
 
 @props({
-    template: template
+    template: template,
+    templateHelpers: {
+        strips
+    },
+    regions: {
+        calculator: '.calculator'
+    }
 })
 export default class InterestConfirmation extends BaseView {
 
     onRender() {
         this.el.classList.add('confirmation-page');
+        this.regions.calculator.append(new Calculator());
     }
 
 }
