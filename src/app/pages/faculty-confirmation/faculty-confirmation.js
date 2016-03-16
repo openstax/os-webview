ff7bf9c3219d8bd2191500b17f4dcd585422f32f
Maher <me24@rice.edu>
2016-03-16 14:41:11 -0500

    Add Faculty Confirmation page styles

--- src/app/pages/faculty-confirmation/faculty-confirmation.js
+++ src/app/pages/faculty-confirmation/faculty-confirmation.js
@@ -1,14 +1,23 @@
 import BaseView from '~/helpers/backbone/view';
+import Calculator from '~/components/calculator/calculator';
 import {props} from '~/helpers/backbone/decorators';
 import {template} from './faculty-confirmation.hbs';
+import {template as strips} from '~/components/strips/strips.hbs';
 
 @props({
-    template: template
+    template: template,
+    templateHelpers: {
+        strips
+    },
+    regions: {
+        calculator: '.calculator'
+    }
 })
 export default class FacultyConfirmation extends BaseView {
 
     onRender() {
         this.el.classList.add('confirmation-page');
+        this.regions.calculator.append(new Calculator());
     }
 
 }
