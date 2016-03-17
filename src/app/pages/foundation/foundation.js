4024f761725e950dc5990bfe001f1404ff49a004
Zach Roehr <zach.roehr@gmail.com>
2016-03-16 23:09:36 -0500

    add html, css & js for foundation page

--- src/app/pages/foundation/foundation.js
+++ src/app/pages/foundation/foundation.js
@@ -1,9 +1,15 @@
 import BaseView from '~/helpers/backbone/view';
 import {props} from '~/helpers/backbone/decorators';
 import {template} from './foundation.hbs';
+import {template as strips} from '~/components/strips/strips.hbs';
 
 @props({
-    template: template
+    template,
+    templateHelpers: {strips}
 })
 
-export default class Foundation extends BaseView {}
+export default class Foundation extends BaseView {
+    onRender() {
+        this.el.classList.add('foundation-page');
+    }
+}
