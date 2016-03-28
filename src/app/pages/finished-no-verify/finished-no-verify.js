e76826168367a91cb401f8fa249987ccdeef4aa2
Zach Roehr <zach.roehr@gmail.com>
2016-03-28 13:43:11 -0500

    add css for verify pages & 404 page

--- src/app/pages/finished-no-verify/finished-no-verify.js
+++ src/app/pages/finished-no-verify/finished-no-verify.js
@@ -1,6 +1,11 @@
 import BaseView from '~/helpers/backbone/view';
 import {props} from '~/helpers/backbone/decorators';
 import {template} from './finished-no-verify.hbs';
+import {template as strips} from '~/components/strips/strips.hbs';
 
-@props({template})
+
+@props({
+    template: template,
+    templateHelpers: {strips}
+})
 export default class FinishedNoVerify extends BaseView {}
--- src/app/pages/finished-verify/finished-verify.js
+++ src/app/pages/finished-verify/finished-verify.js
@@ -1,6 +1,11 @@
 import BaseView from '~/helpers/backbone/view';
 import {props} from '~/helpers/backbone/decorators';
 import {template} from './finished-verify.hbs';
+import {template as strips} from '~/components/strips/strips.hbs';
 
-@props({template})
+
+@props({
+    template: template,
+    templateHelpers: {strips}
+})
 export default class FinishedVerify extends BaseView {}
