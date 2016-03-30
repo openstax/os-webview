ecc68354e50a695e189de4b9797b85070ec7014b
Roy Johnson <roy.e.johnson@rice.edu>
2016-03-30 10:35:58 -0500

    Remove Resources introductory text, add accounts_id to finish-profile

--- src/app/pages/finish-profile/finish-profile.js
+++ src/app/pages/finish-profile/finish-profile.js
@@ -42,6 +42,7 @@ export default class NewAccountForm extends ProxyWidgetView {
                 this.el.querySelector('[name=first_name]').value = userInfo.first_name;
                 this.el.querySelector('[name=last_name]').value = userInfo.last_name;
                 this.el.querySelector('[name=user_id]').value = userInfo.username;
+                this.el.querySelector('[name=Accounts_ID]').value = userInfo.accounts_id;
             } else {
                 this.el.querySelector('#problem-message').textContent = 'Could not load user information';
                 this.el.querySelector('[type="submit"]').disabled = true;
