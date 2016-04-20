1b34facf1360454aa7a7ae4aa373841ad16b7a29
Derek Kent <dak@dak.org>
2016-04-20 11:29:59 -0400

    Update dependencies

--- src/app/config.js
+++ src/app/config.js
@@ -16,10 +16,10 @@ System.config({
   },
 
   map: {
-    "babel": "npm:babel-core@5.8.34",
-    "babel-polyfill": "npm:babel-polyfill@6.3.14",
-    "babel-runtime": "npm:babel-runtime@5.8.35",
-    "backbone": "npm:backbone@1.2.3",
+    "babel": "npm:babel-core@5.8.38",
+    "babel-polyfill": "npm:babel-polyfill@6.7.4",
+    "babel-runtime": "npm:babel-runtime@5.8.38",
+    "backbone": "npm:backbone@1.3.3",
     "backbone.nativeajax": "github:akre54/Backbone.NativeAjax@0.4.3",
     "backbone.nativeview": "github:akre54/Backbone.NativeView@0.3.3",
     "core-js": "npm:core-js@1.2.6",
@@ -41,23 +41,29 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-polyfill@6.3.14": {
-      "babel-regenerator-runtime": "npm:babel-regenerator-runtime@6.3.13",
-      "babel-runtime": "npm:babel-runtime@5.8.35",
-      "core-js": "npm:core-js@1.2.6",
+    "npm:babel-polyfill@6.7.4": {
+      "babel-regenerator-runtime": "npm:babel-regenerator-runtime@6.5.0",
+      "babel-runtime": "npm:babel-runtime@5.8.38",
+      "core-js": "npm:core-js@2.2.2",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:babel-regenerator-runtime@6.3.13": {
+    "npm:babel-regenerator-runtime@6.5.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:babel-runtime@5.8.35": {
+    "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:backbone@1.2.3": {
+    "npm:backbone@1.3.3": {
       "process": "github:jspm/nodelibs-process@0.1.2",
       "underscore": "npm:underscore@1.8.3"
     },
     "npm:core-js@1.2.6": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:core-js@2.2.2": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
