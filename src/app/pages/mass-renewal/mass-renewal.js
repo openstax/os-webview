f0adbd793a10753218e51800c49b511f725e651f
Roy Johnson <roy.e.johnson@rice.edu>
2016-06-01 14:05:11 -0500

    Create Mass Renewal Form
    Create the form and router entry, but not the item in the navigation bar

---
+++ src/app/pages/mass-renewal/mass-renewal.js
@@ -0,0 +1,34 @@
+import ProxyWidgetView from '~/helpers/backbone/proxy-widget-view';
+import salesforceModel from '~/models/salesforce-model';
+import {published as titles} from '~/helpers/book-titles';
+import salesforce from '~/helpers/salesforce';
+import {on, props} from '~/helpers/backbone/decorators';
+import {template} from './mass-renewal.hbs';
+import {template as strips} from '~/components/strips/strips.hbs';
+
+@props({
+    template: template,
+    css: '/app/pages/adoption/adoption.css',
+    templateHelpers: {
+        titles,
+        urlOrigin: window.location.origin,
+        strips
+    }
+})
+export default class AdoptionForm extends ProxyWidgetView {
+    @on('change [type=text],[type=email]')
+    saveSetting(event) {
+        let varName = event.target.name;
+
+        if (varName) {
+            salesforceModel.set(varName, event.target.value);
+        }
+    }
+
+    onRender() {
+        this.el.classList.add('adoption-form');
+        salesforce.populateAdoptionStatusOptions(this.el, ['adopted', 'recommend', 'no']);
+        super.onRender();
+        salesforceModel.prefill(this.el);
+    }
+}
