64d9f91692cfa76bc3a07c01c5f2347f2dcf6334
Roy Johnson <roy.e.johnson@rice.edu>
2016-06-15 17:18:42 -0500

    Allow non-faculty to submit finish-profile
    Make faculty section elements non-required when the faculty section is
    removed from the form.

--- src/app/pages/finish-profile/faculty-section/faculty-section.js
+++ src/app/pages/finish-profile/faculty-section/faculty-section.js
@@ -10,7 +10,7 @@ import {template} from './faculty-section.hbs';
 export default class FacultySection extends ProxyWidgetView {
 
     doValidChecks() {
-        let institutionalEmailInput = this.el.querySelector('[name="00NU0000005oVQV"]'),
+        let institutionalEmailInput = this.el.querySelector('[name="00NU0000005oVQV"][required]'),
             isValid = $.testInstitutionalEmail(institutionalEmailInput);
 
         if (isValid) {
@@ -22,7 +22,16 @@ export default class FacultySection extends ProxyWidgetView {
         }
     }
 
+    setRequiredness(whether) {
+        if (this.requireds) {
+            for (let field of this.requireds) {
+                field.required = whether;
+            }
+        }
+    }
+
     onRender() {
+        this.requireds = this.el.querySelectorAll('[required]');
         salesforce.populateAdoptionStatusOptions(this.el, ['adopted', 'recommend', 'no'], true);
         super.onRender();
     }
