ebb8552dd4d7221cc93b28249808164edadaee2f
Zach Roehr <zach.roehr@gmail.com>
2016-06-17 12:24:51 -0500

    remove blog search & heading field

--- src/app/pages/blog/newsPromise.js
+++ src/app/pages/blog/newsPromise.js
@@ -3,7 +3,7 @@ import PageModel from '~/models/pagemodel';
 let newsPromise = new PageModel().fetch({
     data: {
         type: 'news.NewsArticle',
-        fields: ['slug', 'title', 'date', 'author', 'pin_to_top', 'heading', 'subheading', 'body', 'article_image']
+        fields: ['slug', 'title', 'date', 'author', 'pin_to_top', 'subheading', 'body', 'article_image']
     }
 });
 
