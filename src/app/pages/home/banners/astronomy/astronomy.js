628e032cb3cfedfaeb10b272803c94368b969ae5
Roy Johnson <roy.e.johnson@rice.edu>
2016-06-28 13:42:02 -0500

    Implement Astronomy banner

--- src/app/pages/home/banners/astronomy/astronomy.js
+++ src/app/pages/home/banners/astronomy/astronomy.js
@@ -1,4 +1,4 @@
-import BaseView from '~/helpers/backbone/view';
+import BannerView from '~/pages/home/banners/banner-view';
 import {props} from '~/helpers/backbone/decorators';
 import {template} from './astronomy.hbs';
 
@@ -6,4 +6,4 @@ import {template} from './astronomy.hbs';
     template: template,
     css: '/app/pages/home/banners/astronomy/astronomy.css'
 })
-export default class Astronomy extends BaseView {}
+export default class Astronomy extends BannerView {}
