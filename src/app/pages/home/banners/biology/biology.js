611d598e3596f385cf3c2cce8aa9a08bf77b7e55
Roy Johnson <roy.e.johnson@rice.edu>
2016-06-30 16:59:58 -0500

    Refactor homepage banners
    Use one template for all banners
    Put common CSS into home.css so individual banner css files are less
    busy.

--- src/app/pages/home/banners/biology/biology.js
+++ src/app/pages/home/banners/biology/biology.js
@@ -1,9 +1,18 @@
 import BannerView from '~/pages/home/banners/banner-view';
 import {props} from '~/helpers/backbone/decorators';
-import {template} from './biology.hbs';
+import {template} from '../banner.hbs';
 
 @props({
     template: template,
-    css: '/app/pages/home/banners/biology/biology.css'
+    css: '/app/pages/home/banners/biology/biology.css',
+    templateHelpers: {
+        subject: 'biology',
+        subjectTitle: 'Biology',
+        quote: 'live the wild life',
+        features: ['diagram', 'leaf'],
+        buttonSpec1: 'btn-gold',
+        buttonSpec2: 'btn-turquoise',
+        alignedEdge: 'left'
+    }
 })
-export default class Biology extends BannerView {}
+export default class BiologyBanner extends BannerView {}
--- src/app/pages/home/banners/chemistry/chemistry.js
+++ src/app/pages/home/banners/chemistry/chemistry.js
@@ -1,9 +1,18 @@
 import BannerView from '~/pages/home/banners/banner-view';
 import {props} from '~/helpers/backbone/decorators';
-import {template} from './chemistry.hbs';
+import {template} from '../banner.hbs';
 
 @props({
     template: template,
-    css: '/app/pages/home/banners/chemistry/chemistry.css'
+    css: '/app/pages/home/banners/chemistry/chemistry.css',
+    templateHelpers: {
+        subject: 'chemistry',
+        subjectTitle: 'Chemistry',
+        quote: 'transform',
+        features: ['element'],
+        buttonSpec1: 'btn-gold',
+        buttonSpec2: 'btn-turquoise',
+        alignedEdge: 'right'
+    }
 })
-export default class Chemistry extends BannerView {}
+export default class ChemistryBanner extends BannerView {}
