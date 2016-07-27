29208df383d9131c07926a09fe76a264fac3a990
Roy Johnson <roy.e.johnson@rice.edu>
2016-07-27 16:44:22 -0500

    Populate About Us from CMS

--- src/app/pages/about/about.js
+++ src/app/pages/about/about.js
@@ -1,7 +1,6 @@
-import LoadingView from '~/controllers/loading-view';
+import CMSPageController from '~/controllers/cms';
 import {on} from '~/helpers/controller/decorators';
 import {description as template} from './about.html';
-import bios from './bios.js';
 
 function lastName(bioEntry) {
     return bioEntry.name.substr(1 + bioEntry.name.lastIndexOf(' ')).toLowerCase();
@@ -44,11 +43,17 @@ function assignIds(teams) {
         for (const member of team.members) {
             member.id = i;
             i++;
+            // Because the image names are different
+            member.image = member.team_member_image || member.advisor_image;
         }
     }
 }
 
-export default class AboutUs extends LoadingView {
+export default class AboutUs extends CMSPageController {
+
+    static description = `OpenStax is a nonprofit based at Rice University,
+        and it's our mission to improve student access to education.
+        Read more about who we are and what we do.`;
 
     init() {
         this.template = template;
@@ -57,20 +62,43 @@ export default class AboutUs extends LoadingView {
             classes: ['about-page', 'page']
         };
 
-        bios.forEach((team) => {
-            team.members.sort((a, b) => lastName(a) > lastName(b) ? 1 : -1);
+        this.model = {bios: []};
+        this.queryPage = {
+            type: 'pages.AboutUs'
+        };
+    }
 
-            if (team.name === 'team') {
-                assignColors(team.members);
+    onDataLoaded() {
+        this.model = {
+            bios: [
+                {
+                    name: 'team',
+                    members: this.pageData.openstax_team
+                },
+                {
+                    name: 'advisors',
+                    members: this.pageData.strategic_advisors
+                }
+            ]
+        };
+        assignIds(this.model.bios);
+        assignColors(this.pageData.openstax_team);
+        this.update();
+        const updateDescription = (member) => {
+            const id = member.id;
+            const el = this.el.querySelector(`[data-id="${id}"] [data-html="description"]`);
+
+            if (el) {
+                el.innerHTML = member.description;
             }
-        });
+        };
 
-        assignIds(bios);
-        this.model = {bios};
-
-        this.description = `OpenStax is a nonprofit based at Rice University,
-            and it's our mission to improve student access to education.
-            Read more about who we are and what we do.`;
+        for (const member of this.pageData.openstax_team) {
+            updateDescription(member);
+        }
+        for (const member of this.pageData.strategic_advisors) {
+            updateDescription(member);
+        }
     }
 
     @on('click .headshot')
