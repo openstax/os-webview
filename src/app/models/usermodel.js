68b48f6085d7265dc7b2325b1d5c47bc3679388f
Roy Johnson <roy.e.johnson@rice.edu>
2016-08-22 16:06:08 -0500

    Hot fix: Use separate call for pending_verification from Salesforce

--- src/app/models/usermodel.js
+++ src/app/models/usermodel.js
@@ -21,3 +21,22 @@ class UserModel extends BaseModel {
 let userModel = new UserModel();
 
 export default userModel;
+
+const sfUrl = `${settings.apiOrigin}/api/user_salesforce`;
+
+class SFModel extends BaseModel {
+    urlRoot = sfUrl;
+
+    sync(method, model, options) {
+        if (!options.crossDomain) {
+            options.crossDomain = true;
+        }
+        if (!options.xhrFields) {
+            options.xhrFields = {};
+        }
+        options.xhrFields.withCredentials = true;
+        return super.sync(method, model, options);
+    }
+}
+
+export let sfModel = new SFModel();
