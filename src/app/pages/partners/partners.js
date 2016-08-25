0823712b5c7d0920f672336fe49c858bc9add5f0
Derek Kent <dak@dak.org>
2016-08-25 14:40:55 -0400

    Change Partners page API slug to 'pages/partners'

--- src/app/pages/partners/partners.js
+++ src/app/pages/partners/partners.js
@@ -21,7 +21,7 @@ export default class Partners extends CMSPageController {
         'integrate seamlessly with OpenStax books.';
 
     init() {
-        this.slug = 'pages/allies';
+        this.slug = 'pages/partners';
         this.template = template;
         this.css = '/app/pages/partners/partners.css';
         this.view = {
