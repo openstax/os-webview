72390db8e9f521347c19ed60edbdb4787979bc99
Derek Kent <derek.a.kent@gmail.com>
2016-08-29 09:55:53 -0400

    Start recordo cleanly without a settimeout hack (#506)

--- src/app/components/shell/shell.js
+++ src/app/components/shell/shell.js
@@ -1,6 +1,7 @@
 import {Controller} from 'superb';
 import header from './header/header';
 import footer from './footer/footer';
+import {initialize, injectButtons} from 'recordo';
 import {description as template} from './shell.html';
 
 class Shell extends Controller {
@@ -21,6 +22,12 @@ class Shell extends Controller {
     onLoaded() {
         this.regions.header.attach(header);
         this.regions.footer.attach(footer);
+
+        // Start recordo
+        initialize({ignoreAjaxResponse: true});
+        if (/collect=true/.test(window.location.search)) {
+            injectButtons();
+        }
 
         // FIX: Update pages title for the new page
         // headTitle.textContent = `${pageName[0].toUpperCase()}${pageName.slice(1)} - OpenStax`;
