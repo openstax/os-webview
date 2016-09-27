fd43929a77ecbdd13bf5aa8526cfe5c218e1767c
Michael Harrison <mwharrison@rice.edu>
2016-09-27 16:18:44 -0500

    Make TOS page editable

--- src/app/pages/tos/tos.js
+++ src/app/pages/tos/tos.js
@@ -1,7 +1,12 @@
-import {Controller} from 'superb';
+import CMSPageController from '~/controllers/cms';
+import $ from '~/helpers/$';
 import {description as template} from './tos.html';
 
-export default class Tos extends Controller {
+export default class Tos extends CMSPageController {
+
+    static description = 'Since 2012, OpenStax has saved students millions ' +
+        'through free, peer-reviewed college textbooks. Learn more about our ' +
+        'impact on the 3,000+ schools who use our books.';
 
     init() {
         this.template = template;
@@ -9,6 +14,18 @@ export default class Tos extends Controller {
         this.view = {
             classes: ['tos-page', 'page']
         };
+        this.model = {
+            'intro_heading': '',
+            'terms_of_service_content': ''
+        };
+        this.slug = 'pages/tos';
+    }
+
+    onDataLoaded() {
+        document.title = `${this.pageData.title} - OpenStax`;
+        Object.assign(this.model, this.pageData);
+        this.update();
+        $.insertHtml(this.el, this.model);
     }
 
 }
