91e260c400140c1573072ce004b4eab283d863c1
Roy Johnson <roy.e.johnson@rice.edu>
2016-09-28 15:16:51 -0500

    Revert "Make TOS page editable"

--- src/app/pages/tos/tos.js
+++ src/app/pages/tos/tos.js
@@ -1,12 +1,7 @@
-import CMSPageController from '~/controllers/cms';
-import $ from '~/helpers/$';
+import {Controller} from 'superb';
 import {description as template} from './tos.html';
 
-export default class Tos extends CMSPageController {
-
-    static description = 'Since 2012, OpenStax has saved students millions ' +
-        'through free, peer-reviewed college textbooks. Learn more about our ' +
-        'impact on the 3,000+ schools who use our books.';
+export default class Tos extends Controller {
 
     init() {
         this.template = template;
@@ -14,18 +9,6 @@ export default class Tos extends CMSPageController {
         this.view = {
             classes: ['tos-page', 'page']
         };
-        this.model = {
-            'intro_heading': '',
-            'terms_of_service_content': ''
-        };
-        this.slug = 'pages/tos';
-    }
-
-    onDataLoaded() {
-        document.title = `${this.pageData.title} - OpenStax`;
-        Object.assign(this.model, this.pageData);
-        this.update();
-        $.insertHtml(this.el, this.model);
     }
 
 }
