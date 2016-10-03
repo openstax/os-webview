74f3626f550ff7c64d1fbf809067ecf5d69abcd6
Ed Woodward <ecw1@rice.edu>
2016-10-03 08:39:45 -0500

    Added tests with correct instructions for OSX (#581)
    * Added tests with correct instructions for OSX
    
    * Fixed typo in selenium install instructions

---
+++ test/selenium/TestBiology.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import unittest, time
+
+'''
+This test
+   * Opens the Biology book page
+   * Prints out the links found on the page
+   * Some of the links should be from Cloudfront
+   * This shows if the API is working and data is available
+'''
+
+
+class TestBiology(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        propfile = open('properties.ini')
+        items = [line.rstrip('\n') for line in propfile]
+        self.base_url = items[0]
+
+    def test_ap(self):
+        driver = self.driver
+        driver.get(self.base_url + "/details/books/biology")
+        time.sleep(10)
+        print("=== Running Biology Test ===")
+
+        links = driver.find_elements_by_tag_name("a")
+        for link in links:
+            print(link.get_attribute("href"))
+
+        print("=== Biology Test Complete ===\n")
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
