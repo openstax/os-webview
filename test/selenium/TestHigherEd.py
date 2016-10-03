417c05ecc6ed7e36942d89f8ed727e2a959a80a7
Ed Woodward <ecw1@rice.edu>
2016-10-03 08:39:45 -0500

    Added tests with correct instructions for OSX (#581)
    * Added tests with correct instructions for OSX
    
    * Fixed typo in selenium install instructions

---
+++ test/selenium/TestHigherEd.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+import unittest
+
+'''
+This test
+   * Opens the Higher Ed page
+   * Verifies the CTA text from the API displays and prints out positive results
+   * This shows if the API is working and data is available
+'''
+
+
+class TestHigherEd(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        propfile = open('properties.ini')
+        items = [line.rstrip('\n') for line in propfile]
+        self.base_url = items[0]
+
+    def test_higher_ed(self):
+        driver = self.driver
+        driver.get(self.base_url + "/higher-ed")
+        driver.implicitly_wait(7)
+        print("=== Running Higher Ed Test ===")
+
+        if driver.find_element_by_xpath("//a[contains(text(),'Sign up')]"):
+            print('Sign up link found')
+
+        if driver.find_element_by_xpath("//a[contains(text(),'View Books')]"):
+            print('View Books link found')
+        if driver.find_element_by_xpath("//a[contains(text(),'Create Account')]"):
+            print('See our Impact link found')
+        if driver.find_element_by_xpath("//a[contains(text(),'Adopt')]"):
+            print('Adopt link found')
+        if driver.find_element_by_xpath("//a[contains(text(),'Contact Us')]"):
+            print('Contact Us link found')
+        if driver.find_element_by_xpath("//a[contains(text(),'Explore Our Subjects')]"):
+            print('Explore Our Subjects link found')
+        if driver.find_element_by_xpath("//a[contains(text(),'Learn More')]"):
+            print('Learn More link found')
+        if driver.find_element_by_xpath("//a[contains(text(),'Discover Free Content')]"):
+            print('Discover Free Content link found')
+
+        print("=== Higher Ed Test Complete ===\n")
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
