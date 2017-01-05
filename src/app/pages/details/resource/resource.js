f5676ceeaa4fe442d20fd39c839835b4768ea1b7
Roy Johnson <roy.e.johnson@rice.edu>
2017-01-05 13:14:54 -0600

    Make Instructor Resource links to Accounts open in current tab

--- src/app/pages/details/resource/resource.js
+++ src/app/pages/details/resource/resource.js
@@ -9,6 +9,7 @@ export default class Resource extends Controller {
         this.model = model;
         /* eslint camelcase:0 */
         this.model.linkUrl = alternateLink || model.link_document_url || model.link_external;
+        this.model.dataLocal = alternateLink ? 'true' : null;
     }
 
     onLoaded() {
