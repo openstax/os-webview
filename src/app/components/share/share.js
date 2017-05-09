4932cc07b897147800d7446ed2ead903dd9017aa
Roy Johnson <roy.e.johnson@rice.edu>
2017-05-09 09:47:01 -0500

    Add test for Share page

--- src/app/components/share/share.js
+++ src/app/components/share/share.js
@@ -3,15 +3,13 @@ import $ from '~/helpers/$';
 import {description as template} from './share.html';
 
 /* eslint-disable */
-if (!$.isNode()) {
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
-      fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-}
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id) || !fjs) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 /* eslint-enable */
 
 export default class Share extends Controller {
