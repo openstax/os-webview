6a5f639e1db550dd69b4a1fef6819b5014b66e24
Roy Johnson <roy.e.johnson@rice.edu>
2017-05-25 13:54:47 -0500

    Add babel-polypill

--- gulp/tasks/jest.js
+++ gulp/tasks/jest.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const pi = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del']
 });
 
+// Be quiet!
+gutil.log = gutil.noop;
 gulp.task('jest', function () {
     return gulp.src('test/src/**', {
         since: gulp.lastRun('jest')
