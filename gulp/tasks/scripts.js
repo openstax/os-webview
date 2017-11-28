aadee5d779361e886e6a30b204bb749ea009875d
Roy Johnson <roy.e.johnson@rice.edu>
2017-11-28 13:41:48 -0600

    No .js in the externals spec

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -211,7 +211,7 @@ function compileScriptsWebpack() {
         `${config.dest}/app/main.js`
     ]).pipe(webpack({
       // watch: true, // This causes gulp to freeze and not serve
-      externals: /settings\.js$/,
+      externals: 'settings',
       output: {
         path: path.resolve(config.dest),
         filename: "bundle.js",
