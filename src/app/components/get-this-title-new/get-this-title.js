9b63d43f87cd71b148404302007fb7699594c4ef
Roy Johnson <roy.e.johnson@rice.edu>
2018-03-13 10:13:47 -0500

    Restore old get-this-title and rename new one

--- src/app/components/get-this-title-new/get-this-title.js
+++ src/app/components/get-this-title-new/get-this-title.js
@@ -1,19 +1,30 @@
+import VERSION from '~/version';
 import {Controller} from 'superb.js';
 import {on} from '~/helpers/controller/decorators';
 import $ from '~/helpers/$';
 import userModel from '~/models/usermodel';
 import router from '~/router';
+import ModalDialog from '../dialog/dialog';
+import TocDialog from './toc-dialog/toc-dialog';
+import ModalContent from '../modal-content/modal-content';
+import OrderPrintCopy from './order-print-copy/order-print-copy';
 import {highSchoolSlugs} from '~/models/book-titles';
 import {description as template} from './get-this-title.html';
 import {description as polishTemplate} from './get-this-title-polish.html';
 
 export default class GetThisTitle extends Controller {
 
+    // TODO Add table of contents
     init(data) {
         this.template = data.slug.substr(-6) === 'polska' ? polishTemplate : template;
-        this.css = '/app/components/get-this-title/get-this-title.css';
+        this.css = `/app/components/get-this-title-new/get-this-title.css?${VERSION}`;
         this.regions = {
-            submenu: '.submenu'
+            submenu: '.submenu',
+            modal: '.modal-region',
+            toc: '.toc-region'
+        };
+        this.view = {
+            classes: ['get-this-title']
         };
 
         const isHighSchool = highSchoolSlugs.includes(data.slug);
@@ -21,6 +32,10 @@ export default class GetThisTitle extends Controller {
             data.bookstore_coming_soon, isHighSchool].find((x) => x);
 
         this.model = {
+            includeTOC: data.includeTOC,
+            tocHiddenAttribute: '',
+            tableOfContents: data.table_of_contents,
+            modalHiddenAttribute: '',
             ibookLink: data.ibook_link,
             ibookLink2: data.ibook_link_volume_2,
             kindleLink: data.kindle_link,
@@ -51,27 +66,61 @@ export default class GetThisTitle extends Controller {
         };
     }
 
+    onLoaded() {
+        if (this.regions.modal.el) {
+            const printCopyContent = new OrderPrintCopy({
+                individualLink: this.model.amazon.link,
+                amazonPrice: this.model.amazon.price,
+                bookstoreLink: this.model.bookstore.link,
+                bulkLink: this.model.isHighSchool ? '/bulk-order?this.model.slug' : null
+            });
+            const printCopyDialog = new ModalDialog({
+                title: 'Order a print copy',
+                contentComponent: printCopyContent,
+                closeDialog: () => {
+                    this.model.modalHiddenAttribute = '';
+                    this.update();
+                    document.body.classList.remove('no-scroll');
+                }
+            });
+
+            this.regions.modal.attach(printCopyDialog);
+        }
+        if (this.regions.toc.el && this.model.includeTOC) {
+            const tocContent = new TocDialog({
+                tableOfContents: this.model.tableOfContents,
+                webviewLink: this.model.webviewLink
+            });
+            const tocDialog = new ModalDialog({
+                title: 'Table of contents',
+                contentComponent: tocContent,
+                closeDialog: () => {
+                    this.model.tocHiddenAttribute = '';
+                    this.update();
+                    document.body.classList.remove('no-scroll');
+                }
+            });
+
+            this.regions.toc.attach(tocDialog);
+        }
+    }
+
+    onClose() {
+        // If they navigate while a modal is open
+        document.body.classList.remove('no-scroll');
+    }
+
     @on('click .btn')
     blurAfterClick(event) {
         event.target.blur();
     }
 
-    @on('click .show-pdf-submenu')
-    showPdfSubmenu(event) {
-        event.preventDefault();
-        this.model.submenu = 'pdf';
-        this.update();
-        // Focus on first link
-        this.el.querySelector('.pdf-submenu a').focus();
-    }
-
     @on('click .show-print-submenu')
     showPrintSubment(event) {
         event.preventDefault();
-        this.model.submenu = 'print';
+        this.model.modalHiddenAttribute = null;
+        document.body.classList.add('no-scroll');
         this.update();
-        // Focus on first link
-        this.el.querySelector('.print-submenu a').focus();
     }
 
     @on('click .submenu .remover')
@@ -95,6 +144,14 @@ export default class GetThisTitle extends Controller {
                 router.navigate('/give?student', {path: '/give?student'});
             }
         });
+    }
+
+    @on('click .show-toc')
+    showToc(event) {
+        event.preventDefault();
+        this.model.tocHiddenAttribute = null;
+        document.body.classList.add('no-scroll');
+        this.update();
     }
 
 }
--- src/app/pages/details-new/details-tab/details-tab.js
+++ src/app/pages/details-new/details-tab/details-tab.js
@@ -1,7 +1,7 @@
 import VERSION from '~/version';
 import {Controller} from 'superb.js';
 import $ from '~/helpers/$';
-import GetThisTitle from '~/components/get-this-title/get-this-title';
+import GetThisTitle from '~/components/get-this-title-new/get-this-title';
 import LetUsKnow from '../let-us-know/let-us-know';
 import {description as template} from './details-tab.html';
 
