cfaa9787bb1a5996191579a66c6f0836664aa581
nuttawit261 <nuttawit@baalspots.com>
2018-03-15 03:42:04 +0900

    Add change testimonial on tab click, css for panel content

--- src/app/pages/institutional-partnership/institutional-partnership.js
+++ src/app/pages/institutional-partnership/institutional-partnership.js
@@ -1,4 +1,5 @@
 import VERSION from '~/version';
+import {on} from '~/helpers/controller/decorators';
 import {Controller} from 'superb.js';
 import {description as template} from './institutional-partnership.html';
 
@@ -11,6 +12,24 @@ export default class Institutional extends Controller {
             tag: 'main',
             classes: ['institutional-page', 'page']
         };
+    }
+
+    onLoaded() {
+        this.programTestimonial = this.el.querySelector('.testimonial-program');
+        this.applicationTestimonial = this.el.querySelector('.testimonial-application');
+        this.programTestimonial.classList.add('is-show');
+    }
+
+    @on('click #btn-tab1')
+    showProgramTestimonial() {
+    	this.programTestimonial.classList.add('is-show');
+        this.applicationTestimonial.classList.remove('is-show');
+    }
+
+    @on('click #btn-tab2')
+    showApplicationTestimonial() {
+        this.programTestimonial.classList.remove('is-show');
+        this.applicationTestimonial.classList.add('is-show');
     }
 
 }
