de941f6e38661fb46166a708bcdeb299a846ef54
Roy Johnson <roy.e.johnson@rice.edu>
2018-11-12 15:27:29 -0600

    Insert publication details on phone view

--- src/app/pages/details/phone-view/publication-details/publication-details.js
+++ src/app/pages/details/phone-view/publication-details/publication-details.js
@@ -1,4 +1,5 @@
 import {Controller} from 'superb.js';
+import $ from '~/helpers/$';
 import {description as template} from './publication-details.html';
 import {description as templatePolish} from './publication-details-polish.html';
 
@@ -7,6 +8,10 @@ export default class PublicationDetails extends Controller {
     init(model, polish) {
         this.model = model;
         this.template = polish ? templatePolish : template;
+    }
+
+    onLoaded() {
+        $.insertHtml(this.el, this.model);
     }
 
 }
