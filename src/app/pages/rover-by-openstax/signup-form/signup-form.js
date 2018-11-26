3c05cd3ff5a5c32f69f6da89749492223f71ee56
Roy Johnson <roy.e.johnson@rice.edu>
2018-11-26 14:04:20 -0600

    Rover form: Only show number of students field for instructors

--- src/app/pages/rover-by-openstax/signup-form/signup-form.js
+++ src/app/pages/rover-by-openstax/signup-form/signup-form.js
@@ -3,41 +3,30 @@ import VERSION from '~/version';
 import FormInput from '~/components/form-input/form-input';
 import FormSelect from '~/components/form-select/form-select';
 import SalesforceForm from '~/controllers/salesforce-form';
+import {description as template} from './signup-form.html';
 
 export default class SignupForm extends SalesforceForm {
 
     init(roles) {
-        this.template = () => '';
+        this.template = template;
         this.roles = roles;
-    }
-
-    update() {
-        const disable = ! Boolean(this.selectedRole);
-        const elements = Array.from(document.querySelectorAll('input[required],[type="submit"]'));
-
-        elements.forEach((el) => {
-            el.disabled = disable;
-        });
-    }
-
-    onLoaded() {
-        const region = this.regions.self;
-        const validationMessage = function (name) {
-            return this.validated ? this.el.querySelector(`[name="${name}"]`).validationMessage : '';
+        this.regions = {
+            selector: '.selector',
+            common: '.common-fields',
+            instructor: '.instructor-only'
         };
-        const roleOptions = this.roles
-            .map((opt) => ({label: opt.display_name, value: opt.salesforce_name}));
-        const roleSelector = new FormSelect({
-            name: 'Role__c',
-            instructions: 'I am a',
-            validationMessage: () => '',
-            placeholder: 'Please select one',
-            options: roleOptions
-        }, (newValue) => {
-            this.selectedRole = newValue;
-            this.update();
-        });
-        const inputs = [
+        this.selectedRole = null;
+        this.commonInputs = null;
+        this.instructorInput = null;
+    }
+
+    validationMessage(name) {
+        return this.validated ? this.el.querySelector(`[name="${name}"]`).validationMessage : '';
+    };
+
+    createCommonInputs() {
+        const validationMessage = this.validationMessage.bind(this);
+        const commonInputs = [
             new FormInput({
                 name: 'first_name',
                 type: 'text',
@@ -70,22 +59,62 @@ export default class SignupForm extends SalesforceForm {
                 autocomplete: 'organization',
                 validationMessage,
                 suggestions: []
-            }),
-            new FormInput({
-                name: 'Number_of_Students__c',
-                label: 'How many students do you teach each semester?',
-                type: 'number',
-                min: '1',
-                required: true,
-                validationMessage
             })
         ];
 
-        region.attach(roleSelector);
-        inputs.forEach((i) => {
-            region.append(i);
+        commonInputs.forEach((c) => this.regions.common.append(c));
+        return commonInputs;
+    }
+
+    createInstructorInput() {
+        const validationMessage = this.validationMessage.bind(this);
+        const instructorInput = new FormInput({
+            name: 'Number_of_Students__c',
+            label: 'How many students do you teach each semester?',
+            type: 'number',
+            min: '1',
+            required: true,
+            validationMessage
         });
-        setTimeout(() => this.update(), 200);
+
+        this.regions.instructor.append(instructorInput);
+        return instructorInput;
+    }
+
+
+    onLoaded() {
+        const roleOptions = this.roles
+            .map((opt) => ({label: opt.display_name, value: opt.salesforce_name}));
+        const roleSelector = new FormSelect({
+            name: 'Role__c',
+            instructions: 'I am a',
+            validationMessage: () => '',
+            placeholder: 'Please select one',
+            options: roleOptions
+        }, (newValue) => {
+            this.selectedRole = newValue;
+            this.update();
+        });
+
+        this.regions.selector.attach(roleSelector);
+    }
+
+    // eslint-disable-next-line complexity
+    onUpdate() {
+        if (!this.selectedRole) {
+            return;
+        }
+        if (!this.commonInputs) {
+            this.commonInputs = this.createCommonInputs();
+        }
+        if (!['Faculty', 'Adjunct Faculty'].includes(this.selectedRole)) {
+            if (this.instructorInput) {
+                this.instructorInput.detach();
+                this.instructorInput = null;
+            }
+        } else if (!this.instructorInput) {
+            this.instructorInput = this.createInstructorInput();
+        }
     }
 
 }
