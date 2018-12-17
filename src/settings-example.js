1e9a8a2cd0577461f9b447293930eaca334f0b9e
Asim Aziz <asim.aziz@live.com>
2018-12-17 21:08:53 +0500

    resolve conflict after rebse with master

--- src/settings-example.js
+++ src/settings-example.js
@@ -2,11 +2,7 @@ const settings = {
     accountHref: 'https://accounts-dev.openstax.org',
     analyticsID: 'UA-73668038-3',
     apiOrigin: 'https://cms-dev.openstax.org',
-<<<<<<< 388314e2a074727d5b9e4767c280e39201260335
     buildVersion: '2.7.0',
-=======
-    buildVersion: '2.6.0',
->>>>>>> resolve submit your testimonial link issue for mobile display
     tagManagerID: 'GTM-W6N7PB',
     testingEnvironment: (/(localhost|oscms-)/).test(window.location.hostname),
     titleSuffix: ' - OpenStax',
