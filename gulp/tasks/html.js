3a8bb1c0420f7abf079a5243e915a8e6f4f1aa79
Nathan Stitt <nathan@stitt.org>
2019-04-23 15:56:52 -0500

    Hash generated script and style files (#1386)
    * operate on pre-generated file in dest directory
    
    Otherwise it will overwrite it
    
    * enforce 4 space indent and other prefs
    
    https://editorconfig.org is a standard way for multiple
    editors to configure themselves to the project's preferences
    
    * add url prefix for building for distribution
    
    * add gulp-rev for inserting content hash into filenames
    
    * split HTML task into dev and dist variants
    
    * replace hard coded versions with substitution markers
    
    * hash generated build files
    
     * Generate a content has for the build files and rename them to
       include it
     * update the index.html file so it refers to the hashed file
     * Adds dist prefix for hosting under cloudfront

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,19 +1,64 @@
 const gulp = require('gulp');
 const config = require('../config');
 const pi = require('gulp-load-plugins')();
+const rev = require('gulp-rev')
 
-function html() {
-    return gulp.src(`${config.dest}/*.html`, {
-        since: gulp.lastRun(html)
+const replaceURLs = (mapping) => {
+    let transform = gulp.src(`${config.src}/*.html`)
+    Object.keys(mapping).forEach((replacement) => {
+        transform = transform.pipe(
+            pi.replace(
+                new RegExp(`@${replacement}_URL@`, "g"),
+                mapping[replacement],
+            )
+        )
     })
-    .pipe(pi.replace(/@VERSION@/g, config.version))
-    .pipe(gulp.dest(config.dest));
+    return transform.pipe(gulp.dest(config.dest));
 }
 
+function hashFile(name) {
+    return () =>
+        gulp.src(`${config.dest}/${name}`)
+            .pipe(rev())
+            .pipe(gulp.dest(config.dest))
+            .pipe(rev.manifest({
+                path: `${config.dest}/rev-manifest.json`,
+                base: config.dest,
+                merge: true,
+            }))
+            .pipe(gulp.dest(config.dest))
+}
+
+// Don't need to do anything fancy in development, just replace the filename
+function devHTML() {
+    return replaceURLs({
+        BUNDLE:   '/bundle.js',
+        STYLES:   '/styles/main.css',
+        SETTINGS: '/settings.js',
+    })
+}
 function watchHtml() {
-    gulp.watch(`${config.dest}/*.html`, config.watchOpts)
-    .on('change', html);
+    gulp.watch(`${config.src}/*.html`, config.watchOpts)
+        .on('change', devHTML);
 }
+exports.devHTML = devHTML
+exports.devHTML.watch = watchHtml
 
-exports.html = html;
-exports.html.watch = watchHtml;
+// when building production distribution files,
+// include the file's SHA in the filenamnes
+function distHTML(distDone) {
+    return gulp.series(
+        hashFile('bundle.js'),   // while it might seem like this could be "parallel", that causes the
+        hashFile('settings.js'), // rev-manifest.json file to become mangled when both write to it
+        hashFile('styles/main.css'),
+        () => {
+            const revManifest = require(`../../${config.dest}/rev-manifest.json`);
+            return replaceURLs({
+                STYLES:   `${config.distUrlPrefix}/styles/${revManifest['main.css']}`,
+                BUNDLE:   `${config.distUrlPrefix}/${revManifest['bundle.js']}`,
+                SETTINGS: `${config.distUrlPrefix}/${revManifest['settings.js']}`,
+            })
+        }
+    )(distDone)
+}
+exports.distHTML = distHTML
