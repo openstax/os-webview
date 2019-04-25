1c09d0201b4553717c0c098e02cea6d56afc4633
Roy Johnson <roy.e.johnson@rice.edu>
2019-04-25 15:34:23 -0500

    Replace adoption snapshot test

--- test/src/pages/adoption/adoption.test.js
+++ test/src/pages/adoption/adoption.test.js
@@ -3,12 +3,13 @@ import AdoptionForm from '~/pages/adoption/adoption';
 import instanceReady from '../../../helpers/instance-ready';
 
 describe('AdoptionForm', () => {
-    const {instance:p, ready} = instanceReady(AdoptionForm);
+    const {instance, ready} = instanceReady(AdoptionForm);
 
-    it('matches snapshot', () =>
+    it('hides role selector after first page', () =>
         ready.then(() => {
-            console.log('To update snapshot: node_modules/.bin/jest --updateSnapshot --testNamePattern=AdoptionForm');
-            expect(p.el.innerHTML).toMatchSnapshot();
+            expect(instance.roleSelector.isHidden).toBe(false);
+            instance.onPageChange(1);
+            expect(instance.roleSelector.isHidden).toBe(true);
         })
     );
 });
