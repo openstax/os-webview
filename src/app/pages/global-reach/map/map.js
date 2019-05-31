744b4e1aca798fca0c3c8b72e3a9f6a1640611f9
Berk Alp Yakici <berkalp.y@gmail.com>
2019-05-31 10:15:20 -0500

    Query by name instead of id to avoid id mismatch across different environments

--- src/app/pages/global-reach/map/map.js
+++ src/app/pages/global-reach/map/map.js
@@ -52,7 +52,8 @@ export default class Map1 extends Controller {
             (async () => {
                 try {
                     const value = el.features[0].properties.id;
-                    const response = await fetch(`${settings.apiOrigin}${settings.apiPrefix}/schools/?id=${value}`);
+                    const value_name = el.features[0].properties.name;
+                    const response = await fetch(`${settings.apiOrigin}${settings.apiPrefix}/schools/?name=${value_name}`);
                     const data = await response.json();
 
                     if (data.length) {
