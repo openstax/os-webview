5a935d2764f9455bdf912267e48bd8012b5cffdd
Roy Johnson <roy.e.johnson@rice.edu>
2019-06-11 10:43:38 -0500

    Limit TOC to viewport
    And keep put-away icon always visible

--- src/app/pages/details/details-tab/toc-drawer/toc-drawer.js
+++ src/app/pages/details/details-tab/toc-drawer/toc-drawer.js
@@ -1,19 +1,13 @@
 import componentType from '~/helpers/controller/init-mixin';
-import busMixin from '~/helpers/controller/bus-mixin';
 import Contents from '~/pages/details/contents/contents';
-import {on} from '~/helpers/controller/decorators';
-import {description as template} from './toc-drawer.html';
-import css from './toc-drawer.css';
 
 const spec = {
-    template,
-    css,
     view: {
         classes: ['toc-drawer']
     }
 };
 
-export default class extends componentType(spec, busMixin) {
+export default class extends componentType(spec) {
 
     onLoaded() {
         const contentPane = new Contents(
@@ -21,12 +15,7 @@ export default class extends componentType(spec, busMixin) {
             {tag: 'ol', classes: ['table-of-contents']}
         );
 
-        this.regionFrom('.toc-container').append(contentPane);
-    }
-
-    @on('click .close-toc')
-    closeToc() {
-        this.emit('close-toc');
+        this.regions.self.append(contentPane);
     }
 
 }
