1d6366854ac82c1e011206266f43a6c583bec0b0
Roy Johnson <roy.e.johnson@rice.edu>
2019-06-25 11:35:59 -0500

    Fix general pages (#1466)
    `slug` key in spec was indicating it should do standard CMS data fetch. It should not.
    
    Make embedded scripts work in general pages

--- src/app/pages/general/general.js
+++ src/app/pages/general/general.js
@@ -7,8 +7,7 @@ const spec = {
     view: {
         classes: ['general', 'page'],
         tag: 'main'
-    },
-    slug: ''
+    }
 };
 const BaseClass = componentType(spec, canonicalLinkMixin);
 
@@ -24,6 +23,14 @@ export default class General extends BaseClass {
             .then((r) => r.text())
             .then((html) => {
                 this.el.innerHTML = html;
+                const div = document.createElement('div');
+                // Scripts have to be inserted as nodes, not as innerHTML, so swap
+                // each of them out and back in
+                Array.from(this.el.querySelectorAll('script'))
+                    .forEach((s) => {
+                        s.parentNode.replaceChild(div, s);
+                        s.parentNode.replaceChild(s, div);
+                    });
             });
     }
 
