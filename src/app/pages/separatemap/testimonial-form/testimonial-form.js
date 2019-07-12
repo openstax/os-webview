673282749aa62c80f86861e0e204c918d65df919
Roy Johnson <roy.e.johnson@rice.edu>
2019-07-12 14:01:00 -0500

    Use bus interface for FormSelect

--- src/app/pages/separatemap/testimonial-form/testimonial-form.js
+++ src/app/pages/separatemap/testimonial-form/testimonial-form.js
@@ -61,8 +61,6 @@ export default class TestimonialForm extends componentType(spec, busMixin) {
         if (!this.listeningForResponse) {
             this.listeningForResponse = true;
             this.el.querySelector('#form-response').addEventListener('load', hideDialog);
-
-            // Some sort of emit comes here?
         }
     }
 
