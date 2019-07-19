2e2f9355dd22cc2670b4050fcbfc6471ebba6692
Roy Johnson <roy.e.johnson@rice.edu>
2019-07-19 10:03:08 -0500

    Implement cookie notice as footer-dialog (#1491)
    * Implement cookie notice as footer-dialog
    
    * Fix lint problems that sneaked into earlier commit
    
    Copied get-this-title.js from other PR that works

--- src/app/components/modal-content/modal-content.js
+++ src/app/components/modal-content/modal-content.js
@@ -1,25 +1,21 @@
-import {Controller} from 'superb.js';
+import componentType from '~/helpers/controller/init-mixin';
 import css from './modal-content.css';
 import shellBus from '~/components/shell/shell-bus';
 
-export default class ModalContent extends Controller {
+const spec = {
+    css,
+    view: {
+        tag: 'modal-content',
+        classes: ['page-overlay']
+    }
+};
+
+export default class ModalContent extends componentType(spec) {
 
     /**
      * Provides a background overlay that covers the page
      * so that only the content is clearly visible and centered
      */
-    init(content) {
-        this.content = content;
-        this.view = {
-            tag: 'modal-content',
-            classes: ['page-overlay']
-        };
-        this.css = css;
-    }
-
-    template() {
-    }
-
     onLoaded() {
         this.regions.self.append(this.content);
         shellBus.emit('with-modal');
---
+++ src/app/components/shell/cookie-notice/cookie-notice.js
@@ -0,0 +1,67 @@
+import componentType from '~/helpers/controller/init-mixin';
+import busMixin from '~/helpers/controller/bus-mixin';
+import {description as template} from './cookie-notice.html';
+import css from './cookie-notice.css';
+import {on} from '~/helpers/controller/decorators';
+import accountsModel from '~/models/usermodel';
+import shellBus from '../shell-bus';
+
+const spec = {
+    template,
+    css,
+    view: {
+        classes: ['cookie-notice']
+    }
+};
+
+const cookie = {
+    get hash() {
+        return decodeURIComponent(document.cookie)
+            .split('; ')
+            .reduce((a, b) => {
+                const [key, val] = b.split('=');
+
+                a[key] = val;
+                return a;
+            }, {});
+    },
+    setKey(key) {
+        document.cookie = `${key}=true; expires=Tue, 19 Jan 2038 03:14:07 GMT`;
+    }
+};
+const ACKNOWLEDGEMENT_KEY = 'cookie_notice_acknowledged';
+
+function acknowledged() {
+    return cookie.hash[ACKNOWLEDGEMENT_KEY];
+}
+
+function acknowledge() {
+    cookie.setKey(ACKNOWLEDGEMENT_KEY);
+}
+
+class CookieNotice extends componentType(spec, busMixin) {
+
+    @on('click button.primary')
+    dismissPermanently() {
+        acknowledge();
+        this.emit('close');
+    }
+
+}
+
+export default function showNoticeIfNeeded() {
+    accountsModel.load().then((response) => {
+        if ('id' in response && !acknowledged()) {
+            const cookieNotice = new CookieNotice();
+
+            shellBus.emit('showDialog', () => ({
+                title: 'Privacy and cookies',
+                content: cookieNotice,
+                customClass: 'footer-style'
+            }));
+            cookieNotice.on('close', () => {
+                shellBus.emit('hideDialog');
+            });
+        }
+    });
+}
