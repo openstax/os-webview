c4b9aac059a0a5ca402fc1556132a2db1abeb1a4
Roy Johnson <roy.e.johnson@rice.edu>
2019-09-11 12:32:58 -0500

    Fix external link test (#1552)
    * Fix external link test
    
    Was missing parentheses :flushed:
    
    * Update analytics.js
    
    `userid` -> `userId`

--- test/src/helpers/link.test.js
+++ test/src/helpers/link.test.js
@@ -10,8 +10,21 @@ describe('stripOpenStaxDomain', () => {
         expect(linkHelper.stripOpenStaxDomain(url)).toBe('/subjects');
     });
     it('leaves oxauth', () => {
-        const url = `${window.location.origin}/oxauth`;
+        const url = `${window.location.origin}/oxauth/logout`;
 
         expect(linkHelper.stripOpenStaxDomain(url)).toBe(url);
+    });
+    it('leaves oxauth on openstax.org', () => {
+        const url = 'https://openstax.org/oxauth/logout';
+
+        expect(linkHelper.stripOpenStaxDomain(url)).toBe(url);
+    });
+    it('recognizes external', () => {
+        const urls = [
+            `${window.location.origin}/oxauth/logout`,
+            'https://openstax.org/oxauth/logout/'
+        ];
+
+        urls.forEach((url) => expect(linkHelper.isExternal(url)).toBe(true));
     });
 });
