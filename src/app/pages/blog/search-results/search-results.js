f84c383bad8e084b2b7317b4038906762025ea5b
Roy Johnson <roy.e.johnson@rice.edu>
2019-09-30 11:13:25 -0500

    New blog search results page

--- src/app/pages/blog/search-results/search-results.js
+++ src/app/pages/blog/search-results/search-results.js
@@ -1,9 +1,41 @@
-import MoreStories from '../more-stories/more-stories';
+import componentType from '~/helpers/controller/init-mixin';
+import {description as template} from './search-results.html';
+import css from './search-results.css';
+import {loadArticles} from '../more-stories/more-stories';
+import Paginator from '~/components/paginator/paginator';
 import {blurbModel} from '../article-summary/article-summary';
 import {fetchFromCMS} from '~/helpers/controller/cms-mixin';
 import uniqBy from 'lodash/uniqBy';
 
-export default class extends MoreStories {
+const RESULTS_PER_PAGE = 10;
+const spec = {
+    template,
+    css,
+    view: {
+        classes: ['search-results']
+    },
+    regions: {
+        cards: '.cards',
+        paginator: '.paginator'
+    },
+    currentPage: 0,
+    allArticles: []
+};
+
+export default class extends componentType(spec) {
+
+
+    get firstIndex() {
+        return this.currentPage * RESULTS_PER_PAGE;
+    }
+
+    get endBefore() {
+        return Math.min(this.firstIndex + RESULTS_PER_PAGE, this.allArticles.length);
+    }
+
+    get articles() {
+        return this.allArticles.slice(this.firstIndex, this.endBefore);
+    }
 
     refreshResults() {
         const searchParam = window.location.search.substr(1);
@@ -15,12 +47,19 @@ export default class extends MoreStories {
                     this.regions.cards.el.textContent = 'No matching results were found.';
                     return;
                 }
-                this.articles = uniqBy(results, 'id')
+                this.allArticles = uniqBy(results, 'id')
                     .map((data) => {
                         data.heading = data.title;
                         return blurbModel(data.slug, data);
                     });
-                this.loadArticles();
+                loadArticles(this.regions.cards, this.articles);
+                this.updatePaginator({
+                    searchTerm: decodeURIComponent(searchParam),
+                    resultRange: `${this.firstIndex + 1}-${this.endBefore}`,
+                    totalResults: this.allArticles.length,
+                    pages: Math.ceil(this.allArticles.length / RESULTS_PER_PAGE),
+                    currentPage: this.currentPage + 1
+                });
             },
             (err) => {
                 console.warn(`Failed ${slug}:`, err);
@@ -28,10 +67,28 @@ export default class extends MoreStories {
         );
     }
 
+    updatePaginator(props) {
+        if (!this.paginator) {
+            this.paginator = new Paginator(Object.assign({
+                el: this.regions.paginator.el
+            }, props));
+            this.paginator.on('change', (newPage) => {
+                this.currentPage = newPage - 1;
+                this.paginator.emit('update-props', {
+                    currentPage: this.currentPage + 1,
+                    resultRange: `${this.firstIndex + 1}-${this.endBefore}`
+                });
+                loadArticles(this.regions.cards, this.articles);
+            });
+        } else {
+            this.paginator.emit('update-props', props);
+        }
+    }
+
     onLoaded() {
+        this.refreshResults();
         this.boundRR = () => this.refreshResults();
         window.addEventListener('popstate', this.boundRR);
-        this.refreshResults();
     }
 
     onClose() {
