0d192b5096cb420f68e89669b0d33b6bd979457f
Roy Johnson <roy.e.johnson@rice.edu>
2019-10-29 09:53:58 -0500

    Fix tests

--- test/src/data/print-order.js
+++ test/src/data/print-order.js
@@ -5,16 +5,16 @@ export default {
         "seo_title": "",
         "search_description": "",
         "type": "pages.PrintOrder",
-        "detail_url": "https://cms-dev.openstax.org/api/v2/pages/225/",
-        "html_url": "http://openstax.org/print-order/",
+        "detail_url": "https://cms-dev.openstax.org/apps/cms/api/v2/pages/225/",
+        "html_url": "http://cms-dev.openstax.org/print-order/",
         "show_in_menus": false,
         "first_published_at": "2018-07-17T16:16:42.365803-05:00",
         "parent": {
             "id": 29,
             "meta": {
                 "type": "pages.HomePage",
-                "detail_url": "https://cms-dev.openstax.org/api/v2/pages/29/",
-                "html_url": "http://openstax.org/"
+                "detail_url": "https://cms-dev.openstax.org/apps/cms/api/v2/pages/29/",
+                "html_url": "http://cms-dev.openstax.org/"
             },
             "title": "Openstax Homepage"
         }
@@ -26,75 +26,77 @@ export default {
     "featured_providers": [
         {
             "id": "a4441e30-baf8-4242-aab2-07abc5c4fae4",
-            "type": "provider",
             "value": {
-                "name": "indiCo",
-                "cta": "Order from indiCo",
                 "url": "http://www.goindico.com/programs",
-                "blurb": "indiCo handles the fulfillment and distribution of OpenStax textbooks to college bookstore and K12 schools.",
-                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/indico-updated.png"
-            }
+                "cta": "Order from indiCo",
+                "name": "indiCo",
+                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/indico-updated.png",
+                "blurb": "indiCo handles the fulfillment and distribution of OpenStax textbooks to college bookstore and K12 schools."
+            },
+            "type": "provider"
         },
         {
             "id": "6af366db-23a8-40e7-9545-76b673378de2",
-            "type": "provider",
             "value": {
-                "name": "MBS Textbook Exchange, LLC",
-                "cta": "Order from MBS",
                 "url": "https://servicecenter.mbsbooks.com/",
-                "blurb": "MBS handles the fulfillment and distribution of OpenStax textbooks to college bookstore and K12 schools.",
-                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/mbs-logo.original.png"
-            }
+                "cta": "Order from MBS",
+                "name": "MBS Textbook Exchange, LLC",
+                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/mbs-logo.original.png",
+                "blurb": "MBS handles the fulfillment and distribution of OpenStax textbooks to college bookstore and K12 schools."
+            },
+            "type": "provider"
         },
         {
             "id": "a0261b34-1f22-482b-a880-9443e28f9afb",
-            "type": "provider",
             "value": {
-                "name": "TriLiteral",
-                "cta": "Order from TriLiteral",
                 "url": "http://www.triliteral.org/ordering-and-returns/",
-                "blurb": "TriLiteral handles the fulfillment and distribution of the OpenStax textbooks to college bookstores and K12 schools.",
-                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/TLT.png"
-            }
+                "cta": "Order from TriLiteral",
+                "name": "TriLiteral",
+                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/TLT.png",
+                "blurb": "TriLiteral handles the fulfillment and distribution of the OpenStax textbooks to college bookstores and K12 schools."
+            },
+            "type": "provider"
         }
     ],
     "other_providers_intro_blurb": "If you are not interested in the high-quality hard cover textbooks manufactured by OpenStax, we still have options for you. The following providers can assist you with sourcing soft cover, black and white versions of our textbooks:",
     "providers": [
         {
             "id": "e6c81bd2-9dd3-4ad5-815e-d3eb0c5e2dd2",
-            "type": "provider",
             "value": {
-                "name": "XanEdu",
-                "cta": "Order from XanEdu",
                 "url": "http://info.xanedu.com/openstax",
-                "blurb": "OpenStax is now available from XanEdu Custom Solutions! All OpenStax titles printed by XanEdu are perfect-bound soft cover with black and white interior pages.",
-                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/XanEdu_PMS312C_logo_with_tagline_w250px.png"
-            }
+                "cta": "Order from XanEdu",
+                "name": "XanEdu",
+                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/XanEdu_PMS312C_logo_with_tagline_w250px.png",
+                "blurb": "OpenStax is now available from XanEdu Custom Solutions! All OpenStax titles printed by XanEdu are perfect-bound soft cover with black and white interior pages."
+            },
+            "type": "provider"
         },
         {
             "id": "e9a6f37c-af11-40fe-8c7c-89cad392fcc4",
-            "type": "provider",
             "value": {
-                "name": "LAD Custom Publishing",
-                "cta": "Order from LAD",
                 "url": "http://ladcustompub.com/",
-                "blurb": "LAD Custom Publishing is a course pack provider that can add OpenStax texts to your course pack.",
-                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/LAD-Logo-Web.png"
-            }
+                "cta": "Order from LAD",
+                "name": "LAD Custom Publishing",
+                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/LAD-Logo-Web.png",
+                "blurb": "LAD Custom Publishing is a course pack provider that can add OpenStax texts to your course pack."
+            },
+            "type": "provider"
         },
         {
             "id": "fb54f7c7-c6e4-4dbf-8f54-dda5cd30b480",
-            "type": "provider",
             "value": {
-                "name": "Montezuma Publishing",
-                "cta": "Order from Montezuma",
                 "url": "http://www.montezumapublishing.com/crsmat/OpenStax.aspx",
-                "blurb": "Montezuma Publishing provides complete or customized low-cost copies of OpenStax textbooks.",
-                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/montezuma_logo.original.png"
-            }
+                "cta": "Order from Montezuma",
+                "name": "Montezuma Publishing",
+                "icon": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/original_images/montezuma_logo.original.png",
+                "blurb": "Montezuma Publishing provides complete or customized low-cost copies of OpenStax textbooks."
+            },
+            "type": "provider"
         }
     ],
-    "isbn_download_url": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/documents/print_isbns_2_2.pdf",
-    "isbn_cta": "Download a list of ISBN numbers",
+    "us_isbn_download_url": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/documents/print_isbns_2_2.pdf",
+    "us_isbn_cta": "US ISBN numbers and prices",
+    "canadian_isbn_download_url": "https://d3bxy9euw4e147.cloudfront.net/oscms-dev/media/documents/canada-isbn-list.pdf",
+    "canadian_isbn_cta": "Canada ISBN numbers and prices",
     "promote_image": null
 };
