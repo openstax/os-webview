6304b78c7ae2b411ff6692ceefb8110f621f4ed0
Roy Johnson <roy.e.johnson@rice.edu>
2019-12-05 16:02:00 -0600

    Reset recommended callout after Jan 15 2020 (#1631)
    Issue #1604

--- src/app/components/get-this-title/callout-counter.js
+++ src/app/components/get-this-title/callout-counter.js
@@ -1,12 +1,29 @@
 let slug = null;
 let incremented;
+const resetDate = new Date('2020/01/15');
 
 export default {
     setSlug(newSlug) {
         if (newSlug !== slug) {
             slug = newSlug;
             incremented = false;
+            if (slug && Date.now() > resetDate && this.lastReset < resetDate) {
+                this.count = 0;
+                this.lastReset = Date.now();
+            }
         }
+    },
+    get resetIndex() {
+        return `callout-reset-${slug}`;
+    },
+    get lastReset() {
+        const savedValue = Number(localStorage.getItem(this.resetIndex)) ||
+            Number(resetDate) - 100;
+
+        return new Date(savedValue);
+    },
+    set lastReset(now) {
+        localStorage.setItem(this.resetIndex, Number(now).toString());
     },
     get index() {
         if (slug === null) {
