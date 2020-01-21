38fe076abdb52ccc62294629af6faf6cc460aea6
Roy Johnson <roy.e.johnson@rice.edu>
2020-01-21 10:46:39 -0600

    Fix errata corrected messaging (#1651)
    Issue #812

--- src/app/helpers/errata.js
+++ src/app/helpers/errata.js
@@ -1,12 +1,13 @@
-function approvedStatuses(detail) {
-    const posted = new Date(detail.created);
+// eslint-disable-next-line complexity
+export function approvedStatuses(created) {
+    const posted = new Date(created);
     const m = posted.getMonth();
     const y = posted.getFullYear();
-    const postedSpring = m > 1 && m < 9;
-    const correctionSeason = postedSpring ? `Fall ${y}` : `Spring ${y + 1}`;
-    const correctionDate = new Date(postedSpring ? `${y}/11/1` : `${y + 1}/3/1`);
-    const done = Date.now() > correctionDate;
-    const barStatus = done ? 'Corrected' : 'Will correct';
+    const correctionYear = m < 2 ? y : y + 1;
+    const postedSpring = m > 10 || m < 2;
+    const correctionSeason = `${postedSpring ? 'Fall' : 'Spring'} ${correctionYear}`;
+    const correctionDate = new Date(`${correctionYear}/${postedSpring ? 11 : 3}/1`);
+    const barStatus = Date.now() > correctionDate ? 'Corrected' : 'Will correct';
 
     return {
         status: `${barStatus} ${correctionSeason}`,
@@ -24,7 +25,7 @@ export function getDisplayStatus(detail) {
     if (['New', 'Editorial Review'].includes(detail.status)) {
         result.status = 'In Review';
     } else if (detail.resolution === 'Approved') {
-        Object.assign(result, approvedStatuses(detail));
+        Object.assign(result, approvedStatuses(detail.created));
     } else if (detail.status === 'Completed' && detail.resolution === 'Duplicate') {
         result.status = result.barStatus = 'Duplicate';
     } else {
