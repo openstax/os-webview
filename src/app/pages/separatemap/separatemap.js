aecf7d02b5717fee395346a225a7095145b45cd9
Roy Johnson <roy.e.johnson@rice.edu>
2020-02-13 13:57:52 -0600

    Track Map queries and clicks in Google Analytics (#1662)
    Issue #693

--- src/app/pages/separatemap/separatemap.js
+++ src/app/pages/separatemap/separatemap.js
@@ -1,14 +1,15 @@
 import componentType, {canonicalLinkMixin} from '~/helpers/controller/init-mixin';
 import $ from '~/helpers/$';
-import { on } from '~/helpers/controller/decorators';
+import {on} from '~/helpers/controller/decorators';
 import shellBus from '~/components/shell/shell-bus';
-import { description as template } from './separatemap.html';
+import {description as template} from './separatemap.html';
 import css from './separatemap.css';
 import SearchBox from './search-box/search-box';
 import Map from '~/helpers/map-api';
 import {queryById} from '~/models/querySchools';
 import TestimonialForm from './testimonial-form/testimonial-form';
 import {accountsModel} from '~/models/usermodel';
+import analytics from '~/helpers/analytics';
 
 const spec = {
     template,
@@ -122,6 +123,16 @@ export default class SeparateMap extends componentType(spec, canonicalLinkMixin)
     @on('click .mapboxgl-popup-content .put-away button')
     closetooltip() {
         this.map.tooltip.remove();
+    }
+
+    @on('click .back-impact-div')
+    sendCloseMapEvent() {
+        analytics.sendPageEvent('map', 'close', '');
+    }
+
+    @on('click .popup-message-link')
+    sendAdoptionEvent() {
+        analytics.sendPage('map', 'adoption', '');
     }
 
     onClose() {
