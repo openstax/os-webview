b725656f68982fc248dd5f121039a67b994beeea
Roy Johnson <roy.e.johnson@rice.edu>
2020-03-17 15:03:28 -0500

    Change Errata Details details (#1702)
    Issue #937

--- src/app/pages/errata-detail/progress-bar/progress-bar.js
+++ src/app/pages/errata-detail/progress-bar/progress-bar.js
@@ -12,31 +12,19 @@ const spec = {
     model() {
         const thirdNodeFill = ['Corrected', 'Will correct'].includes(this.barStatus) ?
             ' filled' : ' filled-no';
-        const bars = this.bars;
-
-        return {
-            barStatus: this.barStatus,
-            showDatesInBar: this.barStatus === '' || this.reviewedDate,
-            firstNodeClass: bars === 0 ? ' filled ' : '',
-            secondNodeClass: bars === 1 ? ' filled' : '',
-            thirdNodeClass: bars > 1 ? thirdNodeFill : '',
-            createdDate: this.createdDate,
-            reviewedDate: this.reviewedDate,
-            correctedDate: this.correctedDate
-        };
-    }
-};
-
-export default class extends componentType(spec) {
-
-    init(props) {
-        super.init();
-        Object.assign(this, props);
-        this.bars = this.barStatus ? 2 : {
+        const bars = this.barStatus ? 2 : {
             'In Review': 0,
             'Reviewed': 1,
             'Will Correct': 1
         }[this.status];
-    }
 
-}
+        return {
+            barStatus: this.barStatus,
+            firstNodeClass: bars === 0 ? ' filled ' : '',
+            secondNodeClass: bars === 1 ? ' filled' : '',
+            thirdNodeClass: bars > 1 ? thirdNodeFill : ''
+        };
+    }
+};
+
+export default componentType(spec);
