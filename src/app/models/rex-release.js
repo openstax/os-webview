7388ad7dffcef003ba8d853147b4518191d69dc5
Roy Johnson <roy.e.johnson@rice.edu>
2020-04-09 12:43:30 -0500

    Fix: ToC for Rex books doesn't change
    lodash memoize only keys by the first parameter!
    Issue #1023

--- src/app/models/rex-release.js
+++ src/app/models/rex-release.js
@@ -6,19 +6,25 @@ function rexOriginFromWebview(url) {
     return (new URL(url)).origin;
 }
 
-const fetchRexRelease = memoize((rexOrigin, cnxId) => {
-    if ($.isTestingEnvironment()) {
-        return fetch('https://archive.cnx.org/contents/8d50a0af-948b-4204-a71d-4826cba765b8@16.27.json')
-            .then((r) => r.json());
-    }
+// CAUTION: Lodash memoize only distinguishes the first parameter
+const fetchRexRelease = memoize((rexOrigin) => {
     return fetch(`${rexOrigin}/rex/environment.json`)
         .then((r) => r.json())
         .then((r) => fetch(`${rexOrigin}/rex/releases/${r.release_id}/rex/release.json`))
-        .then((r) => r.json())
+        .then((r) => r.json());
+});
+
+// REMEMBER: The first parameter is the memo key
+const fetchContents = memoize((cnxId, rexOrigin) => {
+    if ($.isTestingEnvironment()) {
+        return fetch(`//archive.cnx.org/contents/${cnxId}`)
+            .then((r) => r.json());
+    }
+    return fetchRexRelease(rexOrigin)
         .then((r) => fetch(`//archive.cnx.org/contents/${cnxId}@${r.books[cnxId].defaultVersion}`))
         .then((r) => r.json());
 });
 
 export default function (webviewLink, cnxId) {
-    return fetchRexRelease(rexOriginFromWebview(webviewLink), cnxId);
+    return fetchContents(cnxId, rexOriginFromWebview(webviewLink));
 };
