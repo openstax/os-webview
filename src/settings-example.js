a4b92f39a6873901d59cd0b46d49adc7922720dc
Roy Johnson <roy.e.johnson@rice.edu>
2020-05-28 11:06:41 -0500

    Make button editable in homepage Education section
    Issue #1149

--- src/settings-example.js
+++ src/settings-example.js
@@ -7,18 +7,7 @@ const settings = {
     tagManagerID: 'GTM-W6N7PB',
     mapboxPK: 'pk.eyJ1Ijoib3BlbnN0YXgiLCJhIjoiY2pnbWtjajZzMDBkczJ6cW1kaDViYW02aCJ9.0w3LCa7lzozzRgXM7xvBfQ',
     testingEnvironment: (/(localhost|oscms-)/).test(window.location.hostname),
-    titleSuffix: ' - OpenStax',
-    webmaster: 'webmaster@openstax.org',
-    touchnet: {
-        test: {
-            url: 'https://ebank.rice.edu:8443/C21279test_upay/web/index.jsp',
-            id: 90
-        },
-        prod: {
-            url: 'https://ebank.rice.edu/C21279_upay/web/index.jsp',
-            id: 106
-        }
-    }
+    titleSuffix: ' - OpenStax'
 };
 
 window.SETTINGS = settings;
