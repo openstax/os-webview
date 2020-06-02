e3905ad3483bbf32ba0c33fe4d5bd5b9a7e1d767
Roy Johnson <roy.e.johnson@rice.edu>
2020-06-02 09:32:10 -0500

    Add Google Tag Manager back
    Issue #1160

---
+++ src/app/helpers/tag-manager.js
@@ -0,0 +1,20 @@
+const tagManagerID = 'GTM-W6N7PB';
+
+// eslint-disable-next-line max-params
+(function (w, d, s, l, i) {
+    // Disable ESLint rules since we're copying Google's script
+    /* eslint-disable one-var, prefer-const, prefer-template */
+    w[l] = w[l] || [];
+    w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+    });
+
+    let f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l !== 'dataLayer' ? '&l=' + l : '';
+
+    j.async = true;
+    j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', tagManagerID);
