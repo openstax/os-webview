d1aeabe9b9e8da896c11477a622311911aab6855
Roy Johnson <roy.e.johnson@rice.edu>
2020-09-02 09:15:18 -0500

    Enable 3-dot menu, fix no-give-link error

--- src/app/pages/subjects/book-viewer/book.js
+++ src/app/pages/subjects/book-viewer/book.js
@@ -120,9 +120,7 @@ export function BookCover({
 
     return (
         <div className={classList.join(' ')}>
-            {/*
-                <ThreeDotMenu slug={slug} details={details} />
-            */}
+            <ThreeDotMenu slug={slug} details={details} />
             <a href={`/details/${slug}`}>
                 <img src={coverUrl} alt={title} />
             </a>
