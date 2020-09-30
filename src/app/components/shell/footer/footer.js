98d54290e5996b18e08eae0f6f856aed37e82279
Roy Johnson <roy.e.johnson@rice.edu>
2020-09-30 11:34:59 -0500

    Update footer donate language
    Issue #1310
    Uses `supporters` data item as HTML for that whole block. Recommend `h2` tag for the first part, `p` for the second.

--- src/app/components/shell/footer/footer.js
+++ src/app/components/shell/footer/footer.js
@@ -1,58 +1,78 @@
-import CMSPageController from '~/controllers/cms';
+import React from 'react';
+import {pageWrapper} from '~/controllers/jsx-wrapper';
+import {LoaderPage, RawHTML} from '~/components/jsx-helpers/jsx-helpers.jsx';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import $ from '~/helpers/$';
-import {description as template} from './footer.html';
-import css from './footer.css';
+import './footer.css';
 
-class Footer extends CMSPageController {
+function Footer({data}) {
+    const {supporters, copyright, apStatement, facebookLink, twitterLink, linkedinLink} =
+        $.camelCaseKeys(data);
 
-    init() {
-        this.template = template;
-        this.css = css;
-        this.view = {
-            tag: 'footer',
-            classes: ['page-footer']
-        };
-        this.model = {};
-        this.slug = 'footer';
-        /* eslint arrow-parens: 0 */
-        (async () => {
-            try {
-                const response = await fetch(
-                    `${$.apiOriginAndPrefix}/documents/?search=press%20kit`
-                );
-                const data = await response.json();
-
-                if (data.length) {
-                    this.model.pressKitData = data[0];
-                    this.update();
-                }
-            } catch (e) {
-                console.log(e);
-            }
-        })();
-    }
-
-    onDataLoaded() {
-        const pd = this.pageData;
-
-        Object.assign(this.model, {
-            apStatement: pd.ap_statement,
-            copyright: pd.copyright,
-            supporters: pd.supporters,
-            twitterLink: pd.twitter_link,
-            facebookLink: pd.facebook_link,
-            linkedinLink: pd.linkedin_link
-        });
-        this.update();
-        this.onLoaded();
-    }
-
-    onLoaded() {
-        $.insertHtml(this.el, this.model);
-    }
-
+    return (
+        <React.Fragment>
+            <div className="top">
+                <div className="boxed">
+                    <RawHTML html={supporters} />
+                    <div className="column col1">
+                        <h3>Help</h3>
+                        <a href="/contact">Contact Us</a>
+                        <a href="https://openstax.secure.force.com/help">Support Center</a>
+                        <a href="/faq">FAQ</a>
+                    </div>
+                    <div className="column col2">
+                        <h3>OpenStax</h3>
+                        <a href="/press">Press</a>
+                        <a href="http://www2.openstax.org/l/218812/2016-10-04/lvk">Newsletter</a>
+                        <a href="/careers">Careers</a>
+                    </div>
+                    <div className="column col3">
+                        <h3>Policies</h3>
+                        <a href="/accessibility-statement">Accessibility Statement</a>
+                        <a href="/tos">Terms of Use</a>
+                        <a href="/license">Licensing</a>
+                        <a href="/privacy-policy">Privacy Policy</a>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="boxed">
+                    <div className="copyrights">
+                        <RawHTML html={copyright} />
+                        <RawHTML Tag="ap-html" html={apStatement} />
+                    </div>
+                    <div className="social" role="directory">
+                        <a className="btn btn-social facebook" href={facebookLink} title="Facebook">
+                            <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+                        </a>
+                        <a className="btn btn-social twitter" href={twitterLink} title="Twitter">
+                            <FontAwesomeIcon icon={['fab', 'twitter']} />
+                        </a>
+                        <a className="btn btn-social linkedin" href={linkedinLink} title="LinkedIn">
+                            <FontAwesomeIcon icon={['fab', 'linkedin-in']} title="LinkedIn" />
+                        </a>
+                        <a className="btn btn-social instagram" href="https://www.instagram.com/openstax/" title="Instagram">
+                            <FontAwesomeIcon icon={['fab', 'instagram']} title="Instagram" />
+                        </a>
+                        <a className="rice-logo" href="http://www.rice.edu">
+                            <img src="/images/rice-white-text.png" alt="Rice University logo" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 }
 
-const footer = new Footer();
+function FooterLoader() {
+    return (
+        <LoaderPage slug="footer" Child={Footer} />
+    );
+}
 
-export default footer;
+const view = {
+    tag: 'footer',
+    classes: ['page-footer']
+};
+
+export default new (pageWrapper(FooterLoader, view))();
