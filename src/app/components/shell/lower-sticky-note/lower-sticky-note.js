a67f2f0f723eb8cced117afc2395ea6b6bb236f0
Roy Johnson <roy.e.johnson@rice.edu>
2020-11-20 11:34:46 -0600

    Handle A/B testing in sticky note
    Issue #1902

--- src/app/components/shell/lower-sticky-note/lower-sticky-note.js
+++ src/app/components/shell/lower-sticky-note/lower-sticky-note.js
@@ -3,6 +3,7 @@ import {pageWrapper} from '~/controllers/jsx-wrapper';
 import {RawHTML} from '~/components/jsx-helpers/jsx-helpers.jsx';
 import {usePutAway, useStickyData} from '../shared.jsx';
 import analytics from '~/helpers/analytics';
+import $ from '~/helpers/$';
 import './lower-sticky-note.css';
 
 function trackClickFor(el, target, eventArgs) {
@@ -31,9 +32,30 @@ function trackClick(event) {
     );
 }
 
+function htmlWithTagRemoved(html, tag) {
+    const el = document.createElement('div');
+
+    el.innerHTML = html;
+    const elToRemove = el.querySelector(tag);
+
+    if (elToRemove) {
+        elToRemove.remove();
+    }
+    return el.innerHTML;
+}
+
+function randomAorB(stickyData) {
+    const tagToRemove = Math.random() > 0.5 ? 'a-test' : 'b-test';
+    const headerHtml = htmlWithTagRemoved(stickyData.header, tagToRemove);
+    const bodyHtml = htmlWithTagRemoved(stickyData.body, tagToRemove);
+
+    return [$.htmlToText(headerHtml), bodyHtml];
+}
+
 // eXslint-disable-next-line complexity
 function LowerStickyNote() {
     const stickyData = useStickyData();
+    const [header, body] = randomAorB(stickyData || {});
     const [closed, PutAway] = usePutAway();
     const shouldNotDisplay = !stickyData || closed ||
         stickyData.mode !== 'banner';
@@ -46,8 +68,8 @@ function LowerStickyNote() {
         <div className="lower-sticky-note-content" onClick={trackClick}>
             <PutAway />
             <div className="content">
-                <h1>{stickyData.header}</h1>
-                <RawHTML className="blurb" html={stickyData.body} />
+                <h1>{header}</h1>
+                <RawHTML className="blurb" html={body} />
                 <a className="btn primary" href={stickyData.link}>
                     {stickyData.link_text}
                 </a>
