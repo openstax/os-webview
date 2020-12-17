dfd39ac89b3d4cf20f0d1a907cae79d4224dcd5e
Roy Johnson <roy.e.johnson@rice.edu>
2020-12-17 11:10:52 -0600

    Use Amazon Associates link (with disclosure) in "Order a print copy" submenu (#1909)

---
+++ src/app/pages/details/common/get-this-title-files/amazon-associates-link.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const buyprintServer = 'https://buyprint.openstax.org';
+
+export default function useAmazonAssociatesLink(slug) {
+    const [data, setData] = React.useState({});
+
+    React.useEffect(() => {
+        fetch(`${buyprintServer}/${slug}.json`)
+            .then((response) => response.json())
+            .then(({buy_urls: urls}) => {
+                setData(urls[0]);
+            });
+    }, [slug]);
+
+    return data;
+}
