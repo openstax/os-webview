8108da22a1705e26f703846111bbc6f1c4b05952
Roy Johnson <roy.e.johnson@rice.edu>
2021-01-07 14:37:41 -0600

    Use data from CMS
    Issue #1676

--- src/app/pages/campaign/openstax-tutor/openstax-tutor.js
+++ src/app/pages/campaign/openstax-tutor/openstax-tutor.js
@@ -1,69 +1,30 @@
 import React from 'react';
-import {RawHTML} from '~/components/jsx-helpers/jsx-helpers.jsx';
+import {LoaderPage, RawHTML} from '~/components/jsx-helpers/jsx-helpers.jsx';
 import './openstax-tutor.css';
-
-const data = {
-    headline: 'Courseware for online, in-person, and hybrid classrooms.',
-    description: `Our innovative research-based tool provides students with
-    easy-to-use online courseware they need to stay engaged while reading,
-    writing and doing homework assignments. All while you, the teacher, is
-    provided with insight into how your students are learning and where they
-    need additional support.<br><br>
-    <b>Available books:</b><br>
-    Anatomy and Physiology, Biology 2e, College Physics, Biology for
-    AP<sup>&reg;</sup> Courses, The AP<sup>&reg;</sup> Physics Collection,
-    Psychology 2e, Introduciton to Sociology 2e, and Entrepreneurship.
-    `,
-    buttonUrl: 'case-study-download.pdf',
-    buttonText: 'Download the case study',
-    sectionHeader: 'Features',
-    sectionTitle: 'How OpenStax Tutor works',
-    blurbs: [
-        {
-            icon: '/images/campaign/features-icon-1.svg',
-            title: 'Segmented reading combined with student engagement exercises',
-            description: `We break textbook readings down into easy digestible
-            segments that include videos, simulations and theoretical questions.`
-        },
-        {
-            icon: '/images/campaign/features-icon-2.svg',
-            title: 'Improved study techniques and increased learning success ',
-            description: `Spaced practice study techniques, personalized questions
-            and other features help students learn more efficiently and effectively.
-            65% of students using Tutor say it helps them learn.`
-        },
-        {
-            icon: '/images/campaign/features-icon-3.svg',
-            title: 'Teachers have greater choice in how they test their students',
-            description: `You can build homework assignments with questions from
-            the text, additional assessments and personalized questions. Our
-            performance forecast shows you where your students are struggling-as
-            a class, or by student. `
-        }
-    ],
-    resourceBlurb: `If OpenStax Tutor is not yet available for your subject,
-    don’t worry. We still have dozens of available instructor resources to help
-    you with your classroom.`,
-    resourceUrl: 'tutor-resources',
-    resourceText: 'See resources'
-};
 
 function Blurb({icon, title, description}) {
     return (
         <div className="blurb">
-            <img src={icon} />
+            <img src={icon.file} />
             <h3>{title}</h3>
-            <div>{description}</div>
+            <RawHTML html={description} />
         </div>
     );
 }
 
-export default function TutorPage() {
+function TutorPage({data}) {
     const {
-        headline, description, buttonUrl, buttonText,
-        sectionHeader, sectionTitle, blurbs,
-        resourceBlurb, resourceUrl, resourceText
+        header: headline, description,
+        availableBooksHeader,
+        tutorBooks,
+        caseStudyFileUrl: buttonUrl, caseStudyCta: buttonText,
+        featuresHeader: sectionTitle,
+        featuresCards: blurbs,
+        otherResourcesText: resourceBlurb,
+        otherResourcesLink: resourceUrl,
+        otherResourcesCta: resourceText
     } = data;
+    const sectionHeader = 'Features';
 
     return (
         <main className="openstax-tutor">
@@ -72,6 +33,12 @@ export default function TutorPage() {
                     <div className="text">
                         <h1>{headline}</h1>
                         <RawHTML className="description" html={description} />
+                        <div className="available-books-header">{availableBooksHeader}</div>
+                        <div>
+                            {
+                                tutorBooks.map((obj) => obj.title).join(', ')
+                            }.
+                        </div>
                         <a className="btn super" href={buttonUrl}>{buttonText}</a>
                     </div>
                 </div>
@@ -86,7 +53,7 @@ export default function TutorPage() {
                     <div className="blurbs">
                         {blurbs.map((blurb, i) => <Blurb {...blurb} key={i} />)}
                         <div className="resource-blurb">
-                            <div>{resourceBlurb}</div>
+                            <RawHTML html={resourceBlurb} />
                             <a className="btn primary" href={resourceUrl}>
                                 {resourceText}
                             </a>
@@ -95,5 +62,11 @@ export default function TutorPage() {
                 </div>
             </section>
         </main>
+    );
+}
+
+export default function TutorPageLoader() {
+    return (
+        <LoaderPage slug="pages/tutor-landing" Child={TutorPage} />
     );
 }
