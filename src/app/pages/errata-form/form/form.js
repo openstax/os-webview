d2649d10fa6b0bbaf31a2a52487c1416e54bc77b
Roy Johnson <roy.e.johnson@rice.edu>
2021-01-08 10:05:13 -0600

    Refactor errata form; disable submit button when clicked
    Issue #1229

--- src/app/pages/errata-form/form/form.js
+++ src/app/pages/errata-form/form/form.js
@@ -8,11 +8,13 @@ import $ from '~/helpers/$';
 import shellBus from '~/components/shell/shell-bus';
 import routerBus from '~/helpers/router-bus';
 import BannedNotice from '../banned-notice/banned-notice';
+import cn from 'classnames';
 import css from './form.css';
 
 const sourceNames = {
     tutor: 'OpenStax Tutor'
 };
+const postEndpoint = `${$.apiOriginAndOldPrefix}/errata/`;
 
 function ErrorExplanationBox() {
     const inputRef = useRef();
@@ -35,129 +37,140 @@ function ErrorExplanationBox() {
     );
 }
 
-function SubmitButton({
-    formRef,
-    updateHasBeenSubmitted,
-    submitted,
-    submitFailed
-}) {
-    function doCustomValidation(event) {
-        const invalid = formRef.current.querySelector('form :invalid');
-
-        if (invalid) {
-            event.preventDefault();
-            updateHasBeenSubmitted(true);
+function SubmitButton({hasError, setHideErrors, submitting, setSubmitting}) {
+    function validateBeforeSubmitting(event) {
+        event.preventDefault();
+        setHideErrors(false);
+        if (!hasError) {
+            setSubmitting(true);
         }
     }
 
     return (
-        <div>
-            <input type="submit" className="btn" value="Submit" disabled={submitted} onClick={doCustomValidation} />
-            {
-                submitFailed &&
-                <div className="invalid-message">{submitFailed}</div>
-            }
+        <div className="submit-button">
+            <input
+                type="submit" className="btn" value="Submit"
+                disabled={submitting} onClick={validateBeforeSubmitting}
+            />
+            {hasError && <div className="invalid-message">{hasError}</div>}
         </div>
     );
 }
 
-
-export default function ErrataForm({model}) {
-    const selectedBook = (model.books.find((b) => b.title === model.selectedTitle) || {});
-    const [selectedError, updateSelectedError] = useState();
-    const [submitted, updateSubmitted] = useState(false);
-    const [hasBeenSubmitted, updateHasBeenSubmitted] = useState(false);
-    const [submitFailed, updateSubmitFailed] = useState();
-    const postEndpoint = `${$.apiOriginAndOldPrefix}/errata/`;
-    const formRef = useRef();
-    const helpBoxVisible = () => selectedError === 'Other' ? 'visible' : 'not-visible';
-    const initialSource = model.source && sourceNames[model.source.toLowerCase()];
-
-    function onSubmit(event) {
-        // Safari cannot handle empty files; Edge cannot manipulate FormData
-        // so we remove the file inputs that have no values
-        const formEl = event.target;
-        const fileInputs = Array.from(formEl.querySelectorAll('[type="file"]'));
-        const fiParents = fileInputs.map((el) => el.parentNode);
-
-        fileInputs.forEach((el, index) => {
-            if (el.value === '') {
-                el.parentNode.removeChild(el);
-            }
-        });
-        fileInputs.forEach((el, index) => {
-            el.setAttribute('name', `file_${index+1}`);
-        });
-
-        const form = new FormData(formRef.current);
-
-        event.preventDefault();
-        // Programmatically post the form
-        fetch(postEndpoint, {
-            method: 'POST',
-            body: form
-        }).then((r) => r.json()).then(
-            (json) => {
-                if (json.id) {
-                    updateSubmitted(false);
-                    routerBus.emit('navigate', `/confirmation/errata?id=${json.id}`);
-                } else if (json.submitted_by_account_id) {
-                    shellBus.emit('showDialog', () => ({
-                        title: 'Errata submission rejected',
-                        content: new BannedNotice({
-                            model: {
-                                text: json.submitted_by_account_id[0]
-                            }
-                        })
-                    }));
-                }
-            },
-            (fetchError) => {
-                updateSubmitFailed(`Submit failed: ${fetchError}.`);
-                updateSubmitted(false);
-                // Put them back
-                fileInputs.forEach((el, i) => {
-                    fiParents[i].appendChild(el);
-                });
-            }
-        );
-    }
+function FormFields({
+    submittedBy, books, initialSource, location, source, selectedTitle,
+    hasError, setHideErrors, submitting, setSubmitting
+}) {
+    const selectedBook = (books.find((b) => b.title === selectedTitle) || {});
 
     return (
-        <form
-            className={`body-block ${hasBeenSubmitted ? '' : 'hide-errors'}`}
-            method="post" action={postEndpoint}
-            encType="multipart/form-data" ref={formRef}
-            onSubmit={onSubmit}
-        >
-            <input type="hidden" name="submitted_by_account_id" value={model.submittedBy} />
+        <React.Fragment>
+            <input type="hidden" name="submitted_by_account_id" value={submittedBy} />
             <input type="hidden" name="book" value={selectedBook.id} />
-            <ErrorTypeSelector
-                selectedError={selectedError}
-                updateSelectedError={updateSelectedError}
-            />
-            <div className={`helpbox ${helpBoxVisible()}`}>
-                <span>Need help logging in or have general questions? Contact Support at </span>
-                <a href="mailto:support@openstax.org">support@openstax.org</a>.
-            </div>
+            <ErrorTypeSelector />
             <ErrorSourceSelector
                 initialSource={initialSource}
             />
             <ErrorLocationSelector
                 selectedBook={selectedBook}
-                defaultValue={model.location}
-                readOnly={model.location && model.source}
-                title={model.selectedTitle}
+                defaultValue={location}
+                readOnly={location && source}
+                title={selectedTitle}
             />
             <ErrorExplanationBox />
             <div className="question">Please add a screenshot or any other file that helps explain the error.</div>
-            <FileUploader
-                Slot={() =>
-                    <SubmitButton
-                        formRef={formRef}
-                        updateHasBeenSubmitted={updateHasBeenSubmitted}
-                        submitted={submitted}
-                    />}
+            <div className="button-group">
+                <FileUploader />
+                <SubmitButton {...{hasError, setHideErrors, submitting, setSubmitting}} />
+            </div>
+        </React.Fragment>
+    );
+}
+
+// Safari cannot handle empty files; Edge cannot manipulate FormData
+// so we remove the file inputs that have no values
+function removeEmptyFileWidgets(formEl) {
+    const fileInputs = Array.from(formEl.querySelectorAll('[type="file"]'));
+    const fiParents = fileInputs.map((el) => el.parentNode);
+
+    fileInputs.forEach((el, index) => {
+        if (el.value === '') {
+            el.parentNode.removeChild(el);
+        }
+    });
+    fileInputs.forEach((el, index) => {
+        el.setAttribute('name', `file_${index+1}`);
+    });
+
+    // Return a function to put them back
+    return () => {
+        fileInputs.forEach((el, i) => {
+            fiParents[i].appendChild(el);
+        });
+    };
+}
+
+export default function ErrataForm({
+    model: {books, selectedTitle, source, submittedBy, location}
+}) {
+    const [hasError, setHasError] = useState('You have not completed the form');
+    const [hideErrors, setHideErrors] = useState(true);
+    const [submitting, setSubmitting] = useState(false);
+    const formRef = useRef();
+    const initialSource = source && sourceNames[source.toLowerCase()];
+
+    function validate() {
+        const invalid = formRef.current.querySelector(':invalid');
+
+        setHasError(invalid ? 'You have not completed the form.' : null);
+    }
+
+    React.useEffect(() => {
+        if (submitting) {
+            const formEl = formRef.current;
+            const putFileWidgetsBack = removeEmptyFileWidgets(formEl);
+            const formData = new FormData(formEl);
+
+            // Programmatically post the form
+            fetch(postEndpoint, {
+                method: 'POST',
+                body: formData
+            }).then((r) => r.json())
+                .then(
+                    (json) => {
+                        if (json.id) {
+                            routerBus.emit('navigate', `/confirmation/errata?id=${json.id}`);
+                        } else if (json.submitted_by_account_id) {
+                            shellBus.emit('showDialog', () => ({
+                                title: 'Errata submission rejected',
+                                content: new BannedNotice({
+                                    model: {
+                                        text: json.submitted_by_account_id[0]
+                                    }
+                                })
+                            }));
+                        }
+                    },
+                    (fetchError) => {
+                        setHasError(`Submit failed: ${fetchError}.`);
+                        putFileWidgetsBack();
+                    }
+                );
+        }
+    }, [submitting]);
+
+    return (
+        <form
+            className={cn('body-block', {'hide-errors': hideErrors})}
+            method="post" action={postEndpoint}
+            encType="multipart/form-data"
+            onChange={validate}
+            ref={formRef}
+        >
+            <FormFields
+                {...{submittedBy, books, initialSource, location, source, selectedTitle,
+                    hasError, setHideErrors, submitting, setSubmitting
+                }}
             />
         </form>
     );
