4aae45bff6147cca0255b9c20b34f2030adc8888
Roy Johnson <roy.e.johnson@rice.edu>
2021-01-08 18:13:48 -0600

    Refactor errata-detail page

--- src/app/pages/errata-detail/errata-detail.js
+++ src/app/pages/errata-detail/errata-detail.js
@@ -1,15 +1,58 @@
-import WrappedJsx from '~/controllers/jsx-wrapper';
-import ErrataDetail from './errata-detail.jsx';
+import {pageWrapper} from '~/controllers/jsx-wrapper';
+import {LoaderPage, useDataFromSlug, useDataFromPromise} from '~/components/jsx-helpers/jsx-helpers.jsx';
+import React, {useState, useEffect} from 'react';
+import {ProgressBar} from './progress-bar/progress-bar.jsx';
+import {Detail} from './detail/detail';
+import cmsFetch from '~/models/cmsFetch';
+import {getDisplayStatus, getDetailModel} from '~/helpers/errata';
+import './errata-detail.css';
 
-export default class extends WrappedJsx {
+function ProgressBarBlock({data}) {
+    const {status, barStatus} = getDisplayStatus(data);
 
-    init() {
-        super.init(ErrataDetail, {
-            slug: window.location.pathname.substr(1)
-        });
-        this.view = {
-            classes: ['errata-detail', 'page']
-        };
+    return (
+        <div className="progress-bar-container body-block">
+            <ProgressBar status={status} barStatus={barStatus} />
+        </div>
+    );
+}
+
+function ErrataDetailBlock({data}) {
+    const {detail, showDecisionDetails} = useDataFromPromise(getDetailModel(data), {});
+
+    if (!detail) {
+        return null;
     }
 
+    return (
+        <div className="errata-detail-block">
+            <Detail
+                detail={detail}
+                showDecisionDetails={showDecisionDetails} />
+        </div>
+    );
 }
+
+function ErrataDetail({data}) {
+    return (
+        <React.Fragment>
+            <div class="hero padded"><h1>Errata Submission Details</h1></div>
+            <div class="boxed">
+                <ProgressBarBlock data={data} />
+                <ErrataDetailBlock data={data} />
+            </div>
+        </React.Fragment>
+    );
+}
+
+function ErrataDetailLoader() {
+    return (
+        <LoaderPage slug={window.location.pathname.substr(1)} Child={ErrataDetail} doDocumentSetup />
+    );
+}
+
+const view = {
+    classes: ['errata-detail', 'page']
+};
+
+export default pageWrapper(ErrataDetailLoader, view);
