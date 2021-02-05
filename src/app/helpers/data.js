c348447e08af3aba02251cda38bcac51c6e80931
Roy Johnson <roy.e.johnson@rice.edu>
2021-02-05 15:53:52 -0600

    Handle empty dates better

--- src/app/helpers/data.js
+++ src/app/helpers/data.js
@@ -1,4 +1,7 @@
 export function formatDateForBlog(date) {
+    if (!date) {
+        return null;
+    }
     const d = new Date(date).toUTCString().split(' ');
 
     return `${d[2]} ${d[1]}, ${d[3]}`;
