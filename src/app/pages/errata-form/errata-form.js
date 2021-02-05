3c82db8fe1559359d40ca0d4417a035c885080d1
Roy Johnson <roy.e.johnson@rice.edu>
2021-02-05 13:31:01 -0600

    Prompt for book title in when not passed to errata form
    Issue #1944

--- src/app/pages/errata-form/errata-form.js
+++ src/app/pages/errata-form/errata-form.js
@@ -5,17 +5,23 @@ import bookPromise from '~/models/book-titles';
 import userModel from '~/models/usermodel';
 import linkHelper from '~/helpers/link';
 import Form from './form/form';
+import FormSelect from '~/components/form-select/form-select.jsx';
 import $ from '~/helpers/$';
 import './errata-form.css';
 
-function useFormModel() {
+function getBookTitle() {
+    const queryDict = $.parseSearchString(window.location.search);
+
+    return queryDict.book ? queryDict.book[0] : null;
+}
+
+function useFormModel(title) {
     const [model, setModel] = useState();
 
     useEffect(() => {
-        const queryDict = $.parseSearchString(window.location.search);
-        const title = queryDict.book[0];
-
         Promise.all([userModel.load(), bookPromise]).then(([userInfo, books]) => {
+            const queryDict = $.parseSearchString(window.location.search);
+
             if (userInfo.accounts_id) {
                 const entry = books.find((info) => info.title === title);
                 const infoUrl = urlFromSlug(`books/${entry.meta.slug}`);
@@ -42,8 +48,8 @@ function useFormModel() {
     return model;
 }
 
-function ErrataForm() {
-    const formModel = useFormModel();
+function ErrataForm({title}) {
+    const formModel = useFormModel(title);
 
     if (!formModel) {
         return null;
@@ -63,9 +69,48 @@ function ErrataForm() {
     );
 }
 
+function TitleSelector({setTitle}) {
+    const [options, setOptions] = useState([]);
+
+    useEffect(() => {
+        bookPromise.then((books) => {
+            setOptions(books.map((book) => (
+                {label: book.title, value: book.title}
+            )));
+        });
+    }, []);
+
+    return (
+        <div className="text-content title-selector">
+            <p>
+                It looks like you got referred here but they didn't tell us what
+                book you were looking at.
+            </p>
+            <FormSelect
+                selectAttributes={{
+                    placeholder: 'Please select one',
+                    onChange(event) {
+                        setTitle(event.target.value);
+                    }
+                }}
+                label="What book were you in, again?" options={options} />
+        </div>
+    );
+}
+
+function TitleSelectorOrForm() {
+    const [title, setTitle] = useState(getBookTitle());
+
+    return (
+        title ?
+            <ErrataForm title={title} /> :
+            <TitleSelector setTitle={setTitle} />
+    );
+}
+
 const view = {
     classes: ['errata-form', 'page'],
     tag: 'main'
 };
 
-export default pageWrapper(ErrataForm, view);
+export default pageWrapper(TitleSelectorOrForm, view);
