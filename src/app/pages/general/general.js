6af432a7acfa6bc92cf4d669f65d699a9174719f
Roy Johnson <roy.e.johnson@rice.edu>
2021-03-04 12:57:23 -0600

    Port General Pages to React (#1973)
    Issue #1959

--- src/app/pages/general/general.js
+++ src/app/pages/general/general.js
@@ -1,45 +1,50 @@
-import componentType, {canonicalLinkMixin} from '~/helpers/controller/init-mixin';
-import css from './general.css';
-import $ from '~/helpers/$';
+import {pageWrapper} from '~/controllers/jsx-wrapper';
+import React from 'react';
+import {LoaderPage, RawHTML} from '~/components/jsx-helpers/jsx-helpers.jsx';
 import {urlFromSlug} from '~/models/cmsFetch';
+import './general.css';
 
-const spec = {
-    css,
-    view: {
-        classes: ['general', 'page'],
-        tag: 'main'
-    }
-};
-const BaseClass = componentType(spec, canonicalLinkMixin);
+function GeneralPage({html}) {
+    const parser = new DOMParser();
+    const newDoc = parser.parseFromString(html, 'text/html');
+    const strips = parser
+        .parseFromString(
+            '<img class="strips" src="/images/components/strips.svg" height="10" alt="">',
+            'text/html'
+        )
+        .querySelector('img');
+    const innerHTML = Array.from(newDoc.body.children).reduce((arr, el) => {
+        if (el.classList.contains('block-heading')) {
+            el.appendChild(strips);
+        }
+        arr.push(el.outerHTML);
+        return arr;
+    }, []).join('\n');
 
-export default class General extends BaseClass {
-
-    init() {
-        super.init();
-        this.slug = window.location.pathname.substr(1).replace('general', 'spike');
-    }
-
-    onLoaded() {
-        fetch(urlFromSlug(this.slug))
-            .then((r) => r.text())
-            .then((html) => {
-                const parser = new DOMParser();
-                const newDoc = parser.parseFromString(html, 'text/html');
-                const strips = parser
-                    .parseFromString(
-                        '<img class="strips" src="/images/components/strips.svg" height="10" alt="">',
-                        'text/html'
-                    )
-                    .querySelector('img');
-
-                Array.from(newDoc.body.children).forEach((child) => {
-                    this.el.appendChild(child);
-                    if (child.classList.contains('block-heading')) {
-                        child.appendChild(strips);
-                    }
-                });
-                $.activateScripts(this.el);
-            });
-    }
-
+    return (
+        <RawHTML className="general page" html={innerHTML} embed />
+    );
 }
+
+function GeneralPageLoader() {
+    const [html, setHtml] = React.useState();
+
+    React.useEffect(() => {
+        const slug = window.location.pathname.substr(1).replace('general', 'spike');
+        const url = urlFromSlug(slug);
+
+        fetch(url)
+            .then((r) => r.text())
+            .then(setHtml);
+    }, []);
+
+    return (
+        html ? <GeneralPage html={html} /> : <h1>Loading...</h1>
+    );
+}
+
+const view = {
+    tag: 'main'
+};
+
+export default pageWrapper(GeneralPageLoader, view);
