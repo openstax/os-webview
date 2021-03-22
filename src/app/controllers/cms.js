dc53da01940a8aeeafa3267b65f6fd676db7f6af
RoyEJohnson <roy.e.johnson@rice.edu>
2021-03-22 13:28:34 -0500

    Delete no-longer-used superb stuff

--- src/app/controllers/cms.js
+++
@@ -1,5 +0,0 @@
-import mix from '~/helpers/controller/mixins';
-import cmsMixin from '~/helpers/controller/cms-mixin';
-import {Controller} from 'superb.js';
-
-export default mix(Controller).with(cmsMixin);
