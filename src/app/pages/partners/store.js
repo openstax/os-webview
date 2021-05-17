219e5520a2ab217f26c98312eb177657556c979e
RoyEJohnson <roy.e.johnson@rice.edu>
2021-05-17 11:44:22 -0500

    Default sort partners by ratings

--- src/app/pages/partners/store.js
+++ src/app/pages/partners/store.js
@@ -3,7 +3,7 @@ import {SetStore, ScalarStore} from '~/helpers/store';
 export const books = new SetStore();
 export const types = new ScalarStore();
 export const advanced = new SetStore();
-export const sort = new ScalarStore('');
+export const sort = new ScalarStore('-2');
 export const resultCount = new ScalarStore('0');
 export function clearStores() {
     [books, types, advanced].forEach((store) => store.clear());
