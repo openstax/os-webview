97cba5248373a567baf9c69fa69f07a6e40d10f4
RoyEJohnson <roy.e.johnson@rice.edu>
2021-06-14 14:19:07 -0500

    Activate Sentry

--- src/app/sentry.js
+++ src/app/sentry.js
@@ -1,9 +1,11 @@
 import * as Sentry from '@sentry/react';
 import {Integrations} from '@sentry/tracing';
 
+const packageVersion = require('../../package.json').version;
+
 Sentry.init({
     dsn: 'https://8468b55bf5b2442d9d31840113399b10@o484761.ingest.sentry.io/5691260',
-    release: `osweb@${window.process.env.npm_package_version}`,
+    release: `osweb@${packageVersion}`,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 0.5,
     environment: window.location.hostname
