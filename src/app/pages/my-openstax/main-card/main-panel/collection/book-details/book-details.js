9f1f24cea1fe0aa85d13b8878e39e8df0eedaf18
RoyEJohnson <roy.e.johnson@rice.edu>
2021-06-23 13:38:20 -0500

    add resources to book details
    mobile styling

--- src/app/pages/my-openstax/main-card/main-panel/collection/book-details/book-details.js
+++ src/app/pages/my-openstax/main-card/main-panel/collection/book-details/book-details.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import CollectionContext from '../collection-context';
-import cmsFetch from '~/models/cmsFetch';
 import routerBus from '~/helpers/router-bus';
 import useAdoptions from '~/pages/my-openstax/store/use-adoptions';
-import {RawHTML} from '~/components/jsx-helpers/jsx-helpers.jsx';
+import {RawHTML, useDataFromSlug} from '~/components/jsx-helpers/jsx-helpers.jsx';
 import {EditBookForm} from '../add-book-form/add-book-form';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faChevronLeft} from '@fortawesome/free-solid-svg-icons/faChevronLeft';
 import {faBookOpen} from '@fortawesome/free-solid-svg-icons/faBookOpen';
 import {faLaptop} from '@fortawesome/free-solid-svg-icons/faLaptop';
 import useDialog from '~/pages/my-openstax/dialog/dialog';
+import $ from '~/helpers/$';
 import ClippedImage from '~/components/clipped-image/clipped-image';
 import './book-details.scss';
-
-function useSummaryText(slug) {
-    const [text, setText] = React.useState('');
-
-    React.useEffect(() => cmsFetch(slug).then(({description}) => setText(description)), [slug]);
-
-    return text;
-}
 
 function useAdoptionStatus(book) {
     const {adoptions} = useAdoptions();
@@ -31,11 +23,113 @@ function useAdoptionStatus(book) {
         'I have adopted this book';
 }
 
+function selectResources(resources) {
+    const result = [];
+    const cartridges = resources
+        .filter((r) => r.endsWith('Course Cartridge'))
+        .map((r) => r.replace(' Course Cartridge', ''));
+    const others = resources
+        .filter((r) => !r.endsWith('Course Cartridge'));
+
+    if (cartridges.length) {
+        result.push(`LMS Integration with ${cartridges.join(', ')}`);
+    }
+
+    return [...result, ...others].slice(0, 6);
+}
+
+function InstructorResources({detailData}) {
+    if (!detailData.bookFacultyResources) {
+        return null;
+    }
+    const resources = detailData.bookFacultyResources.map((r) => r.resourceHeading);
+    const displayResources = selectResources(resources);
+
+    return (
+        <div className="resource-panel">
+            <div className="left-side">
+                <img src="/images/my-openstax/toolbox.svg" alt="" />
+                <div>
+                    <div><b>OpenStax {detailData.title} Resources</b></div>
+                    <div><i>Always free, included</i></div>
+                </div>
+                <a
+                    href={`/details/${detailData.meta.slug}?Instructor%20resources`}
+                    className="btn primary"
+                >
+                    Get resources
+                </a>
+            </div>
+            <ul>
+                {displayResources.map((r) => <li key={r}>{r}</li>)}
+            </ul>
+        </div>
+    );
+}
+
+function TutorResources({detailData}) {
+    if (!detailData.tutorMarketingBook) {
+        return null;
+    }
+
+    return (
+        <div className="resource-panel">
+            <div className="left-side">
+                <img
+                    className="tutor-logo"
+                    src="/images/openstax-tutor/tutor-logo.svg" alt=""
+                />
+                <div>
+                    <div><b>OpenStax Tutor Courseware</b></div>
+                    <div><i>$10/student</i></div>
+                </div>
+                <a
+                    href="/openstax-tutor"
+                    className="btn primary"
+                >
+                    Learn more
+                </a>
+            </div>
+            <ul>
+                <li>
+                    <b>Personalized homework</b><br />
+                    Build from our library, add your own questions, or edit ours
+                </li>
+                <li>
+                    <b>Digital reading</b><br />
+                    Keep students on track with videos, simulations, and conceptual
+                    questions interjecting their book reading
+                </li>
+                <li>
+                    <b>Analytics about student success</b><br />
+                    View at the assignment or section level
+                </li>
+                <li><b>LMS Integration</b></li>
+            </ul>
+        </div>
+    );
+}
+
+function OnlineResources({detailData}) {
+    return (
+        <div className="resources">
+            <h3 className="top-bar subdivision">
+                OpenStax resources and technology available for this book
+            </h3>
+            <div className="subdivision">
+                <InstructorResources detailData={detailData} />
+                <TutorResources detailData={detailData} />
+            </div>
+        </div>
+    );
+}
+
 export default function BookDetails({book}) {
     const [Dialog, open, close] = useDialog();
     const {setSelectedBook} = React.useContext(CollectionContext);
-    const summaryText = useSummaryText(book.slug);
+    const detailData = $.camelCaseKeys(useDataFromSlug(book.slug)) || {};
     const adoptionStatus = useAdoptionStatus(book);
+    const summaryText = detailData.description;
 
     function goBack(e) {
         e.preventDefault();
@@ -88,13 +182,7 @@ export default function BookDetails({book}) {
                     </div>
                 </div>
                 <div className="after-summary">
-                    <div className="resources">
-                        <h3 className="top-bar subdivision">
-                            OpenStax resources and technology available for this book
-                        </h3>
-                        <div className="subdivision">
-                        </div>
-                    </div>
+                    <OnlineResources detailData={detailData} />
                     <div className="partners">
                         <div className="text-block">
                             <h3>Technology Partners</h3>
