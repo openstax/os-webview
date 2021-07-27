92fa123d0d37368b80689645fe3e203b37e20533
Roy Johnson <roy.e.johnson@rice.edu>
2021-07-27 09:54:57 -0500

    Update DSN (#2065)

--- src/app/sentry.js
+++ src/app/sentry.js
@@ -4,7 +4,7 @@ import {Integrations} from '@sentry/tracing';
 const packageVersion = require('../../package.json').version;
 
 Sentry.init({
-    dsn: 'https://8468b55bf5b2442d9d31840113399b10@o484761.ingest.sentry.io/5691260',
+    dsn: 'https://68df3e19624c434eb975dafa316c03ff@o484761.ingest.sentry.io/5691260',
     release: `osweb@${packageVersion}`,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 0.5,
