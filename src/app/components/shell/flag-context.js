55b59c63c2ca389a3f5c6645d51edb27f62f6669
RoyEJohnson <roy.e.johnson@rice.edu>
2021-11-16 17:47:15 -0600

    Missed one of the sf user fetch calls
    fix test

--- src/app/components/shell/flag-context.js
+++ src/app/components/shell/flag-context.js
@@ -2,13 +2,13 @@ import {useState, useEffect} from 'react';
 import buildContext from '~/components/jsx-helpers/build-context';
 import cmsFetch from '~/models/cmsFetch';
 
+export const flagPromise = cmsFetch('flags?flag=enable_my_openstax')
+    .then(([obj]) => (/true|on/i).test(obj.conditions[0]?.value));
+
 function useContextValue() {
     const [value, setValue] = useState(false);
 
-    useEffect(() => {
-        cmsFetch('flags?flag=enable_my_openstax')
-            .then(([obj]) => setValue((/true|on/i).test(obj.conditions[0]?.value)));
-    }, []);
+    useEffect(() => flagPromise.then(setValue), []);
 
     return value;
 }
