36115a5e9359ff9e9eaa33dfcaa3340c45be9008
Roy Johnson <roy.e.johnson@rice.edu>
2022-01-04 13:42:53 -0600

    Resurrect Creator Fest Page

---
+++ src/app/pages/creator-fest/home-content/home-content.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import About from './about/about';
+import Why from './why/why';
+import './home-content.scss';
+
+export default function HomeContent({pagePanels, register}) {
+    return (
+        <div className="home">
+            <About data={pagePanels[0]} register={register} />
+            {
+                pagePanels.slice(1).map((data, i) =>
+                    <Why data={data} key={i} />
+                )
+            }
+        </div>
+    );
+}
