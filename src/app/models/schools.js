f10b56a52de6c6ecc14f2b3a4d6f30b2469c60a0
Roy Johnson <roy.e.johnson@rice.edu>
2022-02-11 14:40:06 -0600

    wip
    Need to deal with country as well

--- src/app/models/schools.js
+++
@@ -1,9 +0,0 @@
-import cmsFetch from './cmsFetch';
-
-export default cmsFetch('salesforce/schools/')
-    .then((objList) => objList.map((obj) => ({
-        name: obj.name,
-        type: obj.type,
-        location: obj.location,
-        'total_school_enrollment': obj.total_school_enrollment
-    })));
