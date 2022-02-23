fafb80ba12df253acab475d4108064c8f06af27d
Roy Johnson <roy.e.johnson@rice.edu>
2022-02-23 09:25:47 -0600

    Fix "Edit account details" link

--- src/settings-example.js
+++ src/settings-example.js
@@ -1,5 +1,5 @@
 const settings = {
-    accountHref: 'https://accounts-dev.openstax.org',
+    accountHref: 'https://dev.openstax.org/accounts',
     analyticsID: 'UA-73668038-3',
     apiOrigin: 'https://dev.openstax.org',
     mapboxPK: 'pk.eyJ1Ijoib3BlbnN0YXgiLCJhIjoiY2pnbWtjajZzMDBkczJ6cW1kaDViYW02aCJ9.0w3LCa7lzozzRgXM7xvBfQ'
