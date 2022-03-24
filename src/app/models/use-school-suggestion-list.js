3ddae8362071edf316873504cb006cbca95fa636
Roy Johnson <roy.e.johnson@rice.edu>
2022-03-24 15:37:00 -0500

    Use feature flag to control which API to use for school search
    fix test fetcher

--- src/app/models/use-school-suggestion-list.js
+++ src/app/models/use-school-suggestion-list.js
@@ -1,19 +1,20 @@
 import React, {useState, useMemo} from 'react';
+import useFlagContext from '~/components/shell/flag-context';
 import sfApiFetch from '~/pages/my-openstax/store/sfapi';
+import cmsFetch from '~/models/cmsFetch';
 import debounce from 'lodash/debounce';
 
 const debouncedFetch = debounce(
-    (value, setSchools) => {
+    (schoolFetch, value, setSchools) => {
         if (value.length > 1) {
-            sfApiFetch('schools', `/search?name=${value}`)
-                .then((list) => list.map(
-                    (entry) => ({
-                        name: entry.name,
-                        type: entry.school_type,
-                        location: entry.location,
-                        total_school_enrollment: entry.total_school_enrollment // eslint-disable-line camelcase
-                    })
-                ))
+            schoolFetch(value).then((list) => list.map(
+                (entry) => ({
+                    name: entry.name,
+                    type: entry.school_type,
+                    location: entry.location,
+                    total_school_enrollment: entry.total_school_enrollment // eslint-disable-line camelcase
+                })
+            ))
                 .then(setSchools);
         } else {
             setSchools([]);
@@ -22,7 +23,21 @@ const debouncedFetch = debounce(
     300
 );
 
+function useSchoolFetchFunction() {
+    const {my_openstax: isEnabled} = useFlagContext();
+    const fn = useMemo(
+        () => isEnabled ?
+            (value) => sfApiFetch('schools', `/search?name=${value}`) :
+            (value) => cmsFetch(`salesforce/schools?search=${value}`)
+        ,
+        [isEnabled]
+    );
+
+    return fn;
+}
+
 export default function useMatchingSchools(value) {
+    const schoolFetch = useSchoolFetchFunction();
     const [schools, setSchools] = useState([]);
     const schoolNames = useMemo(() => schools.map((s) => s.name).sort(), [schools]);
     const schoolSet = useMemo(() => new window.Set(schoolNames.map((s) => s.toLowerCase())), [schoolNames]);
@@ -31,8 +46,8 @@ export default function useMatchingSchools(value) {
     const schoolOptions = useMemo(() => schoolNames.map((n) => ({label: n, value: n})), [schoolNames]);
 
     React.useEffect(
-        () => debouncedFetch(value, setSchools),
-        [value]
+        () => debouncedFetch(schoolFetch, value, setSchools),
+        [value, schoolFetch]
     );
 
     return {schoolNames, schoolIsOk, selectedSchool, schoolOptions};
