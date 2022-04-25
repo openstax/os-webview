92c569a6b0c93b9010e76a07e1365809d3d38039
Dante Soares <dante.m.soares@rice.edu>
2022-04-25 15:42:24 -0500

    Always preload webview-settings with crossorigin="anonymous" and use default fetch mode

--- src/app/models/cmsFetch.js
+++ src/app/models/cmsFetch.js
@@ -13,10 +13,9 @@ export function urlFromSlug(initialSlug) {
 
 export default async function cmsFetch(path) {
     const url = path.replace(/[^?]+/, urlFromSlug);
-    const mode = process.env.API_ORIGIN ? 'cors' : 'no-cors';
 
     try {
-        return (await retry(() => fetch(url, {mode}))).json();
+        return (await retry(() => fetch(url))).json();
     } catch (err) {
         return Promise.reject(new Error(`Failed to fetch ${path}: ${err}`));
     }
