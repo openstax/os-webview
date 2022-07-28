195898372042234b5fc82447b4fb96301a761f3b
Roy Johnson <roy.e.johnson@rice.edu>
2022-07-28 11:31:18 -0500

    test publication-info including ibooks

--- test/src/pages/details/let-us-know.test.js
+++ test/src/pages/details/let-us-know.test.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import {MemoryRouter} from 'react-router-dom';
-import {DetailsContextProvider} from '~/pages/details/context';
-import {LanguageContextProvider} from '~/contexts/language';
+import BookDetailsWrapper from './book-details-wrapper';
 import LetUsKnow from '~/pages/details/common/let-us-know/let-us-know';
 import {render, screen} from '@testing-library/preact';
 
 const englishTitle = 'Some book';
 const polishTitle = 'Fizyka dla szkół wyższych. Tom 1';
 
-function WrapComponent({children}) {
-    return (
-        <MemoryRouter initialEntries={["/details/books/college-algebra"]}>
-            <LanguageContextProvider>
-                <DetailsContextProvider>
-                    {children}
-                </DetailsContextProvider>
-            </LanguageContextProvider>
-        </MemoryRouter>
-    );
-}
-
 test('handles English title', async () => {
-    render(<WrapComponent><LetUsKnow title={englishTitle} /></WrapComponent>);
+    render(<BookDetailsWrapper><LetUsKnow title={englishTitle} /></BookDetailsWrapper>);
     await screen.findByText('Using this book? Let us know.');
 });
 test('handles Polish title', async () => {
-    render(<WrapComponent><LetUsKnow title={polishTitle} /></WrapComponent>);
+    render(<BookDetailsWrapper><LetUsKnow title={polishTitle} /></BookDetailsWrapper>);
     await screen.findByText('Korzystasz z tej książki? Daj nam znać.');
 });
---
+++ test/src/pages/details/publication-info.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {render, screen} from '@testing-library/preact';
+import useDetailsContext from '~/pages/details/context';
+import PublicationInfo from '~/pages/details/common/publication-info';
+import BookDetailsWrapper from './book-details-wrapper';
+
+function WrappedPublicationInfo({additionalItems={}}) {
+    const model = {...useDetailsContext(), ...additionalItems};
+
+    return (
+        <PublicationInfo model={model} url="/some/path" />
+    );
+}
+
+test('Shows publication dates', async () => {
+    render(<BookDetailsWrapper><WrappedPublicationInfo /></BookDetailsWrapper>);
+    await screen.findByText('Publish Date:');
+    expect(screen.getByText('Hardcover:'));
+    expect(screen.getByText('Paperback:'));
+    expect(screen.queryAllByText('iBooks', {exact: false})).toHaveLength(0);
+});
+
+test('Shows iBooks publication dates when present', async () => {
+    const additionalItems = {
+        ibookIsbn10: 'fakeISBN1',
+        ibookVolume2Isbn10: 'fakeISBN2'
+    };
+
+    render(
+        <BookDetailsWrapper>
+            <WrappedPublicationInfo additionalItems={additionalItems} />
+        </BookDetailsWrapper>
+    );
+    await screen.findByText('Publish Date:');
+    expect(screen.queryAllByText('iBooks', {exact: false})).toHaveLength(2);
+});
