69a714ddce2f21e23f98165b2546fb9b546e6bdc
Roy Johnson <roy.e.johnson@rice.edu>
2022-09-21 18:53:17 -0500

    Fix usePageData

--- src/app/pages/press/page-context.js
+++ src/app/pages/press/page-context.js
@@ -1,6 +1,8 @@
 import buildContext from '~/components/jsx-helpers/build-context';
 import usePageData from '~/helpers/use-page-data';
 
+const fpdParams = {slug: 'press'};
+
 function useContextValue() {
     return usePageData('press');
 }
