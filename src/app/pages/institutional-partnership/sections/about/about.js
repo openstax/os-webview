81b5774d6b8c2983e227a2705f427ea032c5d2cb
Roy Johnson <roy.e.johnson@rice.edu>
2023-01-26 16:38:41 -0600

    Optimize images on Institutional Partnerships page

--- src/app/pages/institutional-partnership/sections/about/about.js
+++ src/app/pages/institutional-partnership/sections/about/about.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import RawHTML from '~/components/jsx-helpers/raw-html';
+import useOptimizedImage from '~/helpers/use-optimized-image';
 import './about.scss';
 
 export default function About({
     heading, description, altText,
     image: {meta: {downloadUrl: image}}
 }) {
+    const optimizedImage = useOptimizedImage(image);
+
     return (
         <section className="about near-white">
             <div className="content-block">
@@ -13,7 +16,7 @@ export default function About({
                     <h2>{heading}</h2>
                     <RawHTML className="description-block" html={description} />
                 </div>
-                <img src={image} alt={altText} />
+                <img src={optimizedImage} alt={altText} />
             </div>
         </section>
     );
--- src/app/pages/institutional-partnership/sections/banner/banner.js
+++ src/app/pages/institutional-partnership/sections/banner/banner.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import RawHTML from '~/components/jsx-helpers/raw-html';
+import useOptimizedImage from '~/helpers/use-optimized-image';
 import './banner.scss';
 
 export default function Banner({
     heading, description, linkText, link,
     backgroundImage: {meta: {downloadUrl: backgroundImage}}
 }) {
+    const maxDim = window.innerWidth < 1920 ? 1920 : null;
+    const optimizedImage = useOptimizedImage(backgroundImage, maxDim);
+
     return (
         <section className="banner">
-            <div className="background-image" style={{backgroundImage: `url(${backgroundImage})`}} />
+            <div className="background-image" style={{backgroundImage: `url(${optimizedImage})`}} />
             <div className="content-block">
                 <h1>{heading}</h1>
                 <RawHTML html={description} />
