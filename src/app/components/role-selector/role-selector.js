66c95e9cb199fba8f0daf3da11ee06d607297200
Roy Johnson <roy.e.johnson@rice.edu>
2023-03-01 14:45:40 -0600

    Fix field name in gated content form

--- src/app/components/role-selector/role-selector.js
+++ src/app/components/role-selector/role-selector.js
@@ -3,14 +3,16 @@ import LoaderPage from '~/components/jsx-helpers/loader-page';
 import DropdownSelect from '~/components/select/drop-down/drop-down';
 import './role-selector.scss';
 
-export function RoleDropdown({options, setValue}) {
-    const optionsAsOptions = options.map((opt) => (
-        {label: opt.displayName, value: opt.salesforceName}
-    ));
+export function RoleDropdown({ options, setValue, name = 'subject' }) {
+    const optionsAsOptions = options.map((opt) => ({
+        label: opt.displayName,
+        value: opt.salesforceName
+    }));
 
     return (
         <DropdownSelect
-            name="subject" required
+            name={name}
+            required
             options={optionsAsOptions}
             placeholder="Please select one"
             onValueUpdate={setValue}
@@ -19,7 +21,13 @@ export function RoleDropdown({options, setValue}) {
 }
 
 /* eslint-disable */
-function RoleSelector({data: options, value, setValue, children, hidden=false}) {
+function RoleSelector({
+    data: options,
+    value,
+    setValue,
+    children,
+    hidden = false,
+}) {
     const [studentContent, facultyContent] = children;
 
     return (
@@ -30,8 +38,8 @@ function RoleSelector({data: options, value, setValue, children, hidden=false}) 
                     <RoleDropdown options={options} setValue={setValue} />
                 </label>
             </form>
-            {value === 'Student' && studentContent}
-            {!['', undefined, 'Student'].includes(value) && facultyContent}
+            {value === "Student" && studentContent}
+            {!["", undefined, "Student"].includes(value) && facultyContent}
         </div>
     );
 }
--- src/app/pages/blog/gated-content-dialog/gated-content-dialog.js
+++ src/app/pages/blog/gated-content-dialog/gated-content-dialog.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {useDialog} from '~/components/dialog/dialog';
+import { useDialog } from '~/components/dialog/dialog';
 import linkHelper from '~/helpers/link';
 import useUserContext from '~/contexts/user';
-import {useDataFromSlug, camelCaseKeys} from '~/helpers/page-data-utils';
+import { useDataFromSlug, camelCaseKeys } from '~/helpers/page-data-utils';
 import ContactInfo from '~/components/contact-info/contact-info';
-import {RoleDropdown} from '~/components/role-selector/role-selector';
+import { RoleDropdown } from '~/components/role-selector/role-selector';
 import FormInput from '~/components/form-input/form-input';
 import DropdownSelect from '~/components/select/drop-down/drop-down';
 import useFormTarget from '~/components/form-target/form-target';
@@ -16,8 +16,11 @@ const formSubmitUrl = window.SETTINGS.gatedContentEndpoint;
 function SubjectSelector() {
     const data = useDataFromSlug('snippets/subjects?locale=en');
     const options = React.useMemo(
-        () => camelCaseKeys(data || [])
-            .map((obj) => ({label: obj.name, value: obj.name})),
+        () =>
+            camelCaseKeys(data || []).map((obj) => ({
+                label: obj.name,
+                value: obj.name
+            })),
         [data]
     );
     const [value, setValue] = React.useState();
@@ -28,7 +31,8 @@ function SubjectSelector() {
             <div className="control-group">
                 <label className="field-label">Subject of interest</label>
                 <DropdownSelect
-                    name="subject" required
+                    name="subject"
+                    required
                     options={options}
                     placeholder="Please select one"
                     onValueUpdate={setValue}
@@ -39,7 +43,7 @@ function SubjectSelector() {
     );
 }
 
-function RoleSelector({value, setValue}) {
+function RoleSelector({ value, setValue }) {
     const roleData = useDataFromSlug('snippets/roles');
     const roleOptions = React.useMemo(
         () => camelCaseKeys(roleData) || [],
@@ -51,44 +55,52 @@ function RoleSelector({value, setValue}) {
         <div className="form-input">
             <div className="control-group">
                 <label className="field-label">Your role</label>
-                <RoleDropdown options={roleOptions} setValue={setValue} />
+                <RoleDropdown
+                    options={roleOptions}
+                    setValue={setValue}
+                    name="role"
+                />
                 <div className="invalid-message">{message}</div>
             </div>
         </div>
     );
 }
 
-function GatedContentForm({submitted, setSubmitted}) {
+function GatedContentForm({ submitted, setSubmitted }) {
     const afterSubmit = React.useCallback(
         () => setSubmitted(true),
         [setSubmitted]
     );
-    const {onSubmit, submitting, FormTarget} = useFormTarget(afterSubmit);
+    const { onSubmit, submitting, FormTarget } = useFormTarget(afterSubmit);
     const [selectedRole, setSelectedRole] = React.useState();
 
     return (
         <React.Fragment>
             <FormTarget submitting={submitting} />
             <form
-                className={cn({submitted})} action={formSubmitUrl} onSubmit={onSubmit}
+                className={cn({ submitted })}
+                action={formSubmitUrl}
+                onSubmit={onSubmit}
                 target="form-target"
             >
                 <ContactInfo>
                     <SubjectSelector />
-                    <RoleSelector value={selectedRole} setValue={setSelectedRole} />
-                    {
-                        selectedRole === 'Faculty' &&
-                            <FormInput
-                                label="Number of students you teach"
-                                inputProps={{
-                                    type: 'number',
-                                    name: 'num_students',
-                                    required: true,
-                                    min: 1,
-                                    max: 999
-                                }}
-                            />
-                    }
+                    <RoleSelector
+                        value={selectedRole}
+                        setValue={setSelectedRole}
+                    />
+                    {selectedRole === 'Faculty' && (
+                        <FormInput
+                            label="Number of students you teach"
+                            inputProps={{
+                                type: 'number',
+                                name: 'num_students',
+                                required: true,
+                                min: 1,
+                                max: 999
+                            }}
+                        />
+                    )}
                 </ContactInfo>
                 <button className="primary">Submit</button>
             </form>
@@ -96,7 +108,7 @@ function GatedContentForm({submitted, setSubmitted}) {
     );
 }
 
-function GatedContentBody({submitted, setSubmitted}) {
+function GatedContentBody({ submitted, setSubmitted }) {
     const loginLocation = new window.URL(linkHelper.loginLink());
 
     return (
@@ -105,17 +117,21 @@ function GatedContentBody({submitted, setSubmitted}) {
                 This post is exclusive. Please login or fill out the form to
                 access the article.
             </div>
-            <a className="btn secondary" href={loginLocation.href} data-local="true">
+            <a
+                className="btn secondary"
+                href={loginLocation.href}
+                data-local="true"
+            >
                 Login to my account
             </a>
-            <GatedContentForm {...{submitted, setSubmitted}} />
+            <GatedContentForm {...{ submitted, setSubmitted }} />
         </div>
     );
 }
 
 function GatedContentDialog() {
     const [Dialog, open, close] = useDialog();
-    const {userModel} = useUserContext();
+    const { userModel } = useUserContext();
     const [submitted, setSubmitted] = React.useState(false);
 
     React.useEffect(() => {
@@ -131,11 +147,11 @@ function GatedContentDialog() {
             title="Thanks for reading the OpenStax blog."
             className="gated-content"
         >
-            <GatedContentBody {...{submitted, setSubmitted}} />
+            <GatedContentBody {...{ submitted, setSubmitted }} />
         </Dialog>
     );
 }
 
-export default function WaitForData({articleData}) {
-    return (articleData?.gatedContent ? <GatedContentDialog /> : null);
+export default function WaitForData({ articleData }) {
+    return articleData?.gatedContent ? <GatedContentDialog /> : null;
 }
