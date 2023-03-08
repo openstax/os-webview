729b40cae08fb00f80c4cbe064673ec65df474c3
Roy Johnson <roy.e.johnson@rice.edu>
2023-03-08 13:50:54 -0600

    Fix carousel layout issues (#2357)

--- src/app/components/carousel/carousel.js
+++ src/app/components/carousel/carousel.js
@@ -6,50 +6,68 @@ import useWindowContext, {WindowContextProvider} from '~/contexts/window';
 import cn from 'classnames';
 import './carousel.scss';
 
-export function FrameChanger({chevronDirection, onClick, hoverText, disable=false}) {
-    const icon = ({
-        'left': faChevronLeft,
-        'right': faChevronRight
-    })[chevronDirection];
+export function FrameChanger({
+    chevronDirection,
+    onClick,
+    hoverText,
+    disable = false
+}) {
+    const icon = {
+        left: faChevronLeft,
+        right: faChevronRight
+    }[chevronDirection];
 
     return (
         <button
-            type="button" className={`frame-changer ${chevronDirection}`}
-            onClick={onClick} disabled={disable}
+            type='button'
+            className={`frame-changer ${chevronDirection}`}
+            onClick={onClick}
+            disabled={disable}
         >
             <FontAwesomeIcon icon={icon} />
-            {
-                hoverText && <span className="hover-text">{hoverText}</span>
-            }
+            {hoverText && <span className='hover-text'>{hoverText}</span>}
         </button>
     );
 }
 
-function LeftFrameChanger({frameNumber, setFrameNumber, step=1, hoverTextThing}) {
+function LeftFrameChanger({
+    frameNumber,
+    setFrameNumber,
+    step = 1,
+    hoverTextThing
+}) {
     const destFrame = frameNumber - step;
     const hoverText = hoverTextThing ? `Previous ${hoverTextThing}` : null;
 
     return (
-        destFrame >= 0 &&
+        destFrame >= 0 && (
             <FrameChanger
-                chevronDirection="left"
+                chevronDirection='left'
                 onClick={() => setFrameNumber(destFrame)}
                 hoverText={hoverText}
             />
+        )
     );
 }
 
-function RightFrameChanger({frameNumber, frameCount, setFrameNumber, step=1, hoverTextThing}) {
+function RightFrameChanger({
+    frameNumber,
+    frameCount,
+    setFrameNumber,
+    step = 1,
+    hoverTextThing
+}) {
     const destFrame = frameNumber + step;
     const hoverText = hoverTextThing ? `Next ${hoverTextThing}` : null;
 
     return (
-        destFrame < frameCount - 1 &&
+        destFrame <= frameCount - 1 && (
             <FrameChanger
-                chevronDirection="right"
+                chevronDirection='right'
                 onClick={() => setFrameNumber(destFrame)}
                 hoverText={hoverText}
             />
+        )
     );
 }
 
@@ -63,19 +81,21 @@ function getStep(atATime, containerEl, frameNumber) {
         containerEl.querySelectorAll('.items > *')
     ).slice(frameNumber);
     const {left} = itemsFromCurrent[0].getBoundingClientRect();
-    const wholeItems = itemsFromCurrent.filter(
-        (i) => {
-            const {right} = i.getBoundingClientRect();
+    const wholeItems = itemsFromCurrent.filter((i) => {
+        const {right} = i.getBoundingClientRect();
 
-            return right - left <= vpWidth;
-        }
-    );
+        return right - left <= vpWidth;
+    });
 
     return Math.max(Number(atATime), wholeItems.length);
 }
 
 function Carousel({
-    children, atATime=1, mobileSlider=false, initialFrame=0, hoverTextThing,
+    children,
+    atATime = 1,
+    mobileSlider = false,
+    initialFrame = 0,
+    hoverTextThing,
     frameCount = React.Children.count(children)
 }) {
     const [frameNumber, setFrameNumber] = React.useState(initialFrame);
@@ -85,31 +105,37 @@ function Carousel({
     // Not useMemo because it has to update when children render
     const step = getStep(atATime, ref.current, frameNumber);
 
-    React.useLayoutEffect(
-        () => {
-            const targetItem = ref.current.querySelectorAll('.items > *')[frameNumber];
-            const {left: viewportLeft} = ref.current.getBoundingClientRect();
-            const {left: targetLeft} = targetItem.getBoundingClientRect();
-            const left = targetLeft - viewportLeft + ref.current.scrollLeft;
+    React.useLayoutEffect(() => {
+        const targetItem =
+            ref.current.querySelectorAll('.items > *')[frameNumber];
+        const {left: viewportLeft} = ref.current.getBoundingClientRect();
+        const {left: targetLeft} = targetItem.getBoundingClientRect();
+        const left = targetLeft - viewportLeft + ref.current.scrollLeft;
 
-            ref.current.scrollTo({
-                left,
-                behavior: firstTimeRef.current ? 'auto' : 'smooth'
-            });
-            firstTimeRef.current = false;
-        },
-        [frameNumber, wcx.innerWidth]
-    );
+        ref.current.scrollTo({
+            left,
+            behavior: firstTimeRef.current ? 'auto' : 'smooth'
+        });
+        firstTimeRef.current = false;
+    }, [frameNumber, wcx.innerWidth]);
 
     return (
         <div className={cn('carousel', {'mobile-slider': mobileSlider})}>
-            <div className="viewport" ref={ref}>
-                <div className="items">
-                    {children}
-                </div>
+            <div className='viewport' ref={ref}>
+                <div className='items'>{children}</div>
             </div>
-            <LeftFrameChanger {...{frameNumber, setFrameNumber, step, hoverTextThing}} />
-            <RightFrameChanger {...{frameNumber, setFrameNumber, frameCount, step, hoverTextThing}} />
+            <LeftFrameChanger
+                {...{frameNumber, setFrameNumber, step, hoverTextThing}}
+            />
+            <RightFrameChanger
+                {...{
+                    frameNumber,
+                    setFrameNumber,
+                    frameCount,
+                    step,
+                    hoverTextThing
+                }}
+            />
         </div>
     );
 }
