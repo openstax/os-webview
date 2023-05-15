29bc21df4df0618e2616aaf0db3d69d14f136f87
Roy Johnson <roy.e.johnson@rice.edu>
2023-05-15 15:05:37 -0500

    Add howUsing to json

--- src/app/pages/adoption/how-using/how-using.js
+++ src/app/pages/adoption/how-using/how-using.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import FormInput from '~/components/form-input/form-input';
+import FormSelect from '~/components/form-select/form-select';
+import {adoptionOptions} from '~/contexts/salesforce';
 import './how-using.scss';
 
 function HowManyStudents({book, dispatch}) {
@@ -27,6 +29,26 @@ function HowManyStudents({book, dispatch}) {
     );
 }
 
+function HowUsingBook({book, dispatch}) {
+    const updateBookValue = React.useCallback(
+        (value) => dispatch({[book.value]: value}),
+        [book, dispatch]
+    );
+
+    return (
+        <FormSelect
+            label={`How are you using ${book.text}?`}
+            options={adoptionOptions}
+            selectAttributes={{
+                name: `hu_${book.text}`,
+                placeholder: 'Please select one',
+                required: true
+            }}
+            onValueUpdate={updateBookValue}
+        />
+    );
+}
+
 
 function reducer(state, action) {
     return {...state, ...action};
@@ -34,24 +56,28 @@ function reducer(state, action) {
 
 export default function HowUsing({selectedBooks}) {
     const [bookData, dispatch] = React.useReducer(reducer, {});
+    const [useData, udDispatch] = React.useReducer(reducer, {});
     const json = React.useMemo(
         () => {
             const rewrittenBookData = selectedBooks.map(
-                ({value: name}) => ({name, students: Number(bookData[name])})
+                ({value: name}) => ({name, students: Number(bookData[name]), howUsing: useData[name]})
             );
 
             return JSON.stringify({
                 'Books': rewrittenBookData
             });
         },
-        [bookData, selectedBooks]
+        [bookData, useData, selectedBooks]
     );
 
     return (
         <div className="how-using">
             {
                 selectedBooks.map((book) =>
-                    <HowManyStudents book={book} key={book.value} dispatch={dispatch} />
+                    <React.Fragment key={book.value}>
+                        <HowManyStudents book={book} dispatch={dispatch} />
+                        <HowUsingBook book={book} dispatch={udDispatch} />
+                    </React.Fragment>
                 )
             }
             <input type="hidden" name="adoption_json" value={json} />
