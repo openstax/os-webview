9ee81c2507aa53880f39e652ff30cc32ab9787c3
Roy Johnson <roy.e.johnson@rice.edu>
2023-05-16 15:34:03 -0500

    Assignable image carousel added
    Just placeholder images for now

--- src/app/pages/assignable/sections/about/about.js
+++ src/app/pages/assignable/sections/about/about.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import RawHTML from '~/components/jsx-helpers/raw-html';
 import useOptimizedImage from '~/helpers/use-optimized-image';
+import { useToggle } from '~/helpers/data';
+import Dialog from '~/components/dialog/dialog';
+import Carousel from '~/components/carousel/carousel';
 import './about.scss';
+
+function CarouselDialog({isOpen, onPutAway}) {
+    const images = [1, 2, 3, 4].map((i) => `https://via.placeholder.com/640x480?text=[image ${i}]`);
+
+    return (
+        <Dialog title="Screenshots" isOpen={isOpen} onPutAway={onPutAway}>
+            <section className="carousel">
+                <Carousel>
+                    {
+                        images.map(
+                            (src, i) =>
+                                <div className="img-container" key={src}>
+                                    <img src={src} alt={`#${i}`} />
+                                </div>
+                        )
+                    }
+                </Carousel>
+            </section>
+        </Dialog>
+    );
+}
 
 export default function About({
     data: {
@@ -12,6 +36,7 @@ export default function About({
     }
 }) {
     const optimizedImage = useOptimizedImage(image);
+    const [isOpen, toggle] = useToggle(false);
 
     return (
         <section className="about near-white">
@@ -20,7 +45,10 @@ export default function About({
                     <h2>{heading}</h2>
                     <RawHTML className="description-block" html={description} />
                 </div>
-                <img src={optimizedImage} alt={altText} />
+                <button onClick={toggle}>
+                    <img src={optimizedImage} alt={altText} />
+                </button>
+                <CarouselDialog isOpen={isOpen} onPutAway={toggle} />
             </div>
         </section>
     );
