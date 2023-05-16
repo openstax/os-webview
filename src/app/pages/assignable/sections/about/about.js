bb1fc7ebf71877fe5c9ef7fef1e9f2cd578c6fcf
Roy Johnson <roy.e.johnson@rice.edu>
2023-05-16 15:34:03 -0500

    Assignable image carousel added
    Just placeholder images for now

--- src/app/pages/assignable/sections/about/about.js
+++ src/app/pages/assignable/sections/about/about.js
@@ -3,6 +3,27 @@ import RawHTML from '~/components/jsx-helpers/raw-html';
 import Carousel from '~/components/carousel/carousel';
 import './about.scss';
 
+function CarouselDialog({isOpen, onPutAway}) {
+    const images = [1, 2, 3, 4].map((i) => `https://via.placeholder.com/640x480?text=[image ${i}]`);
+
+    return (
+        <Dialog title="Screenshots" isOpen={isOpen} onPutAway={onPutAway}>
+            <section className="carousel">
+                <Carousel>
+                    {
+                        images.map(
+                            (src, i) =>
+                                <div className="img-container" key={src}>
+                                    <img src={src} alt={`#${i}`} />
+                                </div>
+                        )
+                    }
+                </Carousel>
+            </section>
+        </Dialog>
+    );
+}
+
 export default function About({
     data: {
         section2Heading: heading,
