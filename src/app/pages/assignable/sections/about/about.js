63bcf860404ebbe0683a12b12efed3161df0a7cf
Roy Johnson <roy.e.johnson@rice.edu>
2023-05-22 16:22:59 -0500

    Fetch images from CMS
    No longer have section image

--- src/app/pages/assignable/sections/about/about.js
+++ src/app/pages/assignable/sections/about/about.js
@@ -1,42 +1,16 @@
 import React from 'react';
 import RawHTML from '~/components/jsx-helpers/raw-html';
-import useOptimizedImage from '~/helpers/use-optimized-image';
-import { useToggle } from '~/helpers/data';
-import Dialog from '~/components/dialog/dialog';
 import Carousel from '~/components/carousel/carousel';
 import './about.scss';
-
-function CarouselDialog({isOpen, onPutAway}) {
-    const images = [1, 2, 3, 4].map((i) => `https://via.placeholder.com/640x480?text=[image ${i}]`);
-
-    return (
-        <Dialog title="Screenshots" isOpen={isOpen} onPutAway={onPutAway}>
-            <section className="carousel">
-                <Carousel>
-                    {
-                        images.map(
-                            (src, i) =>
-                                <div className="img-container" key={src}>
-                                    <img src={src} alt={`#${i}`} />
-                                </div>
-                        )
-                    }
-                </Carousel>
-            </section>
-        </Dialog>
-    );
-}
 
 export default function About({
     data: {
         section2Heading: heading,
         section2Description: description,
-        altText,
-        section2Image: {meta: {downloadUrl: image}}
+        imageCarousel
     }
 }) {
-    const optimizedImage = useOptimizedImage(image);
-    const [isOpen, toggle] = useToggle(false);
+    const images = imageCarousel[0];
 
     return (
         <section className="about near-white">
@@ -45,10 +19,16 @@ export default function About({
                     <h2>{heading}</h2>
                     <RawHTML className="description-block" html={description} />
                 </div>
-                <button onClick={toggle}>
-                    <img src={optimizedImage} alt={altText} />
-                </button>
-                <CarouselDialog isOpen={isOpen} onPutAway={toggle} />
+                <Carousel>
+                    {
+                        images.map(
+                            ({image: {file: src, height, width, title, id}}) =>
+                                <div className="img-container" key={id}>
+                                    <img src={src} alt={title} width={width} height={height} />
+                                </div>
+                        )
+                    }
+                </Carousel>
             </div>
         </section>
     );
