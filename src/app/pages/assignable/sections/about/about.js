4c258556a8771e3fdf84805bdab854102149f089
Roy Johnson <roy.e.johnson@rice.edu>
2023-05-26 13:18:07 -0500

    Assignable image carousel added (#2402)
    * Assignable image carousel added
    
    Just placeholder images for now
    
    * Fetch images from CMS
    
    No longer have section image
    
    * Handle 403 for user
    
    * Adjust carousel layout

--- src/app/pages/assignable/sections/about/about.js
+++ src/app/pages/assignable/sections/about/about.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import RawHTML from '~/components/jsx-helpers/raw-html';
-import useOptimizedImage from '~/helpers/use-optimized-image';
+import Carousel from '~/components/carousel/carousel';
 import './about.scss';
 
 export default function About({
     data: {
         section2Heading: heading,
         section2Description: description,
-        altText,
-        section2Image: {meta: {downloadUrl: image}}
+        imageCarousel
     }
 }) {
-    const optimizedImage = useOptimizedImage(image);
+    const images = imageCarousel[0];
 
     return (
         <section className="about near-white">
@@ -20,7 +19,16 @@ export default function About({
                     <h2>{heading}</h2>
                     <RawHTML className="description-block" html={description} />
                 </div>
-                <img src={optimizedImage} alt={altText} />
+                <Carousel>
+                    {
+                        images.map(
+                            ({image: {file: src, height, width, title, id}}) =>
+                                <div className="img-container" key={id}>
+                                    <img src={src} alt={title} width={width} height={height} />
+                                </div>
+                        )
+                    }
+                </Carousel>
             </div>
         </section>
     );
