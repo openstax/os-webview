936b098a36859066112181cb41ef973102bcffac
Roy Johnson <roy.e.johnson@rice.edu>
2023-07-11 13:42:09 -0500

    Display Coming Soon badge on coming_soon books on k12 pages

--- src/app/models/book-titles.js
+++ src/app/models/book-titles.js
@@ -1,4 +1,4 @@
 import cmsFetch from '~/helpers/cms-fetch';
 
-export default cmsFetch('pages/?type=books.Book&fields=title,id&limit=250')
+export default cmsFetch('pages/?type=books.Book&fields=title,id,book_state&limit=250')
     .then((r) => r.items);
