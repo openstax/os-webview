5806852b0e65338a6164981d0c423ad3a5f1dca4
Roy Johnson <roy.e.johnson@rice.edu>
2023-08-29 11:29:44 -0500

    Blog -> Webinar in analytics on Webinars page

--- src/app/pages/webinars/main-page/main-page.tsx
+++ src/app/pages/webinars/main-page/main-page.tsx
@@ -24,7 +24,7 @@ export default function MainPage() {
             />
             <ExploreByCollection
                 collections={collections}
-                analyticsNav='Blog Collections'
+                analyticsNav='Webinar Collections'
             />
             <LatestWebinars />
         </div>
