cca06da50514545769b9d635d77eb0e2accf4f28
Roy Johnson <roy.e.johnson@rice.edu>
2023-09-15 11:22:01 -0500

    Adjust layout of no-results page (#2474)

--- src/app/pages/blog/search-results/no-results.tsx
+++ src/app/pages/blog/search-results/no-results.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PinnedArticle from '../pinned-article/pinned-article';
+import Section from '~/components/explore-page/section/section';
 import {LatestBlurbs} from '../more-stories/more-stories';
 import './no-results.scss';
 
@@ -9,7 +10,9 @@ export default function NoResults() {
             <h1>No matching blog posts found</h1>
             <div>Our featured and recent blog posts are below.</div>
             <PinnedArticle />
-            <LatestBlurbs page={1} pageSize={3} openInNewWindow />
+            <Section className="more-stories" name="Latest blog posts">
+                <LatestBlurbs page={1} pageSize={3} openInNewWindow />
+            </Section>
         </div>
     );
 }
