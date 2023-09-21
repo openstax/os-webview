bdf6afac5183c615f6f3dd89e9512300a0e802e9
Roy Johnson <roy.e.johnson@rice.edu>
2023-09-21 13:29:14 -0500

    Include badge in book cover link

--- src/app/components/book-tile/book-tile.js
+++ src/app/components/book-tile/book-tile.js
@@ -125,9 +125,9 @@ export default function BookTile({ book: [book] }) {
 
     return (
         <div className={classes}>
-            {assignable && <AssignableBadge />}
             <a href={`/details/${slug}`}>
                 <img src={coverUrl} role="presentation" width="240" height="240" />
+                {assignable && <AssignableBadge />}
             </a>
             <div className="text-block">
                 <a href={`/details/${slug}`}>{title}</a>
