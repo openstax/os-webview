3a62d93b4a0b6e787a357edfea645c7f50160871
Roy Johnson <roy.e.johnson@rice.edu>
2023-11-27 16:46:29 -0600

    Strip "books/" from slug used on BuyPrint server

--- src/app/pages/details/common/get-this-title-files/amazon-associates-link.tsx
+++ src/app/pages/details/common/get-this-title-files/amazon-associates-link.tsx
@@ -9,9 +9,10 @@ type AmazonAssociatesLink = {
 
 export default function useAmazonAssociatesLink(slug: string) {
     const [data, setData] = React.useState<AmazonAssociatesLink>({});
+    const strippedSlug = slug.replace('books/', '');
 
     React.useEffect(() => {
-        fetch(`${buyprintServer}/${slug}.json`)
+        fetch(`${buyprintServer}/${strippedSlug}.json`)
             .then((response) => response.json())
             .then(({buy_urls: urls}) => {
                 if (urls.length > 0) {
