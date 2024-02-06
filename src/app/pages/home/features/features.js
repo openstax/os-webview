be1111783d6aa596b1bd220eeb88a6e23ff4c6d5
Roy Johnson <roy.e.johnson@rice.edu>
2024-02-06 17:04:34 -0600

    Use tablist for dynamic features list on homepage
    For: https://github.com/openstax/openstax.org/issues/2401

--- src/app/pages/home/features/features.js
+++ src/app/pages/home/features/features.js
@@ -6,16 +6,16 @@ import {Tabs, Item} from '~/components/tablist/tablist';
 import './features.scss';
 
 function Feature({name}) {
-    return <div className="feature">{name}</div>;
+    return <div className='feature'>{name}</div>;
 }
 
 function FeatureList({featureList, exploreLink}) {
     return (
-        <div className="feature-list">
+        <div className='feature-list'>
             {featureList?.map((f) => (
                 <Feature key={f} name={f} />
             ))}
-            <a className="feature explore" href={exploreLink}>
+            <a className='feature explore' href={exploreLink}>
                 <span>Explore now</span>&nbsp;
                 <FontAwesomeIcon icon={faArrowRight} />
             </a>
@@ -27,18 +27,18 @@ export default function Features({data}) {
     const bgImage = useOptimizedImage(data.bgImage, 570);
 
     return (
-        <section className="features">
-            <img className="right-bg" src={bgImage} alt />
-            <div className="boxed text-block">
+        <section className='features'>
+            <img className='right-bg' src={bgImage} alt />
+            <div className='boxed text-block'>
                 <h2>{data.headline}</h2>
-                <Tabs aria-label="Features" defaultSelectedKey="instructors">
-                    <Item key="instructors" title={data.tab1Heading}>
+                <Tabs aria-label='Features' defaultSelectedKey='instructors'>
+                    <Item key='instructors' title={data.tab1Heading}>
                         <FeatureList
                             featureList={data.tab1Features}
                             exploreLink={data.tab1ExploreUrl}
                         />
                     </Item>
-                    <Item key="students" title={data.tab2Heading}>
+                    <Item key='students' title={data.tab2Heading}>
                         <FeatureList
                             featureList={data.tab2Features}
                             exploreLink={data.tab2ExploreUrl}
