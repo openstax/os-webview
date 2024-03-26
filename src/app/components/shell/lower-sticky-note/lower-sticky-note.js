1605bfb731b059100e5740bdf579705cf801c8d3
Roy Johnson <roy.e.johnson@rice.edu>
2024-03-26 11:24:32 -0500

    Hide sticky note give banner on Economics books details pages
    [DISCO-169]

--- src/app/components/shell/lower-sticky-note/lower-sticky-note.js
+++ src/app/components/shell/lower-sticky-note/lower-sticky-note.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {usePutAway, useStickyData} from '../shared.js';
+import {useLocation} from 'react-router-dom';
 import JITLoad from '~/helpers/jit-load';
+
+function useIsTemporarilyBanned() {
+    const {pathname} = useLocation();
+    const econBanUntil = new Date('2024-05-16').valueOf();
+    const inBanPeriod = Date.now().valueOf() < econBanUntil;
+
+    return ((/princ.*econ.*/).test(pathname) && inBanPeriod);
+}
 
 export default function LowerStickyNote() {
     const stickyData = useStickyData();
     const [closed, PutAway] = usePutAway();
+    const isTemporarilyBanned = useIsTemporarilyBanned();
     const shouldNotDisplay = !stickyData || closed ||
+        isTemporarilyBanned ||
         stickyData.mode !== 'banner';
 
     if (shouldNotDisplay) {
