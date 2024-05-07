0f4e254eb0f0d68f5f11addfa019c9dc6152e7af
Roy Johnson <roy.e.johnson@rice.edu>
2024-05-07 15:12:11 -0500

    2.125.0 Merge 1 May release to main (#2584)
    * Update version; upgrade packages
    
    * Change salesforce subdomain assignment
    
    * Fix carousel styling on Assignable page
    
    * Handle no adoptions returned
    
    * Disable adoptions query for this release.

--- src/app/pages/renewal-form/renewal-form.js
+++ src/app/pages/renewal-form/renewal-form.js
@@ -3,7 +3,8 @@ import {useLocation} from 'react-router-dom';
 import linkHelper from '~/helpers/link';
 import useUserContext from '~/contexts/user';
 import useAdoptions from '~/models/renewals';
-import adoptionsPromise from './salesforce-data';
+// -- We'll be trying to do this for the next release.
+// import _adoptionsPromise from './salesforce-data';
 import BookTagsMultiselect, {BookTagsContextProvider, useBookTagsContext}
     from '~/components/multiselect/book-tags/book-tags';
 
@@ -173,12 +174,12 @@ function EnsureLoggedIn() {
     const {userStatus: {uuid}} = useUserContext();
     const defaultMsg = `Reporting your use of OpenStax helps us
     secure additional funding for future titles!`;
-    const [adoptionInfo, setAdoptionInfo] = React.useState(defaultMsg);
+    const [adoptionInfo, _setAdoptionInfo] = React.useState(defaultMsg);
 
-    React.useEffect(
-        () => adoptionsPromise.then((info) => info && setAdoptionInfo(info)),
-        []
-    );
+    // React.useEffect(
+    //     () => adoptionsPromise.then((info) => info && setAdoptionInfo(info)),
+    //     []
+    // );
 
     React.useEffect(
         () => {
