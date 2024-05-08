afa2a904a941de214d0fa3f3771d1ae69bf9f637
Roy Johnson <roy.e.johnson@rice.edu>
2024-05-08 10:15:01 -0500

    Provide translations for current Role list

--- src/app/components/role-selector/role-selector.js
+++ src/app/components/role-selector/role-selector.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LoaderPage from '~/components/jsx-helpers/loader-page';
 import DropdownSelect from '~/components/select/drop-down/drop-down';
 import {FormattedMessage, useIntl} from 'react-intl';
+import useLanguageContext from '~/contexts/language';
 import './role-selector.scss';
 
 export function RoleDropdown({ options, setValue, name = 'subject' }) {
@@ -22,6 +23,18 @@ export function RoleDropdown({ options, setValue, name = 'subject' }) {
     );
 }
 
+// Jest didn't like dynamic id for formatMessage
+const spanishRoles = {
+    'Student': 'Estudiante',
+    'Faculty': 'Instructor(a)',
+    'Home School Teacher': 'Instructor(a) de educación en el hogar',
+    'Administrator': 'Administrador(a)',
+    'Librarian': 'Bibliotecario',
+    'Instructional Designer': 'Diseñador(a) instruccional',
+    'Adjunct Faculty': 'Adjunto Facultad',
+    'Other': 'Otro'
+};
+
 /* eslint-disable */
 function RoleSelector({
     data: options,
@@ -31,13 +44,19 @@ function RoleSelector({
     hidden = false
 }) {
     const [studentContent, facultyContent] = children;
+    const {language} = useLanguageContext();
+    const translatedOptions = options.map(
+        (opt) => {
+            return {...opt, displayName: language === 'es' && spanishRoles[opt.salesforceName] || opt.displayName};
+        }
+    );
 
     return (
         <div className="role-selector">
             <form data-region="selector">
                 <label hidden={hidden}>
                     <FormattedMessage id="role-selector.i-am" defaultMessage='I am a' />
-                    <RoleDropdown options={options} setValue={setValue} />
+                    <RoleDropdown options={translatedOptions} setValue={setValue} />
                 </label>
             </form>
             {value === "Student" && studentContent}
