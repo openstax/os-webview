c4030827d81d8f0c2444b30ecacb4e962e717edc
Roy Johnson <roy.e.johnson@rice.edu>
2024-05-21 09:33:49 -0500

    Merge changes from 15 May 2024 release (#2599)
    * Update version, upgrade packages
    
    * Tighten up Amazon button block
    
    * Add tracking parameters to renewal form

--- src/app/components/tracking-parameters/tracking-parameters.tsx
+++ src/app/components/tracking-parameters/tracking-parameters.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function TrackingParameters() {
+export default function TrackingParameters({source='OS Web'}) {
     return (
         <React.Fragment>
-            <input type="hidden" name="application_source" value="OS Web" />
+            <input type="hidden" name="application_source" value={source} />
             <input type="hidden" name="tracking_parameters" value={window.location.href} />
         </React.Fragment>
     );
