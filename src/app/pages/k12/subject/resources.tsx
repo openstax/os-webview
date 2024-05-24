9f0d12d58cc37767f4a813e8f4e34f0f9d379361
Roy Johnson <roy.e.johnson@rice.edu>
2024-05-24 17:27:48 -0500

    Track k12 resource downloads (#2610)
    * functional
    
    * Resources to TSX

--- src/app/pages/k12/subject/resources.tsx
+++ src/app/pages/k12/subject/resources.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import {Tabs, Item} from '~/components/tablist/tablist';
 import useUserContext from '~/contexts/user';
 import {useOpenGiveDialog} from '~/pages/details/common/get-this-title-files/give-before-pdf/use-give-dialog';
+import trackLink from '~/pages/details/common/track-link';
+import {TrackedMouseEvent} from '~/components/shell/router-helpers/useLinkHandler';
 import './resources.scss';
 
-function LinkWithGiveDialog({href, text}) {
+function LinkWithGiveDialog({href, text, track}: {
+    href: string;
+    text: string;
+    track: string;
+}) {
     const {GiveDialog, openGiveDialog} = useOpenGiveDialog();
+    const {userStatus} = useUserContext();
+    const {pathname} = useLocation();
+    const trackDownloadClick = React.useCallback(
+        (event: React.MouseEvent) => {
+            if (userStatus?.isInstructor) {
+                trackLink(event as TrackedMouseEvent, pathname);
+            }
+        },
+        [userStatus, pathname]
+    );
 
     return (
         <React.Fragment>
-            <a href={href} onClick={openGiveDialog}>{text}</a>
-            <GiveDialog link={href} variant='K12 resource' />
+            <a href={href} onClick={openGiveDialog} data-track={track}>{text}</a>
+            <GiveDialog
+            link={href} variant='K12 resource' track={track}
+            onDownload={trackDownloadClick}
+            />
         </React.Fragment>
     );
 }
 
-function ResourceLink({ data }) {
+function ResourceLink({ data, track }: {
+    data: {
+        book: string;
+        resourceUnlocked: boolean;
+        linkExternal: string;
+        linkDocumentUrl: string;
+    };
+    track: string;
+}) {
     const url = data.linkExternal || data.linkDocumentUrl;
     const {isVerified} = useUserContext();
 
@@ -23,17 +51,38 @@ function ResourceLink({ data }) {
         <li>
             {
                 data.resourceUnlocked || isVerified ?
-                <LinkWithGiveDialog href={url} text={data.book} /> :
+                <LinkWithGiveDialog href={url} text={data.book} track={track} /> :
                 <span>{data.book} (verified instructor only)</span>
             }
         </li>
     );
 }
 
-function ResourceToContent({ resources }) {
+type ResourceHeader = {
+    id: string;
+    heading: string;
+    resourceCategory: string;
+    description: string;
+    comingSoon: boolean;
+    comingSoonText: string;
+    k12: boolean;
+    videoReferenceNumber: number;
+    trackResource: boolean;
+    printLink: string;
+    book: string;
+    icon: string;
+    resourceUnlocked: boolean;
+    linkExternal: string;
+    linkDocumentUrl: string;
+};
+type ResourceDict = {
+    [name: string]: ResourceHeader[];
+}
+
+function ResourceToContent({ resources }: {resources: ResourceDict}) {
     return (
         <div className="card-grid">
-            {Reflect.ownKeys(resources)?.map((name) => {
+            {(Reflect.ownKeys(resources) as string[])?.map((name) => {
                 const resourceList = resources[name];
 
                 return (
@@ -44,7 +93,7 @@ function ResourceToContent({ resources }) {
                         </div>
                         <ul>
                             {resourceList.map((r) => (
-                                <ResourceLink key={r.book} data={r} />
+                                <ResourceLink key={r.book} data={r} track={name} />
                             ))}
                         </ul>
                     </div>
@@ -56,8 +105,8 @@ function ResourceToContent({ resources }) {
 
 // Resource headers have one entry for each heading/book combo
 // Need to group them by heading`
-function resourceHeadersToResources(resourceHeaders) {
-    return resourceHeaders.reduce((a, b) => {
+function resourceHeadersToResources(resourceHeaders: ResourceHeader[]) {
+    return resourceHeaders.reduce<{[key: string]: ResourceHeader[]}>((a, b) => {
         if (!(b.heading in a)) {
             a[b.heading] = [];
         }
@@ -66,14 +115,23 @@ function resourceHeadersToResources(resourceHeaders) {
     }, {});
 }
 
+type HeaderKeys = 'facultyResourceHeaders' | 'studentResourceHeaders';
 export default function Resources({
     data,
     labels,
     selectedLabel
+}: {
+    data: {
+        resourcesHeading: string;
+        facultyResourceHeaders: ResourceHeader[];
+        studentResourceHeaders: ResourceHeader[];
+    };
+    labels: string[];
+    selectedLabel: string;
 }) {
     const resources = React.useMemo(
         () => ['facultyResourceHeaders', 'studentResourceHeaders'].map(
-            (k) => resourceHeadersToResources(data[k])
+            (k) => resourceHeadersToResources(data[k as HeaderKeys])
         ),
         [data]
     );
