c96ecbed83ad5ec3c68497234ffd1ddd6b5b8352
Roy Johnson <roy.e.johnson@rice.edu>
2024-06-12 09:28:58 -0500

    Display 2 or 1 at a time on narrower screens

--- src/app/pages/subjects/new/specific/components/carousel-section.js
+++ src/app/pages/subjects/new/specific/components/carousel-section.js
@@ -28,7 +28,7 @@ export default function CarouselSection({
             <a className="btn primary" href={linkUrl}>
                 {linkText}
             </a>
-            <Carousel mobileSlider hoverTextThing={thing} ref={ref} atATime={atATime}>
+            <Carousel hoverTextThing={thing} ref={ref} atATime={atATime}>
                 {children}
             </Carousel>
         </React.Fragment>
