f87d90184738e7158bd0f5f73fdd6dae2261c322
Roy Johnson <roy.e.johnson@rice.edu>
2024-10-15 10:00:47 -0500

    BookViewer

--- src/app/pages/subjects/new/specific/book-viewer.tsx
+++ src/app/pages/subjects/new/specific/book-viewer.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import useSpecificSubjectContext from './context';
+import useSpecificSubjectContext, {CategoryData} from './context';
 import BookTile from '~/components/book-tile/book-tile';
 import {ActiveElementContextProvider} from '~/contexts/active-element';
 import {useLocation} from 'react-router-dom';
 import throttle from 'lodash/throttle';
 import './book-viewer.scss';
 
-function Category({category: [heading, categoryData]}) {
+function Category({
+    category: [heading, categoryData]
+}: {
+    category: [string, CategoryData];
+}) {
     const booksObj = categoryData.books;
     const books = Object.values(booksObj);
     const text = categoryData.categoryDescription;
     const {hash} = useLocation();
-    const ref = React.useRef();
+    const ref = React.useRef(null);
 
     // Image loads screw up the scroll-to location, so we listen for them
     // and re-scroll when images complete loading
     // throttled to reduce possible jitter
     React.useEffect(() => {
-        const isInitialScrollTarget = window.decodeURIComponent(hash.substring(1)) === heading;
+        const isInitialScrollTarget =
+            window.decodeURIComponent(hash.substring(1)) === heading;
 
         if (!isInitialScrollTarget) {
-            return null;
+            return undefined;
         }
         const throttleGoTo = throttle((el) => {
             el.scrollIntoView({block: 'center', behavior: 'smooth'});
@@ -36,12 +41,12 @@ function Category({category: [heading, categoryData]}) {
     }, [heading, hash]);
 
     return (
-        <div id={heading} className='category' ref={ref}>
+        <div id={heading} className="category" ref={ref}>
             <h2>{heading}</h2>
             <div>{text}</div>
-            <div className='books'>
+            <div className="books">
                 {books.map((b) => (
-                    <BookTile key={b.id} book={b} />
+                    <BookTile key={b[0].id} book={b} />
                 ))}
             </div>
         </div>
@@ -53,8 +58,8 @@ export default function BookViewer() {
 
     return (
         <ActiveElementContextProvider>
-            <section className='book-viewer'>
-                <div className='content'>
+            <section className="book-viewer">
+                <div className="content">
                     {categories.map((c) => (
                         <Category key={c[0]} category={c} />
                     ))}
