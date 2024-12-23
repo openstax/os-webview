d52c7382a4abf4a425c4f7775974914303285f05
Roy Johnson <roy.e.johnson@rice.edu>
2024-12-23 13:47:45 -0600

    carousel

--- src/app/components/carousel/carousel.tsx
+++ src/app/components/carousel/carousel.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import {Carousel as BaseCarousel, CarouselButton, CarouselItem, CarouselScroller} from 'react-aria-carousel';
+import {
+    Carousel as BaseCarousel,
+    CarouselButton,
+    CarouselItem,
+    CarouselScroller
+} from 'react-aria-carousel';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faChevronLeft} from '@fortawesome/free-solid-svg-icons/faChevronLeft';
 import {faChevronRight} from '@fortawesome/free-solid-svg-icons/faChevronRight';
 import './carousel.scss';
 
+// FrameChanger was part of the homegrown carousel, but react-aria-carousel has such items
+// built-in, so this is only used by pdf-unit (body-units) now. Might be able to use
+// CarouselButton (from react-aria-carousel) for that.
 export function FrameChanger({
     chevronDirection,
     onClick,
-    hoverText = '',
-    disable = false
+    hoverText,
+    disable
+}: {
+    chevronDirection: 'left' | 'right';
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
+    hoverText?: string;
+    disable?: boolean;
 }) {
     const icon = {
         left: faChevronLeft,
@@ -22,25 +35,23 @@ export function FrameChanger({
 
     return (
         <button
-            type='button'
+            type="button"
             className={`frame-changer ${chevronDirection}`}
             onClick={onClick}
             disabled={disable}
             aria-label={label}
         >
             <FontAwesomeIcon icon={icon} />
-            {hoverText && <span className='hover-text'>{hoverText}</span>}
+            {hoverText && <span className="hover-text">{hoverText}</span>}
         </button>
     );
 }
 
-function HoverText({which, thing}) {
+function HoverText({which, thing}: {which: string; thing: string}) {
     if (!thing) {
         return null;
     }
-    return (
-        <span className='hover-text'>{`${which} ${thing}`}</span>
-    );
+    return <span className="hover-text">{`${which} ${thing}`}</span>;
 }
 
 export default function Carousel({
@@ -48,27 +59,29 @@ export default function Carousel({
     children,
     hoverTextThing,
     ref
-}) {
-    const slides = React.useMemo(
-        () => {
-            return React.Children.toArray(children).map((child, i) =>
-            <CarouselItem index={i} key={i}>{child}</CarouselItem>);
-        },
-        [children]
-    );
+}: React.PropsWithChildren<{
+    atATime?: number;
+    hoverTextThing: string;
+    ref?: Parameters<typeof BaseCarousel>[0]['ref'];
+}>) {
+    const slides = React.useMemo(() => {
+        return React.Children.toArray(children).map((child, i) => (
+            <CarouselItem index={i} key={i}>
+                {child}
+            </CarouselItem>
+        ));
+    }, [children]);
 
     return (
         <BaseCarousel className="carousel" itemsPerPage={atATime} ref={ref}>
-            <CarouselScroller className="scroller">
-                {slides}
-            </CarouselScroller>
+            <CarouselScroller className="scroller">{slides}</CarouselScroller>
             <CarouselButton dir="prev" className="frame-changer left">
                 <FontAwesomeIcon icon={faChevronLeft} />
-                <HoverText which='Previous' thing={hoverTextThing} />
+                <HoverText which="Previous" thing={hoverTextThing} />
             </CarouselButton>
             <CarouselButton dir="next" className="frame-changer right">
                 <FontAwesomeIcon icon={faChevronRight} />
-                <HoverText which='Next' thing={hoverTextThing} />
+                <HoverText which="Next" thing={hoverTextThing} />
             </CarouselButton>
         </BaseCarousel>
     );
