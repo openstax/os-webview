87850133264ddee871e1995496c5d790724aeefc
Roy Johnson <roy.e.johnson@rice.edu>
2025-01-23 09:14:36 -0600

    Remove unused link helper functions

--- src/app/helpers/link.ts
+++ src/app/helpers/link.ts
@@ -5,20 +5,6 @@ const ABSOLUTE_OPENSTAX = new RegExp(
     `(?:https?://openstax.org|${window.location.origin})(?!/(?:books|accounts|oxauth|blog-feed|documents)/)`
 );
 const MAILTO = /^mailto:(.+)/;
-const CNX = /cnx.org/;
-const REX = /openstax.org\/books/;
-const CLOUDFRONT = /cloudfront.net/;
-const AMAZON = /amazon.com/;
-
-function findAncestor(el: HTMLElement) {
-    let parent: ParentNode | null = el;
-
-    while (parent && !(parent instanceof window.HTMLAnchorElement)) {
-        parent = parent.parentNode;
-    }
-
-    return parent;
-}
 
 function ignoreUrl(url: string | null) {
     return typeof url !== 'string' || MAILTO.test(url);
@@ -29,8 +15,7 @@ function ignoreClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
 }
 
 function validUrlClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
-    const target = e.target as HTMLAnchorElement;
-    const el = findAncestor(target) || target;
+    const el = (e.target as HTMLElement).closest('a') as HTMLAnchorElement;
     const href = el.getAttribute('href');
 
     if (ignoreClick(e) || ignoreUrl(href)) {
@@ -40,34 +25,12 @@ function validUrlClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
     return el;
 }
 
-function isTOCLink(el: Node) {
-    const tocEl = document.querySelector('.table-of-contents');
-
-    return tocEl && tocEl.contains(el);
-}
-
 function stripOpenStaxDomain(href: string) {
     return href.replace(ABSOLUTE_OPENSTAX, '');
 }
 
 function isExternal(href: string) {
     return EXTERNAL.test(href) && !ABSOLUTE_OPENSTAX.test(href);
-}
-
-function isCNX(href: string) {
-    return CNX.test(href);
-}
-
-function isREX(href: string) {
-    return REX.test(href);
-}
-
-function isAmazon(href: string) {
-    return AMAZON.test(href);
-}
-
-function isCloudFront(href: string) {
-    return CLOUDFRONT.test(href);
 }
 
 function loginOrOutLink(loginOrLogout: 'login' | 'logout') {
@@ -90,11 +53,6 @@ function logoutLink() {
 export default {
     isExternal,
     validUrlClick,
-    isTOCLink,
-    isCNX,
-    isREX,
-    isCloudFront,
-    isAmazon,
     loginLink,
     logoutLink,
     stripOpenStaxDomain
