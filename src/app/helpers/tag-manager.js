0acf91630e53f4593ad9bdfa1592de0de16297dc
Roy Johnson <roy.e.johnson@rice.edu>
2025-01-23 14:37:46 -0600

    Leave tag-manager as JS; it's not our code

--- src/app/helpers/tag-manager.js
+++ src/app/helpers/tag-manager.js
@@ -2,24 +2,20 @@
 import {accountsModel} from '~/models/usermodel';
 const tagManagerID = 'GTM-W6N7PB';
 
-const extendedWindow: Window & {
-  dataLayer?: unknown[];
-  oxDLF?: unknown[];
-} = window;
+window.dataLayer = window.dataLayer || [];
 
-extendedWindow.dataLayer = extendedWindow.dataLayer || [];
-extendedWindow.oxDLF = extendedWindow.oxDLF || [];
+window.oxDLF = window.oxDLF || [];
 
 // eslint-disable-next-line max-params
 (function (w, d, s, l, i) {
+    // Disable ESLint rules since we're copying Google's script
+    /* eslint-disable one-var, prefer-const, prefer-template */
     w[l] = w[l] || [];
     w[l].push({
         'gtm.start': new Date().getTime(),
         event: 'gtm.js'
     });
 
-    // Disable ESLint rules since we're copying Google's script
-    /* eslint-disable one-var, prefer-const, prefer-template */
     let f = d.getElementsByTagName(s)[0],
         j = d.createElement(s),
         dl = l !== 'dataLayer' ? '&l=' + l : '';
@@ -28,18 +24,18 @@ extendedWindow.oxDLF = extendedWindow.oxDLF || [];
     if (f) {
         j.async = true;
         j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode?.insertBefore(j, f);
+        f.parentNode.insertBefore(j, f);
     }
-})(extendedWindow, document, 'script' as const, 'dataLayer' as const, tagManagerID);
+})(window, document, 'script', 'dataLayer', tagManagerID);
 
 
 /* the analytics helper delays analytics until both of these are set
  * they can also be set again later and it'll trigger a re-configure */
 accountsModel.load().then(
   (user) => {
-    extendedWindow.oxDLF?.push({user});
+    window.oxDLF.push({user});
   }
 );
-export function setContentTags(contentTags: unknown) {
-  extendedWindow.oxDLF?.push({contentTags});
+export function setContentTags(contentTags) {
+  window.oxDLF.push({contentTags});
 }
