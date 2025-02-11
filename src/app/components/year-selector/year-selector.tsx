ad6144ec68bbc1475adb9aff7a1e23a6d204a57f
Roy Johnson <roy.e.johnson@rice.edu>
2025-02-11 11:03:35 -0600

    Prettier & tests

--- src/app/components/year-selector/year-selector.tsx
+++ src/app/components/year-selector/year-selector.tsx
@@ -2,24 +2,24 @@ import React from 'react';
 import DropdownSelect from '~/components/select/drop-down/drop-down';
 
 const now = new Date();
-const previousAcademicYear = now.getFullYear() - (
-    now.getMonth() < 7 ? 2 : 1
-);
+const previousAcademicYear = now.getFullYear() - (now.getMonth() < 7 ? 2 : 1);
 
 export default function YearSelector({
-    selectedYear=(previousAcademicYear + 1).toString(),
+    selectedYear = (previousAcademicYear + 1).toString(),
     onValueUpdate
 }: {
     selectedYear?: string;
-    onValueUpdate?: (value: string) => void
+    onValueUpdate?: (value: string) => void;
 }) {
-    const yearOptions = React.useMemo(() =>
-        [0, 1, 2].map((plusYears) => previousAcademicYear + plusYears)
-        .map((startYear) => ({
-            label: `${startYear}-${startYear + 1}`,
-            value: startYear.toString(),
-            selected: selectedYear === startYear.toString()
-        })),
+    const yearOptions = React.useMemo(
+        () =>
+            [0, 1, 2]
+                .map((plusYears) => previousAcademicYear + plusYears)
+                .map((startYear) => ({
+                    label: `${startYear}-${startYear + 1}`,
+                    value: startYear.toString(),
+                    selected: selectedYear === startYear.toString()
+                })),
         [selectedYear]
     );
 
@@ -27,7 +27,7 @@ export default function YearSelector({
         <label className="year-selector">
             I am reporting usage of OpenStax books for the
             <DropdownSelect
-                name='baseYear'
+                name="baseYear"
                 options={yearOptions}
                 required
                 onValueUpdate={onValueUpdate}
---
+++ test/src/components/year-selector2.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render} from '@testing-library/preact';
+
+// The date needs to be set before the import
+jest.useFakeTimers().setSystemTime(new Date('2024-08-04'));
+describe('year-selector', () => {
+    it('starts with 1 year back when after July', async () => {
+        const YearSelector = (await import('~/components/year-selector/year-selector')).default;
+
+        render(<YearSelector />);
+        const options = Array.from(document.querySelectorAll('[role="option"]'));
+
+        expect(options[0].textContent).toBe('2023-2024');
+        expect(options[1].getAttribute('aria-selected')).toBeTruthy();
+    });
+});
