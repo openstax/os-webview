be3e5e18d3e9c2751025a27971310587230e0632
Roy Johnson <roy.e.johnson@rice.edu>
2025-02-14 14:07:43 -0600

    Test descriptor wording

--- test/src/components/year-selector2.test.tsx
+++ test/src/components/year-selector2.test.tsx
@@ -4,7 +4,7 @@ import {render} from '@testing-library/preact';
 // The date needs to be set before the import
 jest.useFakeTimers().setSystemTime(new Date('2024-08-04'));
 describe('year-selector', () => {
-    it('starts with 1 year back when after July', async () => {
+    it('starts with 1 year back when after June', async () => {
         const YearSelector = (await import('~/components/year-selector/year-selector')).default;
 
         render(<YearSelector />);
