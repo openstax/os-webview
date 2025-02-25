ace03268e4e4537bf3421fd7fb049dc374530c86
Roy Johnson <roy.e.johnson@rice.edu>
2025-02-25 14:49:26 -0600

    faq-section

--- src/app/components/faq-section/faq-section.tsx
+++ src/app/components/faq-section/faq-section.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import AccordionGroup from '~/components/accordion-group/accordion-group';
 
-export default function FAQ({heading, items}) {
+export default function FAQ({heading, items}: {
+    heading: string;
+    items: Parameters<typeof AccordionGroup>[0]['items'];
+}) {
     return (
         <div className='boxed'>
             <h1>{heading}</h1>
