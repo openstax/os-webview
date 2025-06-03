2865f1b20a59352f14b9e8e54289782ea68af772
Roy Johnson <roy.e.johnson@rice.edu>
2025-06-03 16:23:42 -0500

    Prettier

--- src/app/components/validation-message/validation-message.tsx
+++ src/app/components/validation-message/validation-message.tsx
@@ -5,27 +5,32 @@ export type ElementWithValidationMessage = HTMLElement & {
 };
 
 function matchesOrContainsValidationMessage(el: ElementWithValidationMessage) {
-    return el?.validationMessage
-    || el?.querySelector<ElementWithValidationMessage>(':invalid')?.validationMessage
-    || 'invalid input';
+    return (
+        el?.validationMessage ||
+        el?.querySelector<ElementWithValidationMessage>(':invalid')
+            ?.validationMessage ||
+        'invalid input'
+    );
 }
 
 // watchValue changes trigger updates
 // elementRef will be queryied for invalid elements
-export default function ValidationMessage({watchValue, elementRef}: {
+export default function ValidationMessage({
+    watchValue,
+    elementRef
+}: {
     watchValue?: object;
-    elementRef: React.MutableRefObject<ElementWithValidationMessage>
+    elementRef: React.MutableRefObject<ElementWithValidationMessage>;
 }) {
     const [validationMessage, setValidationMessage] = React.useState('');
 
     React.useEffect(
-        () => setValidationMessage(
-            matchesOrContainsValidationMessage(elementRef.current)
-        ),
+        () =>
+            setValidationMessage(
+                matchesOrContainsValidationMessage(elementRef.current)
+            ),
         [watchValue, elementRef, setValidationMessage]
     );
 
-    return (
-        <div className="invalid-message">{validationMessage}</div>
-    );
+    return <div className="invalid-message">{validationMessage}</div>;
 }
