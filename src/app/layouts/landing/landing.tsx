39347f6549a6399d05d69c7fc7d9ead0226a2796
Roy Johnson <roy.e.johnson@rice.edu>
2025-06-18 06:21:35 -0500

    CORE-899: Flex page handle give button option (#2741)
    * CORE-899: Flex page handle give button option
    
    [CORE-899]
    
    * Flaky test
    
    Update breadcrumb.test.tsx

--- src/app/layouts/landing/landing.tsx
+++ src/app/layouts/landing/landing.tsx
@@ -21,7 +21,7 @@ type Props = {
         layout: Array<{
             value: {
                 navLinks: LinkFields[];
-                showGive?: boolean;
+                showGiveNowButton?: boolean;
             };
         }>;
     };
@@ -39,16 +39,15 @@ export default function LandingLayout({
     children,
     data
 }: React.PropsWithChildren<Props>) {
-    const layoutValue = data?.layout[0]?.value;
-    const showGive = layoutValue?.showGive;
+    const {navLinks, showGiveNowButton} = data?.layout[0]?.value || {};
 
     // BrowserRouter has to include everything that uses useLocation
     return (
         <React.Fragment>
             <header className="landing-page-header">
                 <Header
-                    links={layoutValue?.navLinks ?? []}
-                    showGive={showGive}
+                    links={navLinks ?? []}
+                    showGive={showGiveNowButton}
                 />
             </header>
             <SalesforceContextProvider>
