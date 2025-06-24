eee91f1b537607d0b50c0bc76b13aa96744da9e1
Roy Johnson <roy.e.johnson@rice.edu>
2025-06-24 10:29:28 -0500

    Test what can be tested

--- src/app/components/shell/router-helpers/page-routes.tsx
+++ src/app/components/shell/router-helpers/page-routes.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import {useParams, Routes, Route, Navigate} from 'react-router-dom';
 import usePageData from '~/helpers/use-page-data';
-import Error404 from '~/pages/404/404';
 import FlexPage, {
     FlexPageData,
-    isFlexPage,
-    useDataToSetLayoutParameters
+    LayoutUsingData
 } from '~/pages/flex-page/flex-page';
 import useLayoutContext from '~/contexts/layout';
-import usePortalContext from '~/contexts/portal';
 import {assertDefined} from '~/helpers/data';
 import {GeneralPageFromSlug} from '~/pages/general/general';
 import {ImportedPage} from './page-loaders';
+import {RouteAsPortalOrNot} from './portal-page-routes';
 
 type PageData = FlexPageData & {
     meta: {slug: string};
@@ -83,7 +81,7 @@ const mismatch: Record<string, string> = {
     'edtech-partner-program': 'openstax-ally-technology-partner-program'
 };
 
-function usePageDataFromRoute() {
+export function usePageDataFromRoute() {
     const {dir: name, '*': other} = useParams();
     const data = usePageData<PageData>(`pages/${mismatch[name as string] ?? name}`, true);
     const hasError = data && 'error' in data;
@@ -91,49 +89,11 @@ function usePageDataFromRoute() {
     return {name, data, hasError, other};
 }
 
-// eslint-disable-next-line complexity
-function RouteAsPortalOrNot() {
-    const {name, data, hasError, other} = usePageDataFromRoute();
-    const {portalPrefix, setPortal} = usePortalContext();
-
-    if (!data) {
-        return null;
-    }
-
-    if (hasError) {
-        return <Error404 />;
-    }
-
-    const isFlex = !hasError && isFlexPage(data);
-    const isPortal = isFlex && data.layout[0].type === 'landing';
-
-    if (isPortal) {
-        if (portalPrefix !== `/${name}`) {
-            setPortal(assertDefined(name));
-            return null;
-        }
-        if (!other) {
-            return <FlexPage data={data} />;
-        }
-        return <PortalRoute data={data} />;
-    }
-
-    setPortal('');
-    if (isFlex) {
-        if (other) {
-            // Non-portal flex pages do not have children; keep it canonical
-            return <Navigate to={`/${name}`} />;
-        }
-        return <FlexPageUsingItsOwnLayout data={data} />;
-    }
-    return <NonFlexPageUsingDefaultLayout data={data} />;
+export function FlexPageUsingItsOwnLayout({data}: {data: FlexPageData}) {
+    return <LayoutUsingData data={data}><FlexPage data={data} /></LayoutUsingData>;
 }
 
-function FlexPageUsingItsOwnLayout({data}: {data: FlexPageData}) {
-    return useDataToSetLayoutParameters(data) ? null : <FlexPage data={data} />;
-}
-
-function NonFlexPageUsingDefaultLayout({data}: {data: PageData}) {
+export function NonFlexPageUsingDefaultLayout({data}: {data: PageData}) {
     const {dir: name} = useParams();
     const {setLayoutParameters} = useLayoutContext();
     const isGeneral = Boolean(data?.body);
@@ -143,46 +103,7 @@ function NonFlexPageUsingDefaultLayout({data}: {data: PageData}) {
     return isGeneral ? (
         <GeneralPageFromSlug slug={`spike/${data?.meta.slug}`} />
     ) : (
-        <ImportedPage name={name} />
-    );
-}
-
-function PortalRoute({data}: {data: FlexPageData}) {
-    if (useDataToSetLayoutParameters(data)) {
-        return null;
-    }
-
-    return (
-        <Routes>
-            <Route path="/details/*" element={<DetailsRoutes />} />
-            <Route path="/:dir/*" element={<PortalSubRoute />} />
-        </Routes>
-    );
-}
-
-// eslint-disable-next-line complexity
-function PortalSubRoute() {
-    const {name, data, hasError} = usePageDataFromRoute();
-
-    if (!data) {
-        return null;
-    }
-
-    if (hasError) {
-        return <Error404 />;
-    }
-
-    const isFlex = !hasError && isFlexPage(data);
-
-    if (isFlex) {
-        return <FlexPage data={data} />;
-    }
-    const isGeneral = Boolean(data?.body);
-
-    return isGeneral ? (
-        <GeneralPageFromSlug slug={`spike/${data?.meta.slug}`} />
-    ) : (
-        <ImportedPage name={name} />
+        <ImportedPage name={name as string} />
     );
 }
 
