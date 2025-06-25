c2ea0f587899e6de0d258b6d3ef570b43c036c52
Roy Johnson <roy.e.johnson@rice.edu>
2025-06-25 08:19:17 -0500

    Restore 100% coverage

--- test/src/pages/webinars/search-page.test.tsx
+++ test/src/pages/webinars/search-page.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {describe, expect, it} from '@jest/globals';
 import {render, screen} from '@testing-library/preact';
-import {MemoryRouter} from 'react-router-dom';
+import MemoryRouter from '~/../../test/helpers/future-memory-router';
 import {RouterContextProvider} from '~/components/shell/router-context';
 import SearchPage from '~/pages/webinars/search-page/search-page';
-import useFetchedData from '~/helpers/use-data';
+import * as UFD from '~/helpers/use-data';
 import {upcomingWebinar} from '../../data/webinars';
+import * as UWC from '~/pages/webinars/webinar-context';
 
 function Component({term = ''}: {term?: string}) {
     return (
@@ -17,25 +18,36 @@ function Component({term = ''}: {term?: string}) {
     );
 }
 
+jest.spyOn(UWC, 'default').mockReturnValue({
+    pageData: {},
+    past: [],
+    upcoming: [],
+    subjects: [],
+    collections: [],
+    searchFor: jest.fn(),
+    latestWebinars: []
+});
+
 jest.mock('~/helpers/use-data', () => jest.fn());
+const mockUseFetchedData = jest.spyOn(UFD, 'default');
 
 describe('webinar search page', () => {
     it('short circuits while waiting for webinars to return', () => {
-        (useFetchedData as jest.Mock).mockReturnValue(undefined);
+        mockUseFetchedData.mockReturnValue(undefined);
         render(<Component term='waiting' />);
 
         expect(screen.queryByText('matching')).toBeNull();
     });
     it('displays a message when search returns no results', () => {
-        (useFetchedData as jest.Mock).mockReturnValue([]);
+        mockUseFetchedData.mockReturnValue([]);
         render(<Component term='xnotfoundx' />);
 
         expect(screen.getByText('No matching webinars found')).toBeTruthy();
     });
     it('displays results when they are returned', () => {
         // For code coverage
-        (useFetchedData as jest.Mock).mockImplementation(({postProcess}) => {
-            postProcess(upcomingWebinar);
+        mockUseFetchedData.mockImplementation(({postProcess}) => {
+            postProcess?.(upcomingWebinar);
             return [upcomingWebinar];
         });
         render(<Component term='something' />);
