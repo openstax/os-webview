86b4204448aeeaa68eaf973871531edbab1301e2
Roy Johnson <roy.e.johnson@rice.edu>
2025-07-15 16:03:48 -0500

    Port partners.tsx

--- src/app/pages/partners/partners.tsx
+++ src/app/pages/partners/partners.tsx
@@ -32,27 +32,42 @@ function Confirmation() {
                     }
                     .
                 </div>
-                <button type="button" className="put-away" onClick={toggleDone}>&times;</button>
+                <button type="button" className="put-away" onClick={() => toggleDone(true)}>&times;</button>
             </div>
         </section>
     );
 }
 
-function getFilterOptions(data) {
+type PartnerPageData = {
+    title: string;
+    heading: string;
+    description: string;
+    partner_landing_page_link: string;
+    partner_request_info_link: string;
+    partner_full_partner_heading: string;
+    partner_full_partner_description: string;
+    partner_ally_heading: string;
+    partner_ally_description: string;
+    category_mapping: {[label: string]: string};
+    field_name_mapping: {[label: string]: string};
+    partner_type_choices: string[];
+};
+
+function getFilterOptions(data: PartnerPageData) {
     const excludeList = ['Book', 'Type'];
-    const categoryKeys = Reflect.ownKeys(data.category_mapping)
+    const categoryKeys = Object.keys(data.category_mapping)
         .filter((title) => !excludeList.includes(title));
     const result = categoryKeys
         .map((title) => ({
             title,
-            options: []
+            options: [] as Array<unknown>
         }));
     const mapToTitle = categoryKeys
         .map((k) => [k, data.category_mapping[k]])
         .reduce((obj, [text, prefix]) => {
             obj[prefix] = text;
             return obj;
-        }, {});
+        }, {} as Record<string, string>);
 
     Object.entries(data.field_name_mapping)
         .forEach(([label, value]) => {
@@ -60,9 +75,9 @@ function getFilterOptions(data) {
                 label,
                 value
             };
-            const categoryPrefix = Reflect.ownKeys(mapToTitle)
-                .find((prefix) => value.substr(0, prefix.length) === prefix);
-            const itemInResult = result.find((obj) => obj.title === mapToTitle[categoryPrefix]);
+            const categoryPrefix = Object.keys(mapToTitle)
+                .find((prefix) => value.substring(0, prefix.length) === prefix);
+            const itemInResult = categoryPrefix && result.find((obj) => obj.title === mapToTitle[categoryPrefix]);
 
             if (itemInResult) {
                 if (label === 'Cost per semester') {
@@ -76,7 +91,7 @@ function getFilterOptions(data) {
     return result;
 }
 
-function textsFromData(data) {
+function textsFromData(data: PartnerPageData) {
     const linkTexts = {
         websiteLinkText: data.partner_landing_page_link,
         infoLinkText: (data.partner_request_info_link || 'Request info')
@@ -85,7 +100,7 @@ function textsFromData(data) {
     return {linkTexts};
 }
 
-function Partners({data}) {
+function Partners({data}: {data: PartnerPageData}) {
     const location = useLocation();
     const {confirmation} = location.state || {};
     const advancedFilterOptions = React.useMemo(
