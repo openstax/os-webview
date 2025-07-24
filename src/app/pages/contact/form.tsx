6e089bd5077bd6e55090b44cae3d87a80397c555
Roy Johnson <roy.e.johnson@rice.edu>
2025-07-24 14:58:16 -0500

    Embedded contact header is Contact us

--- src/app/pages/contact/form.tsx
+++ src/app/pages/contact/form.tsx
@@ -95,7 +95,9 @@ function useAfterSubmit(setSubmitted?: SetSubmitted) {
 
 function useSubjectWithInitialization() {
     const isEmbedded = useIsEmbedded();
-    const initialValue = isEmbedded ? 'OpenStax Assignable' : 'General';
+    const searchParams = new window.URLSearchParams(window.location.search);
+    const bodyParams = searchParams.getAll('body');
+    const initialValue = isEmbedded && (bodyParams.length > 0) ? 'OpenStax Assignable' : 'General';
     const subjectState = useState(initialValue);
 
     // useEffect runs too late
