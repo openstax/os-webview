b64da5833166aa34ea4d74f635f6e6b32edc8f90
Roy Johnson <roy.e.johnson@rice.edu>
2025-08-01 15:13:50 -0500

    General page test coverage

---
+++ test/src/general/general.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {render, screen} from '@testing-library/preact';
+import MemoryRouter from '~/../../test/helpers/future-memory-router';
+import {RouterContextProvider} from '~/components/shell/router-context';
+import GeneralPageLoader from '~/pages/general/general';
+import * as DH from '~/helpers/use-document-head';
+import * as PDU from '~/helpers/page-data-utils';
+
+
+jest.spyOn(DH, 'default').mockReturnValue();
+
+describe('general page', () => {
+    it(
+        'renders kinetic page',
+        async () => {
+            render(
+                <MemoryRouter initialEntries={['/general/kinetic']}>
+                    <RouterContextProvider>
+                        <GeneralPageLoader />
+                    </RouterContextProvider>
+                </MemoryRouter>
+            );
+            await screen.findByText(/Example activities/i);
+        }
+    );
+    it('renders kinetic page (trailing slash)', async () => {
+        document.title = 'OpenStax Test';
+        render(
+            <MemoryRouter initialEntries={['/general/kinetic/']}>
+                <RouterContextProvider>
+                    <GeneralPageLoader />
+                </RouterContextProvider>
+            </MemoryRouter>
+        );
+        await screen.findByText(/Example activities/i);
+        expect(document.head.querySelector('link')?.getAttribute('href')).toBe('https://openstax.org/kinetic');
+    });
+    it('handles page load error', async () => {
+        const mockUseTextFromSlug = jest.spyOn(PDU, 'useTextFromSlug').mockReturnValueOnce({
+            head: {},
+            text: new Error('whoops')
+        });
+
+        render(
+            <MemoryRouter initialEntries={['/general/whoops/']}>
+                <RouterContextProvider>
+                    <GeneralPageLoader />
+                </RouterContextProvider>
+            </MemoryRouter>
+        );
+
+        const h = screen.getByRole('heading', {level: 1});
+
+        expect(h.textContent).toBe('Error: {}');
+        mockUseTextFromSlug.mockRestore();
+    });
+});
