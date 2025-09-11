5a611f2bfbb4a1c55ac9c0f597aecf5c8c86f51c
Roy Johnson <roy.e.johnson@rice.edu>
2025-09-11 11:01:05 -0500

    CORE-1221: Fix layoutParameters timing issue
    [CORE-1221]

--- src/app/contexts/layout.tsx
+++ src/app/contexts/layout.tsx
@@ -35,29 +35,24 @@ function useContextValue() {
         },
         [layoutParameters]
     );
-    const LoadableLayout = React.useMemo(
-        () =>
-            loadable({
-                loader: () =>
-                    loaders[
-                        layoutParameters.name as Exclude<LayoutName, null>
-                    ](),
-                loading: LoadingPlaceholder
-            }),
-        [layoutParameters.name]
-    );
     const Layout = React.useCallback(
         ({children}: React.PropsWithChildren<object>) => {
+            if (layoutParameters.name === null) {
+                return <div>{children}</div>;
+            }
+            const LoadableLayout = loadable({
+                loader: () => loaders[layoutParameters.name as LayoutName](),
+                loading: LoadingPlaceholder
+            });
+
             // Avoids initial flash default -> landing
-            return layoutParameters.name === null ? (
-                <div>{children}</div>
-            ) : (
+            return (
                 <LoadableLayout data={layoutParameters.data}>
                     {children}
                 </LoadableLayout>
             );
         },
-        [LoadableLayout, layoutParameters.data, layoutParameters.name]
+        [layoutParameters.data, layoutParameters.name]
     );
 
     return {Layout, setLayoutParameters: updateIfNotEqual, layoutParameters};
