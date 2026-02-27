fe3f113fdac40ac363ce59b8a974035842c2fb0b
tom <tom.g.woodward@gmail.com>
2026-02-27 13:35:02 -0500

    fix block map import

--- src/app/pages/flex-page/block-map.ts
+++ src/app/pages/flex-page/block-map.ts
@@ -1,14 +1,10 @@
 /* eslint-disable camelcase */
-import {
-    cta_block, cards_block, divider, html, hero, links_group,
-    quote, text, section, well, columns, tabbed_content, flex_page
-} from '@openstax/flex-page-renderer/blocks';
+import * as blocks from '@openstax/flex-page-renderer/blocks/index';
 import {FAQBlock} from './blocks/FAQBlock';
 import {BookListBlock} from './blocks/BookListBlock';
 
 export const blockMap = {
-    cta_block, cards_block, divider, html, hero, links_group,
-    quote, text, section, well, columns, tabbed_content, flex_page,
+    ...blocks,
     faq: FAQBlock,
     book_list: BookListBlock
 } as const;
