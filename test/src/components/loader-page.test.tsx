b7849cdc5315d7c39480821935ff0373998d8216
Roy Johnson <roy.e.johnson@rice.edu>
2026-06-08 10:08:50 -0500

    Fix test

--- test/src/components/loader-page.test.tsx
+++ test/src/components/loader-page.test.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {render, screen} from '@testing-library/preact';
 import { LoadedPage } from '~/components/jsx-helpers/loader-page';
 import MemoryRouter from '~/../../test/helpers/future-memory-router';
+import * as DH from '~/helpers/use-document-head';
 import { LayoutContextProvider } from '~/contexts/layout';
+
+jest.spyOn(DH, 'default').mockReturnValue(undefined);
 
 describe('loader-page', () => {
     // The rest of the code is exercised in other tests.
-    it('loads 404 on data error', () => {
+    it('loads 404 on data error', async () => {
         const data = {
             error: true
         };
@@ -20,6 +23,6 @@ describe('loader-page', () => {
             </MemoryRouter>
         );
 
-        screen.findByText('Whoops');
+        await screen.findByText('Uh-oh, no page here');
     });
 });
