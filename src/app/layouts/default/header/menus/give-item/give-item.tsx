db86f5387ddb80dca455ab26f70cad8860a95d77
Michael Volo <volo@rice.edu>
2026-06-30 14:04:05 -0500

    feat(nav): add reusable GiveItem (text link / gold button)

---
+++ src/app/layouts/default/header/menus/give-item/give-item.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import useGiveToday from '~/models/give-today';
+import GiveButton from '../give-button/give-button';
+
+const GIVE_HEADER_URL =
+    'https://riceconnect.rice.edu/donation/support-openstax-header';
+
+export default function GiveItem() {
+    const {showButton} = useGiveToday();
+
+    return showButton ? (
+        <GiveButton />
+    ) : (
+        <a target="_blank" rel="noreferrer" href={GIVE_HEADER_URL} data-analytics-link>
+            Give
+        </a>
+    );
+}
